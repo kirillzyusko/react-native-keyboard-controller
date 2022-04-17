17b76243cf6e5ee01d96bd4710e2692432a0360c
kirillzyusko <zyusko.kirik@gmail.com>
2022-04-17 22:27:24 +0400

    [RNKE-001] - save intermediate changes

--- android/src/main/java/com/reactnativekeyboardevents/KeyboardEventsViewManager.kt
+++ android/src/main/java/com/reactnativekeyboardevents/KeyboardEventsViewManager.kt
@@ -1,20 +1,43 @@
 package com.reactnativekeyboardevents
 
-import android.graphics.Color
 import android.view.View
 import com.facebook.react.uimanager.SimpleViewManager
 import com.facebook.react.uimanager.ThemedReactContext
-import com.facebook.react.uimanager.annotations.ReactProp
+import androidx.core.view.ViewCompat
+
+import com.reactnativekeyboardevents.events.KeyboardTransitionEvent
 
 class KeyboardEventsViewManager : SimpleViewManager<View>() {
+  var mCallerContext: ReactApplicationContext? = null
+
+  fun KeyboardEventsViewManager(reactContext: ReactApplicationContext?) {
+    mCallerContext = reactContext
+
+    ViewCompat.setWindowInsetsAnimationCallback(
+      binding.messageHolder,
+      TranslateDeferringInsetsAnimationCallback(
+        view = binding.messageHolder,
+        persistentInsetTypes = WindowInsetsCompat.Type.systemBars(),
+        deferredInsetTypes = WindowInsetsCompat.Type.ime(),
+        // We explicitly allow dispatch to continue down to binding.messageHolder's
+        // child views, so that step 2.5 below receives the call
+        dispatchMode = WindowInsetsAnimationCompat.Callback.DISPATCH_MODE_CONTINUE_ON_SUBTREE
+      )
+    )
+  }
+
   override fun getName() = "KeyboardEventsView"
 
   override fun createViewInstance(reactContext: ThemedReactContext): View {
     return View(reactContext)
   }
 
-  @ReactProp(name = "color")
-  fun setColor(view: View, color: String) {
-    view.setBackgroundColor(Color.parseColor(color))
+  override fun getExportedCustomBubblingEventTypeConstants(): MutableMap? {
+    val map: MutableMap<String, Any> = MapBuilder.of(
+      KeyboardTransitionEvent.EVENT_NAME,
+      MapBuilder.of("registrationName", "onProgress")
+    )
+
+    return map
   }
 }
---
+++ android/src/main/java/com/reactnativekeyboardevents/events/KeyboardTransitionEvent.kt
@@ -0,0 +1,20 @@
+package com.reactnativekeyboardevents.events
+
+import com.facebook.react.bridge.Arguments
+import com.facebook.react.uimanager.events.Event
+import com.facebook.react.uimanager.events.RCTEventEmitter
+
+class KeyboardTransitionEvent(viewId: Int) : Event<ScreenAppearEvent>(viewId) {
+  override fun getEventName() = EVENT_NAME
+
+  // All events for a given view can be coalesced.
+  override fun getCoalescingKey(): Short = 0
+
+  override fun dispatch(rctEventEmitter: RCTEventEmitter) {
+    rctEventEmitter.receiveEvent(viewTag, eventName, Arguments.createMap())
+  }
+
+  companion object {
+    const val EVENT_NAME = "topKeyboardChange"
+  }
+}
