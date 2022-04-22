53a94b2bea82c8532a00eb148b71219f119a8318
kirillzyusko <zyusko.kirik@gmail.com>
2022-04-22 19:08:40 +0300

    [RNKC-011] - updated example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,6 +1,13 @@
 import * as React from 'react';
 
-import { Animated, StyleSheet, View, TextInput } from 'react-native';
+import {
+  Animated,
+  StyleSheet,
+  View,
+  TextInput,
+  Keyboard,
+  Easing,
+} from 'react-native';
 import {
   KeyboardProvider,
   useKeyboardProgress,
@@ -8,19 +15,63 @@ import {
 
 function KeyboardAnimation() {
   const progress = useKeyboardProgress();
+  const replica = React.useRef(new Animated.Value(0)).current;
+
+  React.useEffect(() => {
+    const listener = Keyboard.addListener('keyboardDidShow', (e) => {
+      Animated.timing(replica, {
+        toValue: -e.endCoordinates.height,
+        duration: 300,
+        easing: Easing.bezier(0.4, 0.0, 0.2, 1),
+        useNativeDriver: true,
+      }).start();
+
+      return () => listener.remove();
+    });
+  }, []);
+  React.useEffect(() => {
+    const listener = Keyboard.addListener('keyboardDidHide', (e) => {
+      Animated.timing(replica, {
+        toValue: 0,
+        duration: 300,
+        easing: Easing.bezier(0.4, 0.0, 0.2, 1),
+        useNativeDriver: true,
+      }).start();
+
+      return () => listener.remove();
+    });
+  }, []);
 
   return (
     <View style={styles.container}>
-      <TextInput style={{ width: 200, marginTop: 50, height: 50, backgroundColor: "yellow" }} />
-      <Animated.View
+      <TextInput
         style={{
-          width: 50,
+          width: 200,
+          marginTop: 50,
           height: 50,
-          backgroundColor: 'red',
-          borderRadius: 25,
-          transform: [{ translateY: progress }],
+          backgroundColor: 'yellow',
         }}
       />
+      <View style={styles.row}>
+        <Animated.View
+          style={{
+            width: 50,
+            height: 50,
+            backgroundColor: 'red',
+            borderRadius: 25,
+            transform: [{ translateY: progress }],
+          }}
+        />
+        <Animated.View
+          style={{
+            width: 50,
+            height: 50,
+            backgroundColor: 'blue',
+            borderRadius: 25,
+            transform: [{ translateY: replica }],
+          }}
+        />
+      </View>
     </View>
   );
 }
@@ -43,5 +94,8 @@ const styles = StyleSheet.create({
     width: 60,
     height: 60,
     marginVertical: 20,
+  },
+  row: {
+    flexDirection: 'row',
   },
 });
