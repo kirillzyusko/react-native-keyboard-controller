266e7df083bf8ec8b685bfa625106d2c209df719
kirillzyusko <zyusko.kirik@gmail.com>
2022-04-27 00:27:21 +0400

    [RNKC-033] - common provider

--- src/animated.tsx
+++ src/animated.tsx
@@ -1,62 +1,143 @@
-import React, { useRef, useContext, useMemo } from 'react';
-import { Animated } from 'react-native';
+import React, { useContext, useMemo } from 'react';
+import { Animated, StyleSheet, ViewStyle } from 'react-native';
+import Reanimated, {
+  useEvent,
+  useHandler,
+  useSharedValue,
+} from 'react-native-reanimated';
 import {
+  EventWithName,
   KeyboardControllerProps,
   KeyboardControllerView,
+  NativeEvent,
   useResizeMode,
 } from './native';
-import { styles } from './styles';
 
-const KeyboardControllerViewAnimated = Animated.createAnimatedComponent(
-  KeyboardControllerView
-) as React.FC<KeyboardControllerProps>;
+const KeyboardControllerViewAnimated = Reanimated.createAnimatedComponent(
+  Animated.createAnimatedComponent(
+    KeyboardControllerView
+  ) as React.FC<KeyboardControllerProps>
+);
 
-type KeyboardAnimation = {
+type AnimatedContext = {
   progress: Animated.Value;
   height: Animated.Value;
 };
-const defaultContext: KeyboardAnimation = {
-  progress: new Animated.Value(0),
-  height: new Animated.Value(0),
+type ReanimatedContext = {
+  progress: Reanimated.SharedValue<number>;
+  height: Reanimated.SharedValue<number>;
+};
+type KeyboardAnimationContext = {
+  animated: AnimatedContext;
+  reanimated: ReanimatedContext;
+};
+const defaultContext: KeyboardAnimationContext = {
+  animated: {
+    progress: new Animated.Value(0),
+    height: new Animated.Value(0),
+  },
+  reanimated: {
+    progress: { value: 0 },
+    height: { value: 0 },
+  },
 };
 const KeyboardContext = React.createContext(defaultContext);
 
-export const useKeyboardAnimation = (): KeyboardAnimation => {
+export const useKeyboardAnimation = (): AnimatedContext => {
   useResizeMode();
   const context = useContext(KeyboardContext);
 
-  return context;
+  return context.animated;
 };
+
+export const useReanimatedKeyboardAnimation = (): ReanimatedContext => {
+  useResizeMode();
+  const context = useContext(KeyboardContext);
+
+  return context.reanimated;
+};
+
+function useAnimatedKeyboardHandler<TContext extends Record<string, unknown>>(
+  handlers: {
+    onKeyboardMove?: (e: NativeEvent, context: TContext) => void;
+  },
+  dependencies?: ReadonlyArray<unknown>
+) {
+  const { context, doDependenciesDiffer } = useHandler(handlers, dependencies);
+
+  return useEvent(
+    (event: EventWithName<NativeEvent>) => {
+      'worklet';
+      const { onKeyboardMove } = handlers;
+
+      if (onKeyboardMove && event.eventName.endsWith('onKeyboardMove')) {
+        onKeyboardMove(event, context);
+      }
+    },
+    ['onKeyboardMove'],
+    doDependenciesDiffer
+  );
+}
+
+type Styles = {
+  container: ViewStyle;
+};
+
+export const styles = StyleSheet.create<Styles>({
+  container: {
+    flex: 1,
+  },
+});
 
 export const KeyboardProvider = ({
   children,
 }: {
   children: React.ReactNode;
 }) => {
-  const progress = useRef(new Animated.Value(0));
-  const height = useRef(new Animated.Value(0));
+  const progress = useMemo(() => new Animated.Value(0), []);
+  const height = useMemo(() => new Animated.Value(0), []);
+  const progressSV = useSharedValue(0);
+  const heightSV = useSharedValue(0);
   const context = useMemo(
-    () => ({ progress: progress.current, height: height.current }),
+    () => ({
+      animated: { progress: progress, height: height },
+      reanimated: { progress: progressSV, height: heightSV },
+    }),
     []
   );
-  const onKeyboardMove = useRef(
-    Animated.event(
-      [
-        {
-          nativeEvent: {
-            progress: progress.current,
-            height: height.current,
+
+  const onKeyboardMove = useMemo(
+    () =>
+      Animated.event(
+        [
+          {
+            nativeEvent: {
+              progress,
+              height,
+            },
           },
-        },
-      ],
-      { useNativeDriver: true }
-    )
+        ],
+        { useNativeDriver: true }
+      ),
+    []
+  );
+
+  const handler = useAnimatedKeyboardHandler(
+    {
+      onKeyboardMove: (event: NativeEvent) => {
+        'worklet';
+        progressSV.value = event.progress;
+        heightSV.value = event.height;
+      },
+    },
+    []
   );
 
   return (
     <KeyboardContext.Provider value={context}>
       <KeyboardControllerViewAnimated
-        onKeyboardMove={onKeyboardMove.current}
+        onKeyboardMoveReanimated={handler}
+        onKeyboardMove={onKeyboardMove}
         style={styles.container}
       >
         {children}
