026a85521b8d8ea139931faf7e2c21a4e154f7b1
Kirill Zyusko <zyusko.kirik@gmail.com>
2022-05-12 22:40:33 +0400

    [RNKC-041] - correct subscribe/unsubscribe mechanism for keyboard events on iOS (#18)

--- ios/KeyboardControllerViewManager.swift
+++ ios/KeyboardControllerViewManager.swift
@@ -23,11 +23,20 @@ class KeyboardControllerView : UIView {
     }
     
     override func willMove(toWindow newWindow: UIWindow?) {
-        super.willMove(toWindow: newWindow)
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillDisappear), name: UIResponder.keyboardWillHideNotification, object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillAppear), name: UIResponder.keyboardWillShowNotification, object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardDidAppear), name: UIResponder.keyboardDidShowNotification, object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardDidDisappear), name: UIResponder.keyboardDidHideNotification, object: nil)
+        if (newWindow == nil) {
+            // Will be removed from a window
+            NotificationCenter.default.removeObserver(self)
+        }
+    }
+    
+    override func didMoveToWindow() {
+        if (self.window != nil) {
+            // Added to a window
+            NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillDisappear), name: UIResponder.keyboardWillHideNotification, object: nil)
+            NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillAppear), name: UIResponder.keyboardWillShowNotification, object: nil)
+            NotificationCenter.default.addObserver(self, selector: #selector(keyboardDidAppear), name: UIResponder.keyboardDidShowNotification, object: nil)
+            NotificationCenter.default.addObserver(self, selector: #selector(keyboardDidDisappear), name: UIResponder.keyboardDidHideNotification, object: nil)
+        }
     }
 
     @objc func keyboardWillAppear(_ notification: Notification) {
@@ -65,10 +74,5 @@ class KeyboardControllerView : UIView {
         var data = [AnyHashable: Any]()
         data["height"] = 0
         KeyboardController.shared?.sendEvent(withName: "KeyboardController::keyboardDidHide", body: data)
-    }
-
-    override func willRemoveSubview(_ subview: UIView) {
-        super.willRemoveSubview(subview)
-        NotificationCenter.default.removeObserver(self) // TODO: correct place?
     }
 }
