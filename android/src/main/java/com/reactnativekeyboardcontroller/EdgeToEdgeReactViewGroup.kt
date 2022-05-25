82bb1a568fc0f7993af4dcae32947f6a194177b0
Kirill Zyusko <zyusko.kirik@gmail.com>
2022-05-25 11:19:13 +0400

    [RNKC-044] - android lint (#26)
    * [RNKC-044] - android lint
    
    * [RNKC-044] - suppress ViewConstructor warning
    
    * [RNKC-044] - clean up workflow file

--- android/src/main/java/com/reactnativekeyboardcontroller/EdgeToEdgeReactViewGroup.kt
+++ android/src/main/java/com/reactnativekeyboardcontroller/EdgeToEdgeReactViewGroup.kt
@@ -1,9 +1,11 @@
 package com.reactnativekeyboardcontroller
 
+import android.annotation.SuppressLint
 import androidx.core.view.WindowCompat
 import com.facebook.react.uimanager.ThemedReactContext
 import com.facebook.react.views.view.ReactViewGroup
 
+@SuppressLint("ViewConstructor")
 class EdgeToEdgeReactViewGroup(private val reactContext: ThemedReactContext) : ReactViewGroup(reactContext) {
   override fun onAttachedToWindow() {
     super.onAttachedToWindow()
