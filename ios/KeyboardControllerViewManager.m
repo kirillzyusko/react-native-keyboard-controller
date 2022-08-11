65d9f62664360b9997e026c7c99582dd5d740af6
Kirill Zyusko <zyusko.kirik@gmail.com>
2022-08-11 10:29:48 +0400

    [RNKC-044] - clang formatter (#48)
    * [RNKC-044] - clang formatter
    
    * [RNKC-044] - align obj-c code to new standards

--- ios/KeyboardControllerViewManager.m
+++ ios/KeyboardControllerViewManager.m
@@ -1,6 +1,6 @@
 #import <React/RCTViewManager.h>
 
-@interface RCT_EXTERN_MODULE(KeyboardControllerViewManager, RCTViewManager)
+@interface RCT_EXTERN_MODULE (KeyboardControllerViewManager, RCTViewManager)
 
 RCT_EXPORT_VIEW_PROPERTY(onKeyboardMove, RCTDirectEventBlock);
 
