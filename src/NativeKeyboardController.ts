1bf9a12d72765c6078a69a0922797b55d3ecc052
kirillzyusko <zyusko.kirik@gmail.com>
2022-08-12 19:09:50 +0400

    [RNKC-051] - temporary commit

--- src/NativeKeyboardController.ts
+++ src/NativeKeyboardController.ts
@@ -4,8 +4,14 @@ import { TurboModuleRegistry } from 'react-native';
 export interface Spec extends TurboModule {
   readonly getConstants: () => {};
 
+  // methods
   setInputMode(mode: number): void;
   setDefaultMode(): void;
+
+  // TODO: implement
+  // event emitter
+  // addListener: (eventName: string) => void;
+  // removeListeners: (count: number) => void;
 }
 
 export default TurboModuleRegistry.get<Spec>('KeyboardController');
