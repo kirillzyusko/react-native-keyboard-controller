f24a0f2cadf96853c1e3fc03ad45324877921812
kirillzyusko <zyusko.kirik@gmail.com>
2022-10-01 20:31:05 +0400

    [RNKC-069] - updated chat example

--- FabricExample/src/screens/Examples/ReanimatedChat/styles.ts
+++ FabricExample/src/screens/Examples/ReanimatedChat/styles.ts
@@ -1,5 +1,15 @@
 import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
-  container: { justifyContent: 'flex-end', flex: 1 },
+  container: {
+    justifyContent: 'flex-end',
+    flex: 1,
+  },
+  inverted: {
+    transform: [
+      {
+        scaleY: -1,
+      },
+    ],
+  },
 });
