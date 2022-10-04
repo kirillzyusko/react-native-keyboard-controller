33dcac1dde11b37a5e9c6d2dd98849ec1e2ff780
kirillzyusko <zyusko.kirik@gmail.com>
2022-10-04 21:06:31 +0400

    [RNKC-067] - further cleanup

--- android/src/main/java/com/reactnativekeyboardcontroller/KeyboardControllerPackage.kt
+++ android/src/main/java/com/reactnativekeyboardcontroller/KeyboardControllerPackage.kt
@@ -4,7 +4,6 @@ import com.facebook.react.ReactPackage
 import com.facebook.react.bridge.NativeModule
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.uimanager.ViewManager
-import com.facebook.soloader.SoLoader
 
 class KeyboardControllerPackage : ReactPackage {
   override fun createNativeModules(reactContext: ReactApplicationContext): List<NativeModule> {
