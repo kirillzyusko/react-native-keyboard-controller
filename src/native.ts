6e6740c84dda479dfa1b0e7d96ab08adcbdef560
kirillzyusko <zyusko.kirik@gmail.com>
2022-10-07 14:24:08 +0400

    [RNKC-074] - more AndroidSoftInputModes

--- src/native.ts
+++ src/native.ts
@@ -12,11 +12,22 @@ const LINKING_ERROR =
   '- You rebuilt the app after installing the package\n' +
   '- You are not using Expo Go\n';
 
+// copied from `android.view.WindowManager.LayoutParams`
 export enum AndroidSoftInputModes {
   SOFT_INPUT_ADJUST_NOTHING = 48,
   SOFT_INPUT_ADJUST_PAN = 32,
   SOFT_INPUT_ADJUST_RESIZE = 16,
   SOFT_INPUT_ADJUST_UNSPECIFIED = 0,
+  SOFT_INPUT_IS_FORWARD_NAVIGATION = 256,
+  SOFT_INPUT_MASK_ADJUST = 240,
+  SOFT_INPUT_MASK_STATE = 15,
+  SOFT_INPUT_MODE_CHANGED = 512,
+  SOFT_INPUT_STATE_ALWAYS_HIDDEN = 3,
+  SOFT_INPUT_STATE_ALWAYS_VISIBLE = 5,
+  SOFT_INPUT_STATE_HIDDEN = 2,
+  SOFT_INPUT_STATE_UNCHANGED = 1,
+  SOFT_INPUT_STATE_UNSPECIFIED = 0,
+  SOFT_INPUT_STATE_VISIBLE = 4,
 }
 
 export type NativeEvent = {
@@ -37,7 +48,7 @@ export type KeyboardControllerProps = {
 type KeyboardController = {
   // android only
   setDefaultMode: () => void;
-  setInputMode: (mode: AndroidSoftInputModes) => void;
+  setInputMode: (mode: number) => void;
   // native event module stuff
   addListener: (eventName: string) => void;
   removeListeners: (count: number) => void;
