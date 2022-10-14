a47fc9f929be1c28dd0d2a0221f9bd2b80e1d0a2
kirillzyusko <zyusko.kirik@gmail.com>
2022-10-14 10:41:48 +0400

    [RNKC-080] - telegram transitions

--- example/src/screens/Examples/ReanimatedChat/index.tsx
+++ example/src/screens/Examples/ReanimatedChat/index.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
-import { TextInput, View } from 'react-native';
-import { useReanimatedKeyboardAnimation } from 'react-native-keyboard-controller';
-import Reanimated, { useAnimatedStyle } from 'react-native-reanimated';
+import { Platform, TextInput, View } from 'react-native';
+import { useKeyboardHandler } from 'react-native-keyboard-controller';
+import Reanimated, {
+  Easing,
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from 'react-native-reanimated';
 import Message from '../../../components/Message';
 import { history } from '../../../components/Message/data';
 import styles from './styles';
@@ -9,7 +14,36 @@ import styles from './styles';
 const AnimatedTextInput = Reanimated.createAnimatedComponent(TextInput);
 
 function ReanimatedChat() {
-  const { height } = useReanimatedKeyboardAnimation();
+  const height = useSharedValue(0);
+  useKeyboardHandler(
+    {
+      onStart: (e) => {
+        'worklet';
+
+        if (Platform.OS === 'android') {
+          // on Android Telegram is not using androidx.core values and uses custom interpolation
+          // duration is taken from here: https://github.com/DrKLO/Telegram/blob/e9a35cea54c06277c69d41b8e25d94b5d7ede065/TMessagesProj/src/main/java/org/telegram/ui/ActionBar/AdjustPanLayoutHelper.java#L39
+          // and bezier is taken from: https://github.com/DrKLO/Telegram/blob/e9a35cea54c06277c69d41b8e25d94b5d7ede065/TMessagesProj/src/main/java/androidx/recyclerview/widget/ChatListItemAnimator.java#L40
+          height.value = withTiming(-e.height, {
+            duration: 250,
+            easing: Easing.bezier(
+              0.19919472913616398,
+              0.010644531250000006,
+              0.27920937042459737,
+              0.91025390625
+            ),
+          });
+        } else {
+          // on iOS Telegram simply moves TextInput synchronously with the content
+          // to achieve such behavior we are instantly change `height.value` to keyboard
+          // final frame - iOS will schedule layout animation and it will move the content
+          // altogether with the keyboard
+          height.value = -e.height;
+        }
+      },
+    },
+    []
+  );
 
   const scrollViewStyle = useAnimatedStyle(
     () => ({
