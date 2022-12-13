9c7f327c168de9c173cb5e612ef2236681623d90
kirillzyusko <zyusko.kirik@gmail.com>
2022-12-13 20:50:24 +0400

    refactor: add a sample of API for testing

--- example/src/screens/Examples/KeyboardAnimation/spec/index.spec.tsx
+++ example/src/screens/Examples/KeyboardAnimation/spec/index.spec.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import { render } from '@testing-library/react-native';
 
-import { setKeyboardPosition } from 'react-native-keyboard-controller/jest';
+import { setKeyboardPosition, setKeyboardVisibleHeight } from 'react-native-keyboard-controller/jest';
 
 import KeyboardAnimation from '../index';
+
+beforeAll(() => {
+  setKeyboardVisibleHeight(400);
+});
 
 describe('unit test sample', () => {
   it('should match to snapshot', () => {
     const screen = render(<KeyboardAnimation />);
 
-    setKeyboardPosition(100);
+    setKeyboardPosition(200);
 
     expect(screen).toMatchSnapshot();
   });
