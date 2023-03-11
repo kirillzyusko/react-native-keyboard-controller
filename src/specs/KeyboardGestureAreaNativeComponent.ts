3d8cf482375ed7eef79e750eea048a518f4dc67d
kirillzyusko <zyusko.kirik@gmail.com>
2023-03-11 22:35:23 +0400

    chore: removed unused ts-expect-error directive

--- src/specs/KeyboardGestureAreaNativeComponent.ts
+++ src/specs/KeyboardGestureAreaNativeComponent.ts
@@ -1,5 +1,4 @@
 import type { HostComponent } from 'react-native';
-// @ts-expect-error - no type definition
 import type { ViewProps } from 'react-native/Libraries/Components/View/ViewPropTypes';
 import codegenNativeComponent from 'react-native/Libraries/Utilities/codegenNativeComponent';
 
