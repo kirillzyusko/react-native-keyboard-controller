5110bbcbb6843563cc9aeaffb70e7efab37ec2d0
kirillzyusko <zyusko.kirik@gmail.com>
2023-03-31 18:33:17 +0400

    refactor: fixed android lint

--- android/src/main/java/com/reactnativekeyboardcontroller/extensions/View.kt
+++ android/src/main/java/com/reactnativekeyboardcontroller/extensions/View.kt
@@ -5,6 +5,7 @@ import android.graphics.Rect
 import android.os.Build
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
 
 /**
  * Call this everytime when using [ViewCompat.setOnApplyWindowInsetsListener]
@@ -36,6 +37,7 @@ private val tmpIntArr = IntArray(2)
 /**
  * Function which updates the given [rect] with this view's position and bounds in its window.
  */
+@RequiresApi(Build.VERSION_CODES.KITKAT)
 fun View.copyBoundsInWindow(rect: Rect) {
   if (isLaidOut && isAttachedToWindow) {
     rect.set(0, 0, width, height)
@@ -46,34 +48,5 @@ fun View.copyBoundsInWindow(rect: Rect) {
       "Can not copy bounds as view is not laid out" +
         " or attached to window",
     )
-  }
-}
-
-/**
- * Provides access to the hidden ViewGroup#suppressLayout method.
- */
-fun ViewGroup.suppressLayoutCompat(suppress: Boolean) {
-  if (Build.VERSION.SDK_INT >= 29) {
-    suppressLayout(suppress)
-  } else {
-    hiddenSuppressLayout(this, suppress)
-  }
-}
-
-/**
- * False when linking of the hidden suppressLayout method has previously failed.
- */
-private var tryHiddenSuppressLayout = true
-
-@SuppressLint("NewApi") // Lint doesn't know about the hidden method.
-private fun hiddenSuppressLayout(group: ViewGroup, suppress: Boolean) {
-  if (tryHiddenSuppressLayout) {
-    // Since this was an @hide method made public, we can link directly against it with
-    // a try/catch for its absence instead of doing the same through reflection.
-    try {
-      group.suppressLayout(suppress)
-    } catch (e: NoSuchMethodError) {
-      tryHiddenSuppressLayout = false
-    }
   }
 }
