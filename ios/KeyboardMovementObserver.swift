c73b8941775226060851c4bfb86a562b66b4b2de
kirillzyusko <zyusko.kirik@gmail.com>
2023-04-04 11:09:32 +0400

    refactor: apply swiftformat

--- ios/KeyboardMovementObserver.swift
+++ ios/KeyboardMovementObserver.swift
@@ -9,18 +9,16 @@
 import Foundation
 
 func interpolate(inputRange: [CGFloat], outputRange: [CGFloat], currentValue: CGFloat) -> CGFloat {
-    let inputMin = inputRange.min() ?? 0
-    let inputMax = inputRange.max() ?? 1
-    let outputMin = outputRange.min() ?? 0
-    let outputMax = outputRange.max() ?? 1
-    
-    let normalizedValue = (currentValue - inputMin) / (inputMax - inputMin)
-    let interpolatedValue = outputMin + (outputMax - outputMin) * normalizedValue
-    
-    return interpolatedValue
+  let inputMin = inputRange.min() ?? 0
+  let inputMax = inputRange.max() ?? 1
+  let outputMin = outputRange.min() ?? 0
+  let outputMax = outputRange.max() ?? 1
+
+  let normalizedValue = (currentValue - inputMin) / (inputMax - inputMin)
+  let interpolatedValue = outputMin + (outputMax - outputMin) * normalizedValue
+
+  return interpolatedValue
 }
-
-
 
 @objc(KeyboardMovementObserver)
 public class KeyboardMovementObserver: NSObject {
@@ -54,9 +52,6 @@ public class KeyboardMovementObserver: NSObject {
   }
 
   @objc public func mount() {
-      print(interpolate(inputRange: [0.0, 50.0, 100.0], outputRange: [0.0, 50.0, 200.0], currentValue: 75.0)) // 150
-      print(interpolate(inputRange: [-100.0, 0, 100.0], outputRange: [0.0, 20.0, 200.0], currentValue: -50.0)) // 10
-      print(interpolate(inputRange: [-100.0, 0, 100.0], outputRange: [0.0, 20.0, 200.0], currentValue: 50)) // 110
     NotificationCenter.default.addObserver(
       self,
       selector: #selector(keyboardWillDisappear),
@@ -88,47 +83,46 @@ public class KeyboardMovementObserver: NSObject {
       object: nil
     )
   }
-    
-    @objc func windowDidBecomeVisible(_ notification: Notification) {
-            print(1111188)
-            /*if (displayLink == nil) {
-                return;
-            }*/
 
-            let keyboardView = self.findKeyboardView();
+  @objc func windowDidBecomeVisible(_: Notification) {
+    print(1_111_188)
+    /* if (displayLink == nil) {
+         return;
+     } */
 
-            if (keyboardView == _keyboardView) {
-                    return;
-                }
+    let keyboardView = findKeyboardView()
 
-            _keyboardView = keyboardView;
+    if keyboardView == _keyboardView {
+      return
+    }
 
-            keyboardView?.addObserver(self, forKeyPath: "center", options: .new, context: nil)
-        }
-    
-    @objc public override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+    _keyboardView = keyboardView
 
-        if keyPath == "center" && object as! NSObject == _keyboardView! {
-            if (displayLink != nil || keyboardHeight == 0.0) {
-                    // setupKeyboardWatcher()
-                    // TODO: if keyboard is not shown yet or we are currently animating keyboard -> we need to ignore values from KVO
-                    return
-                }
-                let keyboardFrameY = ((change?[.newKey]) as! NSValue).cgPointValue.y
-                print(((change?[.newKey]) as! NSValue).cgPointValue.y)
-                print(777777, _keyboardView?.layer.presentation()?.frame.origin.y, keyboardFrameY)
-                let keyboardWindowH = keyboardView?.window?.bounds.size.height ?? 0
-                let keyboardPosition = keyboardWindowH - keyboardFrameY
-                let position = interpolate(inputRange: [keyboardHeight/2,-keyboardHeight/2], outputRange: [keyboardHeight, 0], currentValue: keyboardPosition)
-                // self.updateKeyboardFrame()
-                /*let age = change?[.newKey] {
-                    // print("New age is: \(age)")
-                    self.updateKeyboardFrame()
-             }*/
-                print(keyboardPosition)
-                onEvent("onKeyboardMoveInteractive", position as NSNumber, position / CGFloat(keyboardHeight) as NSNumber)
-            }
-        }
+    keyboardView?.addObserver(self, forKeyPath: "center", options: .new, context: nil)
+  }
+
+  @objc override public func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey: Any]?, context _: UnsafeMutableRawPointer?) {
+    if keyPath == "center", object as! NSObject == _keyboardView! {
+      if displayLink != nil || keyboardHeight == 0.0 {
+        // setupKeyboardWatcher()
+        // TODO: if keyboard is not shown yet or we are currently animating keyboard -> we need to ignore values from KVO
+        return
+      }
+      let keyboardFrameY = (change?[.newKey] as! NSValue).cgPointValue.y
+      print((change?[.newKey] as! NSValue).cgPointValue.y)
+      print(777_777, _keyboardView?.layer.presentation()?.frame.origin.y, keyboardFrameY)
+      let keyboardWindowH = keyboardView?.window?.bounds.size.height ?? 0
+      let keyboardPosition = keyboardWindowH - keyboardFrameY
+      let position = interpolate(inputRange: [keyboardHeight / 2, -keyboardHeight / 2], outputRange: [keyboardHeight, 0], currentValue: keyboardPosition)
+      // self.updateKeyboardFrame()
+      /* let age = change?[.newKey] {
+              // print("New age is: \(age)")
+              self.updateKeyboardFrame()
+       } */
+      print(keyboardPosition)
+      onEvent("onKeyboardMoveInteractive", position as NSNumber, position / CGFloat(keyboardHeight) as NSNumber)
+    }
+  }
 
   @objc public func unmount() {
     // swiftlint:disable notification_center_detachment
@@ -175,7 +169,7 @@ public class KeyboardMovementObserver: NSObject {
       onNotify("KeyboardController::keyboardDidShow", data)
 
       // keyboardView?.addObserver(self, forKeyPath: "center", options: .new, context: nil)
-        
+
       removeKeyboardWatcher()
     }
   }
@@ -183,7 +177,7 @@ public class KeyboardMovementObserver: NSObject {
   @objc func keyboardDidDisappear() {
     var data = [AnyHashable: Any]()
     data["height"] = 0
-      
+
     keyboardHeight = 0.0
 
     onEvent("onKeyboardMoveEnd", 0 as NSNumber, 0)
@@ -193,10 +187,10 @@ public class KeyboardMovementObserver: NSObject {
   }
 
   @objc func setupKeyboardWatcher() {
-      // TODO: sometimes `will` events can be called multiple times. To avoid double re-creation of listener we are adding this condition (if active link is present, then no need to re-setup a listener)
-      if (displayLink != nil) {
-          return
-      }
+    // TODO: sometimes `will` events can be called multiple times. To avoid double re-creation of listener we are adding this condition (if active link is present, then no need to re-setup a listener)
+    if displayLink != nil {
+      return
+    }
     displayLink = CADisplayLink(target: self, selector: #selector(updateKeyboardFrame))
     displayLink?.preferredFramesPerSecond = 120 // will fallback to 60 fps for devices without Pro Motion display
     displayLink?.add(to: .main, forMode: .common)
@@ -232,7 +226,7 @@ public class KeyboardMovementObserver: NSObject {
   }
 
   @objc func updateKeyboardFrame() {
-      print("updateKeyboardFrame")
+    print("updateKeyboardFrame")
     if keyboardView == nil {
       return
     }
