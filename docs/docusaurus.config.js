2bf021dd81429f89cfa596cfb718d0f3a91891b1
Kirill Zyusko <zyusko.kirik@gmail.com>
2023-04-04 17:05:12 +0400

    docs: docusaurus v2.4.0 (#134)
    ## 📜 Description
    
    Upgraded `docusaurus` to `2.4.0`.
    
    ## 💡 Motivation and Context
    
    To keep project up-to-date 🙂 
    
    ## 📢 Changelog
    
    ### Docs
    - update `docusaurus` to `2.4.0`;
    - changed `type` to `docSidebar` and `docId: 'installation',` ->
    `sidebarId: 'tutorialSidebar',`;
    
    ## 🤔 How Has This Been Tested?
    
    Tested on localhost:3000.
    
    ## 📝 Checklist
    
    - [x] CI successfully passed

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -73,8 +73,8 @@ const config = {
         },
         items: [
           {
-            type: 'doc',
-            docId: 'installation',
+            type: 'docSidebar',
+            sidebarId: 'tutorialSidebar',
             position: 'left',
             label: 'Guides',
           },
