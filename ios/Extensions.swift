0eb7e7e663d0b257987de2e6c16afb13880275cc
kirillzyusko <zyusko.kirik@gmail.com>
2023-06-10 12:10:09 +0200

    refactor: apply swiftlint

--- ios/Extensions.swift
+++ ios/Extensions.swift
@@ -9,15 +9,15 @@
 import Foundation
 
 public extension CGFloat {
-    static func interpolate(inputRange: [CGFloat], outputRange: [CGFloat], currentValue: CGFloat) -> CGFloat {
-      let inputMin = inputRange.min() ?? 0
-      let inputMax = inputRange.max() ?? 1
-      let outputMin = outputRange.min() ?? 0
-      let outputMax = outputRange.max() ?? 1
+  static func interpolate(inputRange: [CGFloat], outputRange: [CGFloat], currentValue: CGFloat) -> CGFloat {
+    let inputMin = inputRange.min() ?? 0
+    let inputMax = inputRange.max() ?? 1
+    let outputMin = outputRange.min() ?? 0
+    let outputMax = outputRange.max() ?? 1
 
-      let normalizedValue = (currentValue - inputMin) / (inputMax - inputMin)
-      let interpolatedValue = outputMin + (outputMax - outputMin) * normalizedValue
+    let normalizedValue = (currentValue - inputMin) / (inputMax - inputMin)
+    let interpolatedValue = outputMin + (outputMax - outputMin) * normalizedValue
 
-      return interpolatedValue
-    }
+    return interpolatedValue
+  }
 }
