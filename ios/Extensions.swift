c441aa646a74eddb1cca3f10be0b86465001e90a
Kirill Zyusko <zyusko.kirik@gmail.com>
2023-06-10 12:27:07 +0200

    refactor: move `interpolate` function to Extensions (#170)
    ## 📜 Description
    
    Moved `interpolate` function to Extensions.
    
    ## 💡 Motivation and Context
    
    To follow SOLID principles -> each file should have own responsibility.
    From my point of view it's not correct to keep `interpolate` function in
    `KeyboardMovementObserver`, so in this PR I've created a new file and
    moved `interpolate` implementation to this file.
    
    In future I'll create more extensions 🙂 
    
    ## 📢 Changelog
    
    ### iOS
    - added `Extensions.swift` file;
    - move `interpolate` function to `Extensions.swift`.
    
    ## 🤔 How Has This Been Tested?
    
    Tested manually on simulator.
    
    ## 📝 Checklist
    
    - [x] CI successfully passed

---
+++ ios/Extensions.swift
@@ -0,0 +1,23 @@
+//
+//  Extensions.swift
+//  KeyboardController
+//
+//  Created by Kiryl Ziusko on 10/06/2023.
+//  Copyright © 2023 Facebook. All rights reserved.
+//
+
+import Foundation
+
+public extension CGFloat {
+  static func interpolate(inputRange: [CGFloat], outputRange: [CGFloat], currentValue: CGFloat) -> CGFloat {
+    let inputMin = inputRange.min() ?? 0
+    let inputMax = inputRange.max() ?? 1
+    let outputMin = outputRange.min() ?? 0
+    let outputMax = outputRange.max() ?? 1
+
+    let normalizedValue = (currentValue - inputMin) / (inputMax - inputMin)
+    let interpolatedValue = outputMin + (outputMax - outputMin) * normalizedValue
+
+    return interpolatedValue
+  }
+}
