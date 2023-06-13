6364b4b3aef382db8cb6a48d4b997b2e1e7c0bd3
Ivan <63238943+thanksyouall@users.noreply.github.com>
2023-06-13 17:19:58 +0300

    Change an order of the "view" component

--- src/animated.tsx
+++ src/animated.tsx
@@ -151,21 +151,19 @@ export const KeyboardProvider = ({
         statusBarTranslucent={statusBarTranslucent}
         style={styles.container}
       >
-        <>
-          <Animated.View
-            // we are using this small hack, because if the component (where
-            // animated value has been used) is unmounted, then animation will
-            // stop receiving events (seems like it's react-native optimization).
-            // So we need to keep a reference to the animated value, to keep it's
-            // always mounted (keep a reference to an animated value).
-            //
-            // To test why it's needed, try to open screen which consumes Animated.Value
-            // then close it and open it again (for example 'Animated transition').
-            style={style}
-          />
-          {children}
-        </>
+            {children}
       </KeyboardControllerViewAnimated>
+      <Animated.View
+        // we are using this small hack, because if the component (where
+        // animated value has been used) is unmounted, then animation will
+        // stop receiving events (seems like it's react-native optimization).
+        // So we need to keep a reference to the animated value, to keep it's
+        // always mounted (keep a reference to an animated value).
+        //
+        // To test why it's needed, try to open screen which consumes Animated.Value
+        // then close it and open it again (for example 'Animated transition').
+        style={style}
+      />
     </KeyboardContext.Provider>
   );
 };
