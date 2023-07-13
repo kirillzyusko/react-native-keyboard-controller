37fe7b0ea8c4918a96717fe4ae8770def762baf8
kirillzyusko <zyusko.kirik@gmail.com>
2023-07-13 10:37:58 +0200

    refactor: fixed swiftlint

--- ios/Extensions.swift
+++ ios/Extensions.swift
@@ -23,7 +23,7 @@ public extension CGFloat {
 }
 
 public extension Date {
-  static var currentTimeStamp: Int64{
+  static var currentTimeStamp: Int64 {
     return Int64(Date().timeIntervalSince1970 * 1000)
   }
 }
