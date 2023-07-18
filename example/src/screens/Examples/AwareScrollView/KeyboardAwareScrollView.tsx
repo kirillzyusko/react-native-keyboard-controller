3e7885dd8b05d74a29f49db94ae16883bfba19f8
kirillzyusko <zyusko.kirik@gmail.com>
2023-07-18 19:17:40 +0200

    refactor: measure by target

--- example/src/screens/Examples/AwareScrollView/KeyboardAwareScrollView.tsx
+++ example/src/screens/Examples/AwareScrollView/KeyboardAwareScrollView.tsx
@@ -1,12 +1,9 @@
-import React, { FC, useCallback } from 'react';
-import {
-  GestureResponderEvent,
-  ScrollViewProps,
-  useWindowDimensions,
-} from 'react-native';
-import { useResizeMode } from 'react-native-keyboard-controller';
+import React, { Component, FC } from 'react';
+import { ScrollViewProps, useWindowDimensions } from 'react-native';
 import Reanimated, {
+  MeasuredDimensions,
   interpolate,
+  measure,
   scrollTo,
   useAnimatedRef,
   useAnimatedScrollHandler,
@@ -14,6 +11,7 @@ import Reanimated, {
   useSharedValue,
   useWorkletCallback,
 } from 'react-native-reanimated';
+import { RefObjectFunction } from 'react-native-reanimated/lib/types/lib/reanimated2/hook/commonTypes';
 import { useSmoothKeyboardHandler } from './useSmoothKeyboardHandler';
 
 const BOTTOM_OFFSET = 50;
@@ -22,14 +20,13 @@ const KeyboardAwareScrollView: FC<ScrollViewProps> = ({
   children,
   ...rest
 }) => {
-  useResizeMode();
-
   const scrollViewAnimatedRef = useAnimatedRef<Reanimated.ScrollView>();
   const scrollPosition = useSharedValue(0);
-  const click = useSharedValue(0);
   const position = useSharedValue(0);
+  const layout = useSharedValue<MeasuredDimensions | null>(null);
   const fakeViewHeight = useSharedValue(0);
   const keyboardHeight = useSharedValue(0);
+  const tag = useSharedValue(-1);
 
   const { height } = useWindowDimensions();
 
@@ -41,13 +38,10 @@ const KeyboardAwareScrollView: FC<ScrollViewProps> = ({
     },
     []
   );
-
-  const onContentTouch = useCallback((e: GestureResponderEvent) => {
-    // to prevent clicks when keyboard is animating
-    if (keyboardHeight.value === 0) {
-      click.value = e.nativeEvent.pageY;
-      scrollPosition.value = position.value;
-    }
+  const measureByTag = useWorkletCallback((viewTag: number) => {
+    return measure(
+      (() => viewTag) as unknown as RefObjectFunction<Component<{}, {}, any>>
+    );
   }, []);
 
   /**
@@ -59,12 +53,13 @@ const KeyboardAwareScrollView: FC<ScrollViewProps> = ({
     fakeViewHeight.value = e;
 
     const visibleRect = height - keyboardHeight.value;
+    const point = (layout.value?.pageY || 0) + (layout.value?.height || 0);
 
-    if (visibleRect - click.value <= BOTTOM_OFFSET) {
+    if (visibleRect - point <= BOTTOM_OFFSET) {
       const interpolatedScrollTo = interpolate(
         e,
         [0, keyboardHeight.value],
-        [0, keyboardHeight.value - (height - click.value) + BOTTOM_OFFSET]
+        [0, keyboardHeight.value - (height - point) + BOTTOM_OFFSET]
       );
       const targetScrollY =
         Math.max(interpolatedScrollTo, 0) + scrollPosition.value;
@@ -78,6 +73,27 @@ const KeyboardAwareScrollView: FC<ScrollViewProps> = ({
       onStart: (e) => {
         'worklet';
 
+        // keyboard will appear
+        if (e.height > 0 && keyboardHeight.value === 0) {
+          // persist scroll value
+          scrollPosition.value = position.value;
+        }
+
+        // focus was changed
+        if (
+          tag.value !== e.target ||
+          (keyboardHeight.value !== e.height && e.height > 0)
+        ) {
+          tag.value = e.target;
+
+          if (tag.value !== -1) {
+            // save position of focused text input when keyboard starts to move
+            layout.value = measureByTag(e.target);
+            console.log('UPDATED LAYOUT::', layout.value);
+          }
+        }
+
+        // keyboard will appear or change its size
         if (e.height > 0) {
           // just persist height - later will be used in interpolation
           keyboardHeight.value = e.height;
@@ -109,7 +125,6 @@ const KeyboardAwareScrollView: FC<ScrollViewProps> = ({
       ref={scrollViewAnimatedRef}
       {...rest}
       onScroll={onScroll}
-      onTouchStart={onContentTouch}
       scrollEventThrottle={16}
     >
       {children}
