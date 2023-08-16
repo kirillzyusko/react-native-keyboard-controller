320beee968d606393461a13270210003eeecb6d8
kirillzyusko <zyusko.kirik@gmail.com>
2023-08-16 19:39:38 +0200

    refactor: new way for handling insets

--- android/src/main/java/com/reactnativekeyboardcontroller/views/EdgeToEdgeReactViewGroup.kt
+++ android/src/main/java/com/reactnativekeyboardcontroller/views/EdgeToEdgeReactViewGroup.kt
@@ -1,16 +1,15 @@
 package com.reactnativekeyboardcontroller.views
 
 import android.annotation.SuppressLint
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
+import android.widget.FrameLayout
 import androidx.appcompat.widget.FitWindowsLinearLayout
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowCompat
-import androidx.core.view.WindowInsetsAnimationCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.*
 import com.facebook.react.uimanager.ThemedReactContext
 import com.facebook.react.views.view.ReactViewGroup
 import com.reactnativekeyboardcontroller.KeyboardAnimationCallback
-import com.reactnativekeyboardcontroller.R
 import com.reactnativekeyboardcontroller.extensions.requestApplyInsetsWhenAttached
 
 private val TAG = EdgeToEdgeReactViewGroup::class.qualifiedName
@@ -24,7 +23,6 @@ class EdgeToEdgeReactViewGroup(private val reactContext: ThemedReactContext) : R
     val activity = reactContext.currentActivity
 
     if (activity != null) {
-
       val callback = KeyboardAnimationCallback(
         view = this,
         persistentInsetTypes = WindowInsetsCompat.Type.systemBars(),
@@ -33,30 +31,6 @@ class EdgeToEdgeReactViewGroup(private val reactContext: ThemedReactContext) : R
         // child views, so that step 2.5 below receives the call
         dispatchMode = WindowInsetsAnimationCompat.Callback.DISPATCH_MODE_CONTINUE_ON_SUBTREE,
         context = reactContext,
-        onApplyWindowInsetsListener = { v, insets ->
-          val content =
-            reactContext.currentActivity?.window?.decorView?.rootView?.findViewById<FitWindowsLinearLayout>(
-              androidx.appcompat.R.id.action_bar_root,
-            )
-          content?.setPadding(
-            0,
-            if (this.isStatusBarTranslucent) {
-              0
-            } else {
-              insets?.getInsets(WindowInsetsCompat.Type.systemBars())?.top
-                ?: 0
-            },
-            0,
-            if (this.isNavigationBarTranslucent) {
-              0
-            } else {
-              insets?.getInsets(WindowInsetsCompat.Type.navigationBars())?.bottom
-                ?: 0
-            },
-          )
-
-          insets
-        },
       )
       ViewCompat.setWindowInsetsAnimationCallback(this, callback)
       ViewCompat.setOnApplyWindowInsetsListener(this, callback)
@@ -66,9 +40,45 @@ class EdgeToEdgeReactViewGroup(private val reactContext: ThemedReactContext) : R
     }
   }
 
+  private fun setupWindowInsets() {
+    val v = reactContext.currentActivity?.window?.decorView?.rootView
+    if (v != null) {
+      ViewCompat.setOnApplyWindowInsetsListener(v) { v, insets ->
+        val content =
+          reactContext.currentActivity?.window?.decorView?.rootView?.findViewById<FitWindowsLinearLayout>(
+            androidx.appcompat.R.id.action_bar_root,
+          )
+        println(
+          "12121212 ${insets?.getInsets(WindowInsetsCompat.Type.systemBars())?.top}, ${
+            insets?.getInsets(
+              WindowInsetsCompat.Type.navigationBars()
+            )?.bottom
+          }"
+        )
+        val params = FrameLayout.LayoutParams(
+          FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.MATCH_PARENT
+        )
+
+        params.setMargins(
+          0,
+          if (this.isStatusBarTranslucent) 0 else insets?.getInsets(WindowInsetsCompat.Type.systemBars())?.top
+            ?: 0,
+          0,
+          if (this.isNavigationBarTranslucent) 0 else insets?.getInsets(WindowInsetsCompat.Type.navigationBars())?.bottom
+            ?: 0
+        )
+
+        content?.layoutParams = params
+
+        insets
+      }
+    }
+  }
+
   override fun onAttachedToWindow() {
     super.onAttachedToWindow()
 
+    Handler(Looper.getMainLooper()).post(this::setupWindowInsets)
     reactContext.currentActivity?.let {
       WindowCompat.setDecorFitsSystemWindows(
         it.window,
