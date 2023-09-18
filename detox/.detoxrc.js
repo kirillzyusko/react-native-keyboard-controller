046573e4ced563c17e8578d2769aab2e39ba5811
kirillzyusko <zyusko.kirik@gmail.com>
2023-09-18 13:04:16 +0300

    feat: initial Android support

--- detox/.detoxrc.js
+++ detox/.detoxrc.js
@@ -22,16 +22,16 @@ module.exports = {
     },
     'example.android.debug': {
       type: 'android.apk',
-      binaryPath: 'android/app/build/outputs/apk/debug/app-debug.apk',
-      build: 'cd android && ./gradlew assembleDebug assembleAndroidTest -DtestBuildType=debug',
+      binaryPath: '../example/android/app/build/outputs/apk/debug/app-debug.apk',
+      build: 'cd ../example/android && ./gradlew assembleDebug assembleAndroidTest -DtestBuildType=debug',
       reversePorts: [
         8081
       ]
     },
     'example.android.release': {
       type: 'android.apk',
-      binaryPath: 'android/app/build/outputs/apk/release/app-release.apk',
-      build: 'cd android && ./gradlew assembleRelease assembleAndroidTest -DtestBuildType=release'
+      binaryPath: '../example/android/app/build/outputs/apk/release/app-release.apk',
+      build: 'cd ../example/android && ./gradlew assembleRelease assembleAndroidTest -DtestBuildType=release'
     }
   },
   devices: {
