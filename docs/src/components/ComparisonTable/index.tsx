e20c7886c5e5822851eeb3aa8a67a70b8fc9eb2b
Kirill Zyusko <zyusko.kirik@gmail.com>
2023-10-30 17:57:51 +0100

    docs: added `KeyboardAvoidingView` and `KeyboardStickyView` comparison (#262)

---
+++ docs/src/components/ComparisonTable/index.tsx
@@ -0,0 +1,46 @@
+import React, { CSSProperties } from 'react';
+import Lottie from 'lottie-react';
+
+const withoutBorders = { border: 'none' };
+const lottieView = { paddingLeft: '20%', paddingRight: '20%' };
+const label: CSSProperties = {
+  ...withoutBorders,
+  maxWidth: 400,
+  textAlign: 'center',
+};
+const labels = {
+  ...withoutBorders,
+  backgroundColor: '#00000000',
+};
+
+type Props = {
+  leftLottie: unknown;
+  leftText: React.ReactNode;
+  rightLottie: unknown;
+  rightText: React.ReactNode;
+};
+
+export default function ComparisonTable({ leftLottie, leftText, rightLottie, rightText }: Props): JSX.Element {
+  return (
+    <table>
+      <tbody>
+        <tr style={withoutBorders}>
+          <td style={withoutBorders}>
+            <Lottie animationData={leftLottie} style={lottieView} loop />
+          </td>
+          <td style={withoutBorders}>
+            <Lottie animationData={rightLottie} style={lottieView} loop />
+          </td>
+        </tr>
+        <tr style={labels}>
+          <td style={label}>
+            {leftText}
+          </td>
+          <td style={label}>
+            {rightText}
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  );
+}
--- docs/src/components/KeyboardAvoidingViewComparison/index.tsx
+++ docs/src/components/KeyboardAvoidingViewComparison/index.tsx
@@ -1,47 +1,25 @@
-import React, { CSSProperties } from 'react';
-import Lottie from 'lottie-react';
+import React from 'react';
 
 import before from './kav.lottie.json';
 import after from './kav-animated.lottie.json';
-
-const withoutBorders = { border: 'none' };
-const lottieView = { paddingLeft: '20%', paddingRight: '20%' };
-const label: CSSProperties = {
-  ...withoutBorders,
-  maxWidth: 400,
-  textAlign: 'center',
-};
-const labels = {
-  ...withoutBorders,
-  backgroundColor: '#00000000',
-};
+import ComparisonTable from '../ComparisonTable';
 
 export default function KeyboardAvoidingViewComparison(): JSX.Element {
   return (
-    <table>
-      <tbody>
-        <tr style={withoutBorders}>
-          <td style={withoutBorders}>
-            <Lottie animationData={before} style={lottieView} loop />
-          </td>
-          <td style={withoutBorders}>
-            <Lottie animationData={after} style={lottieView} loop />
-          </td>
-        </tr>
-        <tr style={labels}>
-          <td style={label}>
-            <i>
-              Default <code>react-native</code> implementation on Android
-            </i>
-          </td>
-          <td style={label}>
-            <i>
-              Implementation from <code>react-native-keyboard-controller</code>{' '}
-              with better animations
-            </i>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+    <ComparisonTable
+      leftLottie={before}
+      rightLottie={after}
+      leftText={
+        <i>
+          Default <code>react-native</code> implementation on Android
+        </i>
+      }
+      rightText={
+        <i>
+          Implementation from <code>react-native-keyboard-controller</code>{' '}
+          with better animations
+        </i>
+      }
+    />
   );
 }
