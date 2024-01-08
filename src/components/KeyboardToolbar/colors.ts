ecd03568378f27df6597e2bf1fc067457af61e60
kirillzyusko <zyusko.kirik@gmail.com>
2024-01-08 14:52:35 +0100

    fix: crash on Android

--- src/components/KeyboardToolbar/colors.ts
+++ src/components/KeyboardToolbar/colors.ts
@@ -1,14 +1,32 @@
-import { PlatformColor } from "react-native";
+import { Platform, PlatformColor } from "react-native";
 
 export const colors = {
   light: {
-    primary: PlatformColor("link", "#007aff"),
-    disabled: PlatformColor("systemGray4", "#d1d4d9"),
-    background: "#F8F8F8",
+    primary: Platform.select({
+      ios: PlatformColor("link"), // "#007aff"
+      android: "#1c1c1c",
+    }),
+    disabled: Platform.select({
+      ios: PlatformColor("systemGray4"), // "#d1d4d9"
+      android: "#d1d4d9",
+    }),
+    background: Platform.select({
+      ios: "#F8F8F8",
+      android: "#FCFCFC",
+    }),
   },
   dark: {
-    primary: PlatformColor("label", "#fafafa"),
-    disabled: PlatformColor("systemGray", "#929292"),
-    background: "#555756",
+    primary: Platform.select({
+      ios: PlatformColor("label"), // "#fafafa"
+      android: "#fafafa",
+    }),
+    disabled: Platform.select({
+      ios: PlatformColor("systemGray"), // "#929292"
+      android: "#929292",
+    }),
+    background: Platform.select({
+      ios: "#555756",
+      android: "#333333",
+    }),
   },
 };
