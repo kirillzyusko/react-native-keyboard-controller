f18e223e988ec0c7147c10ce2563c70ef0eaf61f
kirillzyusko <zyusko.kirik@gmail.com>
2024-01-14 15:54:06 +0100

    fix: update in `onEnd` only in instant transitions

--- src/components/KeyboardAvoidingView/hooks.ts
+++ src/components/KeyboardAvoidingView/hooks.ts
@@ -29,8 +29,14 @@ export const useKeyboardAnimation = () => {
 
         isClosed.value = e.height === 0;
 
-        progress.value = e.progress;
-        height.value = e.height;
+        // update value only if transition was instant
+        // otherwise update will happen in `onMove` handler
+        // this `if` condition is needed to handle `secureTextEntry`
+        // see https://github.com/kirillzyusko/react-native-keyboard-controller/issues/327
+        if (e.duration === 0) {
+          progress.value = e.progress;
+          height.value = e.height;
+        }
       },
     },
     [],
