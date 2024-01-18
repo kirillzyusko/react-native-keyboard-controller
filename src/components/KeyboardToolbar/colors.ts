e6a7bc704d9378c77e28275307df1495e55295cb
kirillzyusko <zyusko.kirik@gmail.com>
2024-01-18 12:02:27 +0100

    refactor: better color scheme match on Android

--- src/components/KeyboardToolbar/colors.ts
+++ src/components/KeyboardToolbar/colors.ts
@@ -4,15 +4,15 @@ export const colors = {
   light: {
     primary: Platform.select({
       ios: PlatformColor("link"), // "#007aff"
-      android: "#1c1c1c",
+      android: "#2c2c2c",
     }),
     disabled: Platform.select({
       ios: PlatformColor("systemGray4"), // "#d1d4d9"
-      android: "#d1d4d9",
+      android: "#B0BEC5",
     }),
     background: Platform.select({
       ios: "#F8F8F8",
-      android: "#FCFCFC",
+      android: "#ECEFF1",
     }),
   },
   dark: {
@@ -22,11 +22,11 @@ export const colors = {
     }),
     disabled: Platform.select({
       ios: PlatformColor("systemGray"), // "#929292"
-      android: "#929292",
+      android: "#707070",
     }),
     background: Platform.select({
       ios: "#555756",
-      android: "#333333",
+      android: "#2C2C2E",
     }),
   },
 };
