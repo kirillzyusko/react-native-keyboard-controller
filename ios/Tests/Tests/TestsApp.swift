5e8cd43aa8838a64daec27a69f21495cffbcd1f4
kirillzyusko <zyusko.kirik@gmail.com>
2024-02-22 12:10:00 +0100

    feat: iOS unit tests

---
+++ ios/Tests/Tests/TestsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestsApp.swift
+//  Tests
+//
+//  Created by Kiryl Ziusko on 21/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct TestsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
