43b9d11af7ace5af1d55916bb19faaa6d8b678d8
kirillzyusko <zyusko.kirik@gmail.com>
2024-03-05 08:50:23 +0100

    feat: working PoC

--- android/src/main/java/com/reactnativekeyboardcontroller/views/OverKeyboardViewGroup.kt
+++ android/src/main/java/com/reactnativekeyboardcontroller/views/OverKeyboardViewGroup.kt
@@ -1,8 +1,65 @@
 package com.reactnativekeyboardcontroller.views
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.graphics.Color
+import android.graphics.PixelFormat
+import android.view.Gravity
+import android.view.View
+import android.view.WindowManager
 import com.facebook.react.uimanager.ThemedReactContext
 import com.facebook.react.views.view.ReactViewGroup
 
+@SuppressLint("ViewConstructor")
 class OverKeyboardViewGroup(private val reactContext: ThemedReactContext) : ReactViewGroup(reactContext) {
+  private var windowManager: WindowManager = reactContext.getSystemService(Context.WINDOW_SERVICE) as WindowManager
+  private var view: View = View(reactContext).apply {
+    layoutParams = LayoutParams(
+      LayoutParams.MATCH_PARENT,
+      LayoutParams.MATCH_PARENT // Height in pixels
+    )
+    setBackgroundColor(Color.argb(128, 255, 0, 0))
+  }
+
   // PopupWindow
+  init {
+    /*val textView = TextView(reactContext).apply {
+      text = "This is a popup!"
+      layoutParams = ViewGroup.LayoutParams(
+        ViewGroup.LayoutParams.WRAP_CONTENT,
+        ViewGroup.LayoutParams.WRAP_CONTENT
+      )
+    }
+
+    val popup = PopupWindow(reactContext).apply {
+      contentView = textView
+      backgroundColor = Color.RED
+      width = ViewGroup.LayoutParams.WRAP_CONTENT
+      height = ViewGroup.LayoutParams.WRAP_CONTENT
+      isFocusable = true // Lets taps outside the popup also dismiss it
+    }
+
+    popup.showAtLocation(rootView, Gravity.CENTER, 0, 0)*/
+
+    val layoutParams = WindowManager.LayoutParams(
+      WindowManager.LayoutParams.MATCH_PARENT, // Width
+      WindowManager.LayoutParams.MATCH_PARENT, // Height in pixels
+      // This type ensures it floats over other application windows but under system windows
+      WindowManager.LayoutParams.TYPE_APPLICATION_PANEL,
+      // Ensures touches outside the view pass through to other windows
+      WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE or WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+      PixelFormat.TRANSLUCENT
+    ).apply {
+      gravity = Gravity.BOTTOM // Align it at the bottom of the screen
+    }
+
+    // Now add the view to the WindowManager
+    windowManager.addView(view, layoutParams)
+  }
+
+  override fun onDetachedFromWindow() {
+    super.onDetachedFromWindow()
+
+    windowManager.removeView(view)
+  }
 }
--- example/src/screens/Examples/OverKeyboardView/index.tsx
+++ example/src/screens/Examples/OverKeyboardView/index.tsx
@@ -1,6 +1,50 @@
-import React from "react";
-import { View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { Modal, TextInput, View } from "react-native";
+import {
+  KeyboardEvents,
+  OverKeyboardView,
+} from "react-native-keyboard-controller";
 
 export default function OverKeyboardViewExample() {
-  return <View />;
+  const [isShow, setShow] = useState(false);
+
+  // show
+  useEffect(() => {
+    const subscription = KeyboardEvents.addListener("keyboardDidShow", (e) => {
+      console.log(1, e);
+      setShow(true);
+    });
+
+    return subscription.remove;
+  }, []);
+
+  // hide
+  useEffect(() => {
+    const subscription = KeyboardEvents.addListener("keyboardDidHide", (e) => {
+      console.log(2, e);
+      setShow(false);
+    });
+
+    return subscription.remove;
+  }, []);
+
+  return (
+    <View>
+      <TextInput
+        style={{ backgroundColor: "yellow", width: 200, height: 50, alignSelf: 'center', marginTop: 50 }}
+      />
+      {isShow && (
+        <OverKeyboardView>
+          <View
+            style={{
+              backgroundColor: "yellow",
+              width: 50,
+              height: 50,
+              borderRadius: 25,
+            }}
+          />
+        </OverKeyboardView>
+      )}
+    </View>
+  );
 }
