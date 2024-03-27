08ac9ceb6a64ebd3fa17b2e0836ac69dc64942a6
kirillzyusko <zyusko.kirik@gmail.com>
2024-03-27 13:00:14 +0100

    chore: increase timeout

--- e2e/kit/jest.config.js
+++ e2e/kit/jest.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   preset: "ts-jest",
   testMatch: ["<rootDir>/**/*.e2e.ts"],
-  testTimeout: 120000,
+  testTimeout: 300000,
   maxWorkers: 1,
   globalSetup: "detox/runners/jest/globalSetup",
   globalTeardown: "detox/runners/jest/globalTeardown",
