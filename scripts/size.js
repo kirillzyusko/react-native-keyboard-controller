6fa54e78a174e2fadae8e9ab4cc69954faeb3de2
kirillzyusko <zyusko.kirik@gmail.com>
2024-04-16 22:02:06 +0100

    fix: revert unnecessary changes

--- scripts/size.js
+++ scripts/size.js
@@ -1,6 +1,5 @@
 const { exec } = require("child_process");
 
 exec("npm pack --json", function (error, stdout, stderr) {
-  console.log(1, stdout, error, stderr);
-  // console.log(JSON.parse(stdout)[0].size);
+  console.log(JSON.parse(stdout)[0].size);
 });
