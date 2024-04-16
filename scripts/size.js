9d79ae965e0e4822c006e8ee675f6983a9aabff8
kirillzyusko <zyusko.kirik@gmail.com>
2024-04-16 21:44:41 +0100

    chore: keep only work in current branch

--- scripts/size.js
+++ scripts/size.js
@@ -1,6 +1,6 @@
 const { exec } = require("child_process");
 
 exec("npm pack --json", function (error, stdout, stderr) {
-  console.log(stdout);
+  console.log(1, stdout, error, stderr);
   console.log(JSON.parse(stdout)[0].size);
 });
