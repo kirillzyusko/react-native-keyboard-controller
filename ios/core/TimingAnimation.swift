0a4251047089358bf31dc507a38e5a3be881ea93
kirillzyusko <zyusko.kirik@gmail.com>
2024-05-04 20:56:55 +0200

    feat: changes after rebase

--- ios/core/TimingAnimation.swift
+++ ios/core/TimingAnimation.swift
@@ -9,51 +9,26 @@ import Foundation
 import QuartzCore
 
 public class TimingAnimation : KeyboardAnimation {
-  private weak var animation: CABasicAnimation?
-
     private let p0: CGPoint
     private let p1: CGPoint
     private let p2: CGPoint
     private let p3: CGPoint
-  private let fromValue: Double
-  private let toValue: Double
-  private let speed: Float
-  private let timestamp: CFTimeInterval
+    
+    init(animation: CABasicAnimation, fromValue: Double, toValue: Double) {
+        let timingFunction = animation.timingFunction
+        var controlPoints: [Float] = [0, 0, 0, 0]
+        timingFunction?.getControlPoint(at: 1, values: &controlPoints[0])
+        timingFunction?.getControlPoint(at: 2, values: &controlPoints[2])
+        let p1 = CGPoint(x: CGFloat(controlPoints[0]), y: CGFloat(controlPoints[1]))
+        let p2 = CGPoint(x: CGFloat(controlPoints[2]), y: CGFloat(controlPoints[3]))
+        
+        self.p0 = CGPoint(x: 0.0, y: 0.0)
+        self.p1 = p1
+        self.p2 = p2
+        self.p3 = CGPoint(x: 1.0, y: 1.0)
 
-  init(
-    p1: CGPoint,
-    p2: CGPoint,
-    speed: Float,
-    fromValue: Double,
-    toValue: Double
-  ) {
-      self.p0 = CGPoint(x: 0.0, y: 0.0)
-      self.p1 = p1
-      self.p2 = p2
-      self.p3 = CGPoint(x: 1.0, y: 1.0)
-    self.speed = speed
-    self.fromValue = fromValue
-    self.toValue = toValue
-    timestamp = CACurrentMediaTime()
-      print("\(self.toValue) \(self.p1) \(self.p2)")
-  }
-
-  convenience init(animation: CABasicAnimation, fromValue: Double, toValue: Double) {
-      let timingFunction = animation.timingFunction
-      var controlPoints: [Float] = [0, 0, 0, 0]
-      timingFunction?.getControlPoint(at: 1, values: &controlPoints[0])
-      timingFunction?.getControlPoint(at: 2, values: &controlPoints[2])
-      let p1 = CGPoint(x: CGFloat(controlPoints[0]), y: CGFloat(controlPoints[1]))
-      let p2 = CGPoint(x: CGFloat(controlPoints[2]), y: CGFloat(controlPoints[3]))
-    self.init(
-      p1: p1,
-      p2: p2,
-      speed: (animation as CABasicAnimation).speed,
-      fromValue: fromValue,
-      toValue: toValue
-    )
-    self.animation = animation
-  }
+      super.init(fromValue: fromValue, toValue: toValue, animation: animation)
+    }
 
     func bezier(t: CGFloat) -> CGFloat {
         let u = 1 - t
@@ -70,22 +45,8 @@ public class TimingAnimation : KeyboardAnimation {
         return term0 + term1 + term2 + term3
     }
 
-  // public getters
-  var startTime: CFTimeInterval {
-    // when concurrent animation happens, then `.beginTime` remains the same
-    return max(animation?.beginTime ?? timestamp, timestamp)
-  }
-
-  var diff: Double {
-    return ((animation?.beginTime ?? timestamp) - timestamp).truncatingRemainder(dividingBy: UIUtils.nextFrame)
-  }
-
-  var isIncreasing: Bool {
-    return fromValue < toValue
-  }
-
   // public functions
-  func valueAt(time: Double) -> Double {
+  override func valueAt(time: Double) -> Double {
       let x = time * Double(speed)
       let frames = ((animation?.duration ?? 0.0)) * Double(speed)
       let fraction = min((x / frames), 1)
@@ -139,7 +100,8 @@ public class TimingAnimation : KeyboardAnimation {
     }
 
 
-  func timingAt(value y: Double) -> Double {
+  // temporarily override it because of valueAt2 usage
+  override func timingAt(value y: Double) -> Double {
     var lowerBound = 0.0
     var upperBound = 1.0 // Assuming 1 second is the max duration for simplicity
     let tolerance = 0.00001 // Define how precise you want to be
