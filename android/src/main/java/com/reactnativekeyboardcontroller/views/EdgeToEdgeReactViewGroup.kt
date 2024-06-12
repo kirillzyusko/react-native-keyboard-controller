d1a29efbc12356c85f6a206f6d3f957c2bf90d27
kirillzyusko <zyusko.kirik@gmail.com>
2024-06-12 17:56:08 +0200

    fix: ktlint

--- android/src/main/java/com/reactnativekeyboardcontroller/views/EdgeToEdgeReactViewGroup.kt
+++ android/src/main/java/com/reactnativekeyboardcontroller/views/EdgeToEdgeReactViewGroup.kt
@@ -121,7 +121,7 @@ class EdgeToEdgeReactViewGroup(private val reactContext: ThemedReactContext) : R
         deferredInsetTypes = WindowInsetsCompat.Type.ime(),
         dispatchMode = WindowInsetsAnimationCompat.Callback.DISPATCH_MODE_CONTINUE_ON_SUBTREE,
         context = reactContext,
-        hasTranslucentNavigationBar = isNavigationBarTranslucent
+        hasTranslucentNavigationBar = isNavigationBarTranslucent,
       )
 
       eventView?.let {
