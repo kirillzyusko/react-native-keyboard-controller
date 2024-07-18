63c4a724bc9f12aa6bbcd8dcfb7556dc5772c3d0
kirillzyusko <zyusko.kirik@gmail.com>
2024-07-18 19:00:29 +0200

    fix: swiftlint, swiftformat

--- ios/extensions/UIApplication.swift
+++ ios/extensions/UIApplication.swift
@@ -9,19 +9,21 @@ import Foundation
 import UIKit
 
 public extension UIApplication {
-    static func getTopViewController(base: UIViewController? = UIApplication.shared.keyWindow?.rootViewController) -> UIViewController? {
-        if let nav = base as? UINavigationController {
-            return getTopViewController(base: nav.visibleViewController)
-        }
-        
-        if let tab = base as? UITabBarController, let selected = tab.selectedViewController {
-            return getTopViewController(base: selected)
-        }
-        
-        if let presented = base?.presentedViewController {
-            return getTopViewController(base: presented)
-        }
-        
-        return base
+  static func getTopViewController(
+    base: UIViewController? = UIApplication.shared.keyWindow?.rootViewController
+  ) -> UIViewController? {
+    if let nav = base as? UINavigationController {
+      return getTopViewController(base: nav.visibleViewController)
     }
+
+    if let tab = base as? UITabBarController, let selected = tab.selectedViewController {
+      return getTopViewController(base: selected)
+    }
+
+    if let presented = base?.presentedViewController {
+      return getTopViewController(base: presented)
+    }
+
+    return base
+  }
 }
