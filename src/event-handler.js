a922738048f76da611e7f83a6e5daad4b43a393b
kirillzyusko <zyusko.kirik@gmail.com>
2024-08-07 15:14:36 +0200

    fix: compatibility with REA monorepo

--- src/event-handler.js
+++ src/event-handler.js
@@ -2,8 +2,12 @@ let REACore = null;
 
 try {
   REACore = require("react-native-reanimated/src/core");
-} catch (e) {
-  REACore = require("react-native-reanimated/src/reanimated2/core");
+} catch (e1) {
+  try {
+    REACore = require("react-native-reanimated/src/reanimated2/core");
+  } catch (e2) {
+    console.warn("Failed to load REACore from both paths");
+  }
 }
 const registerEventHandler = REACore.registerEventHandler;
 const unregisterEventHandler = REACore.unregisterEventHandler;
