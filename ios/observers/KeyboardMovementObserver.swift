3b53652753ff1503abf90c1d6a84cd03865aa5b5
kirillzyusko <zyusko.kirik@gmail.com>
2024-08-08 11:31:35 +0200

    fix: read an actual keyboard frame in `didAppear` event

--- ios/observers/KeyboardMovementObserver.swift
+++ ios/observers/KeyboardMovementObserver.swift
@@ -210,6 +210,7 @@ public class KeyboardMovementObserver: NSObject {
 
   @objc func keyboardDidAppear(_ notification: Notification) {
     if let keyboardFrame: NSValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue {
+      let (position, _) = keyboardView.framePositionInWindow
       let keyboardHeight = keyboardFrame.cgRectValue.size.height
       tag = UIResponder.current.reactViewTag
       self.keyboardHeight = keyboardHeight
@@ -218,13 +219,13 @@ public class KeyboardMovementObserver: NSObject {
       )
 
       var data = [AnyHashable: Any]()
-      data["height"] = keyboardHeight
+      data["height"] = position
       data["duration"] = duration
       data["timestamp"] = Date.currentTimeStamp
       data["target"] = tag
 
       onCancelAnimation()
-      onEvent("onKeyboardMoveEnd", keyboardHeight as NSNumber, 1, duration as NSNumber, tag)
+      onEvent("onKeyboardMoveEnd", position as NSNumber, (position / self.keyboardHeight) as NSNumber, duration as NSNumber, tag)
       onNotify("KeyboardController::keyboardDidShow", data)
 
       removeKeyboardWatcher()
@@ -285,9 +286,8 @@ public class KeyboardMovementObserver: NSObject {
       return
     }
 
-    let keyboardFrameY = keyboardView?.layer.presentation()?.frame.origin.y ?? 0
-    let keyboardWindowH = keyboardView?.window?.bounds.size.height ?? 0
-    var keyboardPosition = keyboardWindowH - keyboardFrameY
+    let (visibleKeyboardHeight, keyboardFrameY) = keyboardView.framePositionInWindow
+    var keyboardPosition = visibleKeyboardHeight
 
     if keyboardPosition == prevKeyboardPosition || keyboardFrameY == 0 {
       return
--- src/components/KeyboardAvoidingView/hooks.ts
+++ src/components/KeyboardAvoidingView/hooks.ts
@@ -33,20 +33,6 @@ export const useKeyboardAnimation = () => {
         "worklet";
 
         isClosed.value = e.height === 0;
-
-        // `height` update happens in `onMove` handler
-        // in `onEnd` we need to update only if `onMove`
-        // wasn't called (i. e. duration === 0)
-        //
-        // we can not call this code without condition below
-        // because in some corner cases (iOS with `secureTextEntry`)
-        // `onEnd` can be emitted before `onMove` and in this case
-        // it may lead to choppy/glitchy/jumpy UI
-        // see https://github.com/kirillzyusko/react-native-keyboard-controller/issues/327
-        if (e.duration === 0 || e.target === -1) {
-          progress.value = e.progress;
-          height.value = e.height;
-        }
       },
     },
     [],
