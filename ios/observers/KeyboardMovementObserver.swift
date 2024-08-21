kirillzyusko/react-native-keyboard-controller

commit 6861faf60e21167458cb2306e74a527caded3731

fix: instant keyboard hide causes `KeyboardAvoidingView` keeping bottom space (#539)
## 📜 Description

Fixed a case when `KeyboardAvoidingView` is not getting resized back
when keyboard is hidden instantly.

## 💡 Motivation and Context

The condition:

```tsx
if (e.duration === 0) {
  progress.value = e.progress;
  height.value = e.height;
}
```

prevented view to restore its initial position when keyboard is hidden
instantly. When such thing happens we have next events:

```bash
 LOG  onStart {"duration": 250, "eventName": "onKeyboardMoveStart", "height": 0, "progress": 0, "target": 7054} 1723043957135
 LOG  onEnd {"duration": 250, "eventName": "onKeyboardMoveEnd", "height": 0, "progress": 0, "target": -1} 1723043957142
```

So technically we had to set our `height`/`progress` to `0`. But we skip
it, because `event` actually has non-zero duration (it's 250ms).

We can't simply remove the code that I added before because it'll bring
old bug back, so I was thinking a lot on how to resolve the problem.

### 1️⃣ Add more conditions

The first idea was to modify condition to be like

```tsx
if (e.duration === 0 || e.target === -1) {}
```

However I don't like it because:
- it adds more complexity to the code;
- we have to remember about that case and use this condition in every
hook;
- this code doesn't feel cross-platform;
- I'm not sure if it's a correct fix, because we may have a new
situation, where this condition will lead to unexpected results;

So I started to think about other approaches.

### 2️⃣ Native code modification

At some of point of time I realized, that we expect `onEnd` to report an
actual keyboard height that is currently visible on the screen (at least
in `keyboardDidAppear` method). And if we rely on this fact then we can
remove the condition from `onEnd` handler.

So I decided to try to re-work the code and instead of re-forwarding the

ios/observers/KeyboardMovementObserver.swift
@@ -210,21 +210,24 @@ public class KeyboardMovementObserver: NSObject {
 
   @objc func keyboardDidAppear(_ notification: Notification) {
     if let keyboardFrame: NSValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue {
+      let (position, _) = keyboardView.framePositionInWindow
       let keyboardHeight = keyboardFrame.cgRectValue.size.height
       tag = UIResponder.current.reactViewTag
       self.keyboardHeight = keyboardHeight
       let duration = Int(
         (notification.userInfo?[UIResponder.keyboardAnimationDurationUserInfoKey] as? Double ?? 0) * 1000
       )
+      // always limit progress to the maximum possible value
+      let progress = min(position / self.keyboardHeight, 1.0)
 
       var data = [AnyHashable: Any]()
-      data["height"] = keyboardHeight
+      data["height"] = position
       data["duration"] = duration
       data["timestamp"] = Date.currentTimeStamp
       data["target"] = tag
 
       onCancelAnimation()
-      onEvent("onKeyboardMoveEnd", keyboardHeight as NSNumber, 1, duration as NSNumber, tag)
+      onEvent("onKeyboardMoveEnd", position as NSNumber, progress as NSNumber, duration as NSNumber, tag)
       onNotify("KeyboardController::keyboardDidShow", data)
 
       removeKeyboardWatcher()
@@ -285,9 +288,8 @@ public class KeyboardMovementObserver: NSObject {
       return
     }
 
-    let keyboardFrameY = keyboardView?.layer.presentation()?.frame.origin.y ?? 0
-    let keyboardWindowH = keyboardView?.window?.bounds.size.height ?? 0
-    var keyboardPosition = keyboardWindowH - keyboardFrameY
+    let (visibleKeyboardHeight, keyboardFrameY) = keyboardView.framePositionInWindow
+    var keyboardPosition = visibleKeyboardHeight
 
     if keyboardPosition == prevKeyboardPosition || keyboardFrameY == 0 {
       return

ios/extensions/UIView.swift
@@ -15,3 +15,13 @@ public extension UIView {
     return superview?.convert(frame, to: rootView)
   }
 }
+
+public extension Optional where Wrapped == UIView {
+  var framePositionInWindow: (Double, Double) {
+    let frameY = self?.layer.presentation()?.frame.origin.y ?? 0
+    let windowH = self?.window?.bounds.size.height ?? 0
+    let position = windowH - frameY
+
+    return (position, frameY)
+  }
+}

src/components/KeyboardAvoidingView/hooks.ts
@@ -34,19 +34,8 @@ export const useKeyboardAnimation = () => {
 
         isClosed.value = e.height === 0;
 
-        // `height` update happens in `onMove` handler
-        // in `onEnd` we need to update only if `onMove`
-        // wasn't called (i. e. duration === 0)
-        //
-        // we can not call this code without condition below
-        // because in some corner cases (iOS with `secureTextEntry`)
-        // `onEnd` can be emitted before `onMove` and in this case
-        // it may lead to choppy/glitchy/jumpy UI
-        // see https://github.com/kirillzyusko/react-native-keyboard-controller/issues/327
-        if (e.duration === 0) {
-          progress.value = e.progress;
-          height.value = e.height;
-        }
+        height.value = e.height;
+        progress.value = e.progress;
       },
     },
     [],

commit 9b5510d694c2119be6ec86a706aadf2e726bb017

fix: properly report height for `didShow` event happening during animation (#557)
## 📜 Description

Fixed a case when `onEnd`/`keyboardDidShow` reports incorrect (previous
frame) keyboard height.

## 💡 Motivation and Context

Fixes regressions introduced in
https://github.com/kirillzyusko/react-native-keyboard-controller/pull/539

Initially I thought it's safe to read current keyboard frame in
`didAppear` lifecycle (I thought it'll be always dispatched after
animation finish, so why not to read it). But it was a fatal mistake
because:
- when keyboard changes its size it changes it immediately and in
`didAppear` we'll read old (previous) value ⛔
- on iOS 15 when modal gets hidden, then keyboard appears instantly and
iOS 15 dispatches only one event, so we'll read old frame (i. e. `0`); 🔴
- when iOS 17 with `secureTextEntry` randomly attaches/detaches
`inputAccessoryView`, then we may also encounter a situation, when we
read old frame and we will avoid keyboard incorrectly ❌

So to sum up all previous approaches:
- relying on `duration === 0` is not reliable because keyboard can be
hidden immediately but with `duration === 250` 🤯
- reading current frame in `keyboardDidShow` is not correct approach
because in some situations we may read old values 😔

This PR is a third revision of the solution 😂 Now I learned all lessons
(at least I hope so) and invented a better mechanism. Let's go back to
original problem.

The original problem was in the fact, that a resize event can be
dispatched during animation AND it'll contain incorrect information
about keyboard size (it'll have final destination, though animation
hasn't finished yet). I decided to catch that situation in the code - I
added `didShowDeadline` variable and in `keyboardWillAppear` I set it as
`timestamp + duration`. In `keyboardDidAppear` I verify, that this event
arrived later, and if so, then we read `keyboardHeight` value from

ios/observers/KeyboardMovementObserver.swift
@@ -40,6 +40,7 @@ public class KeyboardMovementObserver: NSObject {
   private var duration = 0
   private var tag: NSNumber = -1
   private var animation: KeyboardAnimation?
+  private var didShowDeadline: Int64 = 0
 
   @objc public init(
     handler: @escaping (NSString, NSNumber, NSNumber, NSNumber, NSNumber) -> Void,
@@ -168,6 +169,7 @@ public class KeyboardMovementObserver: NSObject {
       let keyboardHeight = keyboardFrame.cgRectValue.size.height
       self.keyboardHeight = keyboardHeight
       self.duration = duration
+      didShowDeadline = Date.currentTimeStamp + Int64(duration)
 
       onRequestAnimation()
       onEvent("onKeyboardMoveStart", Float(keyboardHeight) as NSNumber, 1, duration as NSNumber, tag)
@@ -193,18 +195,22 @@ public class KeyboardMovementObserver: NSObject {
   }
 
   @objc func keyboardDidAppear(_ notification: Notification) {
+    let timestamp = Date.currentTimeStamp
     let (duration, frame) = metaDataFromNotification(notification)
     if let keyboardFrame = frame {
       let (position, _) = keyboardView.framePositionInWindow
       let keyboardHeight = keyboardFrame.cgRectValue.size.height
       tag = UIResponder.current.reactViewTag
       self.keyboardHeight = keyboardHeight
+      // if the event is caught in between it's highly likely that it could be a "resize" event
+      // so we just read actual keyboard frame value in this case
+      let height = timestamp >= didShowDeadline ? keyboardHeight : position
       // always limit progress to the maximum possible value
-      let progress = min(position / self.keyboardHeight, 1.0)
+      let progress = min(height / self.keyboardHeight, 1.0)
 
       onCancelAnimation()
-      onEvent("onKeyboardMoveEnd", position as NSNumber, progress as NSNumber, duration as NSNumber, tag)
-      onNotify("KeyboardController::keyboardDidShow", getEventParams(position, duration))
+      onEvent("onKeyboardMoveEnd", height as NSNumber, progress as NSNumber, duration as NSNumber, tag)
+      onNotify("KeyboardController::keyboardDidShow", getEventParams(height, duration))
 
       removeKeyboardWatcher()
       setupKVObserver()