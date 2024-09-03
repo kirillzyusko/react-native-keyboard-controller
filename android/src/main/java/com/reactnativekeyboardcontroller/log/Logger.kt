1e396293e27b83f3af0eb081745eec339cf558b1
kirillzyusko <zyusko.kirik@gmail.com>
2024-09-03 10:43:21 +0200

    fix: revert warnings commit, since these warning are applicable only to release variant but we want to assemble both build types

--- android/src/main/java/com/reactnativekeyboardcontroller/log/Logger.kt
+++ android/src/main/java/com/reactnativekeyboardcontroller/log/Logger.kt
@@ -4,16 +4,16 @@ import android.util.Log
 import com.reactnativekeyboardcontroller.BuildConfig
 
 object Logger {
-  private const val IS_ENABLED = BuildConfig.DEBUG
+  private val enabled = BuildConfig.DEBUG
 
   fun i(tag: String?, message: String, throwable: Throwable? = null) {
-    if (IS_ENABLED) {
+    if (enabled) {
       Log.i(tag, message, throwable)
     }
   }
 
   fun w(tag: String?, message: String, throwable: Throwable? = null) {
-    if (IS_ENABLED) {
+    if (enabled) {
       Log.w(tag, message, throwable)
     }
   }
