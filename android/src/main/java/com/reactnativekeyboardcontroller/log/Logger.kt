53fa210014b59c15f227f80fa65ca295716bf41d
kirillzyusko <zyusko.kirik@gmail.com>
2024-09-03 10:35:39 +0200

    fix: no warnings in Android studio

--- android/src/main/java/com/reactnativekeyboardcontroller/log/Logger.kt
+++ android/src/main/java/com/reactnativekeyboardcontroller/log/Logger.kt
@@ -4,16 +4,16 @@ import android.util.Log
 import com.reactnativekeyboardcontroller.BuildConfig
 
 object Logger {
-  private val enabled = BuildConfig.DEBUG
+  private const val IS_ENABLED = BuildConfig.DEBUG
 
   fun i(tag: String?, message: String, throwable: Throwable? = null) {
-    if (enabled) {
+    if (IS_ENABLED) {
       Log.i(tag, message, throwable)
     }
   }
 
   fun w(tag: String?, message: String, throwable: Throwable? = null) {
-    if (enabled) {
+    if (IS_ENABLED) {
       Log.w(tag, message, throwable)
     }
   }
