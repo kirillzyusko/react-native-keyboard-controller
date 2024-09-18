b2289f90fc0b0c0d2c0e7388fb036cc1baba276e
kirillzyusko <zyusko.kirik@gmail.com>
2024-09-18 16:34:17 +0200

    feat: iOS fabric support

--- ios/views/OverKeyboardViewManager.h
+++ ios/views/OverKeyboardViewManager.h
@@ -5,17 +5,25 @@
 //  Created by Kiryl Ziusko on 17.09.24.
 //  Copyright © 2024 Facebook. All rights reserved.
 //
-
+#ifdef RCT_NEW_ARCH_ENABLED
+#import <React/RCTViewComponentView.h>
+#else
 #import <React/RCTBridge.h>
+#endif
 #import <React/RCTViewManager.h>
 #import <UIKit/UIKit.h>
 
 @interface OverKeyboardViewManager : RCTViewManager
 @end
 
-@interface OverKeyboardView : UIView
+@interface OverKeyboardView :
+#ifdef RCT_NEW_ARCH_ENABLED
+    RCTViewComponentView
+#else
+    UIView
 
-- (instancetype)initWithFrame:(CGRect)frame bridge:(RCTBridge *)bridge;
+- (instancetype)initWithBridge:(RCTBridge *)bridge;
+
+#endif
 @property (nonatomic, assign) BOOL visible;
-
 @end
--- ios/views/OverKeyboardViewManager.mm
+++ ios/views/OverKeyboardViewManager.mm
@@ -13,9 +13,25 @@
 #import <react_native_keyboard_controller/react_native_keyboard_controller-Swift.h>
 #endif
 
+#ifdef RCT_NEW_ARCH_ENABLED
+#import <React/RCTSurfaceTouchHandler.h>
+
+#import <react/renderer/components/reactnativekeyboardcontroller/ComponentDescriptors.h>
+#import <react/renderer/components/reactnativekeyboardcontroller/EventEmitters.h>
+#import <react/renderer/components/reactnativekeyboardcontroller/Props.h>
+#import <react/renderer/components/reactnativekeyboardcontroller/RCTComponentViewHelpers.h>
+
+#import "RCTFabricComponentsPlugins.h"
+#endif
+
 #import <React/RCTTouchHandler.h>
 #import <UIKit/UIKit.h>
 
+#ifdef RCT_NEW_ARCH_ENABLED
+using namespace facebook::react;
+#endif
+
+// MARK: Manager
 @implementation OverKeyboardViewManager
 
 RCT_EXPORT_MODULE(OverKeyboardViewManager)
@@ -27,44 +43,116 @@ RCT_EXPORT_VIEW_PROPERTY(visible, BOOL)
   return NO;
 }
 
+#ifndef RCT_NEW_ARCH_ENABLED
 - (UIView *)view {
-  return [[OverKeyboardView alloc] initWithFrame:CGRectZero bridge:self.bridge];
+  return [[OverKeyboardView alloc] initWithBridge:self.bridge];
 }
+#endif
 
 @end
 
+// MARK: View
+#ifdef RCT_NEW_ARCH_ENABLED
+@interface OverKeyboardView () <RCTOverKeyboardViewViewProtocol>
+@end
+#endif
+
 @implementation OverKeyboardView {
     UIView *_contentView;
+#ifdef RCT_NEW_ARCH_ENABLED
+  RCTSurfaceTouchHandler *_touchHandler;
+#else
     RCTTouchHandler *_touchHandler;
+#endif
 }
 
-- (instancetype)initWithFrame:(CGRect)frame bridge:(RCTBridge *)bridge {
-    self = [super initWithFrame:frame];
+#ifdef RCT_NEW_ARCH_ENABLED
++ (ComponentDescriptorProvider)componentDescriptorProvider
+{
+  return concreteComponentDescriptorProvider<OverKeyboardViewComponentDescriptor>();
+}
+#endif
+
+// Needed because of this: https://github.com/facebook/react-native/pull/37274
++ (void)load
+{
+  [super load];
+  
+  // Eagerly initialize the observer
+  [UIWindow sharedKeyboardWindowObserver];
+}
+
+// MARK: Constructor
+#ifdef RCT_NEW_ARCH_ENABLED
+- (instancetype)init {
+    NSLog(@"123");
+    if (self = [super init]) {
+        _touchHandler = [RCTSurfaceTouchHandler new];
+        _contentView = [[UIView alloc] initWithFrame:CGRectZero];
+      }
+      return self;
+}
+#else
+- (instancetype)initWithBridge:(RCTBridge *)bridge {
+    self = [super initWithFrame:CGRectZero];
     if (self) {
         _contentView = [[UIView alloc] initWithFrame:CGRectZero];
-        
-        // Eagerly initialize the observer
-        [UIWindow sharedKeyboardWindowObserver];
         
         _touchHandler = [[RCTTouchHandler alloc] initWithBridge:bridge];
     }
     return self;
 }
+#endif
 
+#ifdef RCT_NEW_ARCH_ENABLED
+- (void)updateProps:(Props::Shared const &)props oldProps:(Props::Shared const &)oldProps
+{
+  const auto &oldViewProps = *std::static_pointer_cast<const OverKeyboardViewProps>(_props);
+  const auto &newViewProps = *std::static_pointer_cast<const OverKeyboardViewProps>(props);
+
+  if (newViewProps.visible != oldViewProps.visible) {
+    if (newViewProps.visible) {
+      [self show];
+    } else {
+      [self hide];
+    }
+  }
+
+  [super updateProps:props oldProps:oldProps];
+}
+#endif
+
+#ifndef RCT_NEW_ARCH_ENABLED
 - (void)setVisible:(BOOL)visible {
-    _visible = visible;
-    if (_visible) {
-        [self showInLastWindow];
+    if (visible) {
+        [self show];
     } else {
         [self hide];
     }
 }
+#endif
 
 - (void)addSubview:(UIView *)view {
     [_contentView addSubview:view];
 }
 
-- (void)showInLastWindow {
+#ifdef RCT_NEW_ARCH_ENABLED
+- (void)mountChildComponentView:(UIView<RCTComponentViewProtocol> *)childComponentView index:(NSInteger)index
+{
+    [_contentView insertSubview:childComponentView atIndex:index];
+}
+
+- (void)unmountChildComponentView:(UIView<RCTComponentViewProtocol> *)childComponentView index:(NSInteger)index
+{
+    [childComponentView removeFromSuperview];
+}
+#endif
+
+- (void)show {
+    if (_visible) {
+        return;
+    }
+    _visible = true;
     _contentView.frame = self.window.bounds;
     _contentView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
     UIWindow *topWindow = [UIWindow topWindow];
@@ -74,10 +162,19 @@ RCT_EXPORT_VIEW_PROPERTY(visible, BOOL)
 }
 
 - (void)hide {
-    if (_contentView.subviews.count > 0) {
-        [_contentView removeFromSuperview];
-        [_touchHandler detachFromView:_contentView];
+    if (!_visible) {
+        return;
     }
+    _visible = false;
+    [_contentView removeFromSuperview];
+    [_touchHandler detachFromView:_contentView];
 }
+
+#ifdef RCT_NEW_ARCH_ENABLED
+Class<RCTComponentViewProtocol> OverKeyboardViewCls(void)
+{
+  return OverKeyboardView.class;
+}
+#endif
 
 @end
