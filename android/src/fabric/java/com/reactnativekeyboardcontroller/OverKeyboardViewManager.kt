911b765547fa1f69a3696b3e5ea08eef33225849
kirillzyusko <zyusko.kirik@gmail.com>
2024-09-30 15:02:53 +0200

    refactor: `OverKeyboardView` (variable names, manager interfaces)

--- android/src/fabric/java/com/reactnativekeyboardcontroller/OverKeyboardViewManager.kt
+++ android/src/fabric/java/com/reactnativekeyboardcontroller/OverKeyboardViewManager.kt
@@ -3,24 +3,23 @@ package com.reactnativekeyboardcontroller
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.uimanager.LayoutShadowNode
 import com.facebook.react.uimanager.ThemedReactContext
+import com.facebook.react.uimanager.ViewGroupManager
 import com.facebook.react.uimanager.ViewManagerDelegate
 import com.facebook.react.uimanager.annotations.ReactProp
 import com.facebook.react.viewmanagers.OverKeyboardViewManagerDelegate
 import com.facebook.react.viewmanagers.OverKeyboardViewManagerInterface
-import com.facebook.react.views.view.ReactViewGroup
-import com.facebook.react.views.view.ReactViewManager
 import com.reactnativekeyboardcontroller.managers.OverKeyboardViewManagerImpl
 import com.reactnativekeyboardcontroller.views.overlay.OverKeyboardHostShadowNode
 import com.reactnativekeyboardcontroller.views.overlay.OverKeyboardHostView
 
 class OverKeyboardViewManager(
   mReactContext: ReactApplicationContext,
-) : ReactViewManager(),
-  OverKeyboardViewManagerInterface<ReactViewGroup> {
+) : ViewGroupManager<OverKeyboardHostView>(),
+  OverKeyboardViewManagerInterface<OverKeyboardHostView> {
   private val manager = OverKeyboardViewManagerImpl(mReactContext)
   private val mDelegate = OverKeyboardViewManagerDelegate(this)
 
-  override fun getDelegate(): ViewManagerDelegate<ReactViewGroup?> = mDelegate
+  override fun getDelegate(): ViewManagerDelegate<OverKeyboardHostView?> = mDelegate
 
   override fun getName(): String = OverKeyboardViewManagerImpl.NAME
 
@@ -33,9 +32,9 @@ class OverKeyboardViewManager(
 
   @ReactProp(name = "visible")
   override fun setVisible(
-    view: ReactViewGroup?,
+    view: OverKeyboardHostView,
     value: Boolean,
   ) {
-    manager.setVisible(view as OverKeyboardHostView, value)
+    manager.setVisible(view, value)
   }
 }
