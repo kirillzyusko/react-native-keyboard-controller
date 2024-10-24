82310911963e33420107e1900f4cc27a0b430b1d
kirillzyusko <zyusko.kirik@gmail.com>
2024-10-24 12:33:41 +0200

    fix: android

--- FabricExample/android/app/src/main/java/com/keyboardcontrollerfabricexample/MainApplication.kt
+++ FabricExample/android/app/src/main/java/com/keyboardcontrollerfabricexample/MainApplication.kt
@@ -10,7 +10,6 @@ import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
 import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.react.soloader.OpenSourceMergedSoMapping
-import com.facebook.react.flipper.ReactNativeFlipper
 import com.facebook.soloader.SoLoader
 
 class MainApplication : Application(), ReactApplication {
@@ -40,7 +39,6 @@ class MainApplication : Application(), ReactApplication {
       // If you opted-in for the New Architecture, we load the native entry point for this app.
       load()
     }
-    ReactNativeFlipper.initializeFlipper(this, reactNativeHost.reactInstanceManager)
   }
 }
 
