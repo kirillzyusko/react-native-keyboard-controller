1593191ffc5ec76c86beb5a32cb5dfb159483f48
kirillzyusko <zyusko.kirik@gmail.com>
2024-11-11 17:43:28 +0100

    refactor: move data extraction from notification to extensions layer

---
+++ ios/extensions/Notification.swift
@@ -0,0 +1,17 @@
+//
+//  Notification.swift
+//  Pods
+//
+//  Created by Kiryl Ziusko on 04/11/2024.
+//
+
+extension Notification {
+  func keyboardMetaData() -> (Int, NSValue?) {
+    let duration = Int(
+      (userInfo?[UIResponder.keyboardAnimationDurationUserInfoKey] as? Double ?? 0) * 1000
+    )
+    let keyboardFrame = userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue
+
+    return (duration, keyboardFrame)
+  }
+}
