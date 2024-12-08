886b99eed3c10514d853cb926ab4798abd71ef89
kirillzyusko <zyusko.kirik@gmail.com>
2024-12-08 23:21:28 +0100

    fix: swiftformat

--- ios/extensions/UIKeyboardType.swift
+++ ios/extensions/UIKeyboardType.swift
@@ -9,20 +9,20 @@ import Foundation
 import UIKit
 
 extension UIKeyboardType {
-    private static let keyboardTypeToStringMapping: [UIKeyboardType: String] = [
-        .default: "default",
-        .asciiCapable: "ascii-capable",
-        .numbersAndPunctuation: "numbers-and-punctuation",
-        .URL: "url",
-        .numberPad: "number-pad",
-        .phonePad: "phone-pad",
-        .namePhonePad: "name-phone-pad",
-        .emailAddress: "email-address",
-        .decimalPad: "decimal-pad",
-        .twitter: "twitter",
-        .webSearch: "web-search",
-        .asciiCapableNumberPad: "ascii-capable-number-pad",
-    ]
+  private static let keyboardTypeToStringMapping: [UIKeyboardType: String] = [
+    .default: "default",
+    .asciiCapable: "ascii-capable",
+    .numbersAndPunctuation: "numbers-and-punctuation",
+    .URL: "url",
+    .numberPad: "number-pad",
+    .phonePad: "phone-pad",
+    .namePhonePad: "name-phone-pad",
+    .emailAddress: "email-address",
+    .decimalPad: "decimal-pad",
+    .twitter: "twitter",
+    .webSearch: "web-search",
+    .asciiCapableNumberPad: "ascii-capable-number-pad",
+  ]
 
   var name: String {
     return UIKeyboardType.keyboardTypeToStringMapping[self] ?? "default"
