049435dedae546fb81fdce91a3d8c7934c17595b
kirillzyusko <zyusko.kirik@gmail.com>
2024-12-11 17:29:17 +0100

    fix: keep focus in offset map to prevent a case, when key/offset pair gets removed when old screen gets unmounted after new screen mount (iOS 15 case)

--- ios/interactive/KeyboardOffsetProvider.swift
+++ ios/interactive/KeyboardOffsetProvider.swift
@@ -13,16 +13,16 @@ public class KeyboardOffsetProvider: NSObject {
 
   override private init() {}
 
-  private var offsetMap: [String: NSNumber] = [:]
+  private var offsetMap: [String: (offset: NSNumber, tag: NSNumber)] = [:]
 
-  @objc public func setOffset(forTextInputNativeID nativeID: String, offset: NSNumber) {
+  @objc public func setOffset(forTextInputNativeID nativeID: String, offset: NSNumber, withTag tag: NSNumber) {
     KeyboardAreaExtender.shared.updateHeight(to: CGFloat(offset), for: nativeID)
-    offsetMap[nativeID] = offset
+    offsetMap[nativeID] = (offset: offset, tag: tag)
   }
 
   @objc public func getOffset(forTextInputNativeID nativeID: String?) -> NSNumber? {
-    guard let unwrappedNativeID = nativeID else { return nil }
-    return offsetMap[unwrappedNativeID]
+    guard let unwrappedNativeID = nativeID, let pair = offsetMap[unwrappedNativeID] else { return nil }
+    return pair.offset
   }
 
   @objc public func hasOffset(forTextInputNativeID nativeID: String?) -> Bool {
@@ -30,7 +30,10 @@ public class KeyboardOffsetProvider: NSObject {
     return offsetMap[unwrappedNativeID] != nil
   }
 
-  @objc public func removeOffset(forTextInputNativeID nativeID: String) {
-    offsetMap.removeValue(forKey: nativeID)
+  @objc public func removeOffset(forTextInputNativeID nativeID: String, withTag tag: NSNumber) {
+    // Ensure the tag matches before removing the entry
+    if let currentPair = offsetMap[nativeID], currentPair.tag == tag {
+      offsetMap.removeValue(forKey: nativeID)
+    }
   }
 }
