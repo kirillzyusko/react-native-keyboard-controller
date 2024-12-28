ed9e7effe3aa0016f3c87814c3cc7dcc4277ba73
kirillzyusko <zyusko.kirik@gmail.com>
2024-12-28 20:59:33 +0100

    fix: don't handle input switch twice

--- src/components/KeyboardAwareScrollView/index.tsx
+++ src/components/KeyboardAwareScrollView/index.tsx
@@ -112,6 +112,8 @@ const KeyboardAwareScrollView = forwardRef<
     const scrollBeforeKeyboardMovement = useSharedValue(0);
     const { input } = useReanimatedFocusedInput();
     const layout = useSharedValue<FocusedInputLayoutChangedEvent | null>(null);
+    const lastSelection =
+      useSharedValue<FocusedInputSelectionChangedEvent | null>(null);
 
     const { height } = useWindowDimensions();
 
@@ -154,9 +156,13 @@ const KeyboardAwareScrollView = forwardRef<
         const inputHeight = layout.value?.layout.height || 0;
         const point = absoluteY + inputHeight;
 
+        console.log({ absoluteY, inputHeight, point, visibleRect });
+
         if (visibleRect - point <= bottomOffset) {
           const relativeScrollTo =
             keyboardHeight.value - (height - point) + bottomOffset;
+
+          console.log({ relativeScrollTo });
           const interpolatedScrollTo = interpolate(
             e,
             [initialKeyboardSize.value, keyboardHeight.value],
@@ -171,6 +177,11 @@ const KeyboardAwareScrollView = forwardRef<
           const targetScrollY =
             Math.max(interpolatedScrollTo, 0) + scrollPosition.value;
 
+          console.log({
+            targetScrollY,
+            scrollPosition: scrollPosition.value,
+            interpolatedScrollTo,
+          });
           scrollTo(scrollViewAnimatedRef, 0, targetScrollY, animated);
 
           return interpolatedScrollTo;
@@ -259,6 +270,15 @@ const KeyboardAwareScrollView = forwardRef<
     const onSelectionChange = useCallback(
       (e: FocusedInputSelectionChangedEvent) => {
         "worklet";
+
+        const lastTarget = lastSelection.value?.target;
+
+        lastSelection.value = e;
+
+        if (e.target !== lastTarget) {
+          // ignore this event, because "focus changed" event handled in `useSmoothKeyboardHandler`
+          return;
+        }
 
         console.log(e);
 
