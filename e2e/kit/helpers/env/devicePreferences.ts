c305450b5a0724bf5e991773090706c35396e813
kirillzyusko <zyusko.kirik@gmail.com>
2024-12-31 13:33:19 +0100

    Revert "e2e: faster selection test (#747)"
    This reverts commit f096a2f66e0fe8677afb02c051e614b647ddaf6f.

--- e2e/kit/helpers/env/devicePreferences.ts
+++ e2e/kit/helpers/env/devicePreferences.ts
@@ -2,44 +2,37 @@ import parseDeviceName from "../../utils/parseDeviceName";
 
 type Preference = {
   emojiButtonCoordinates?: { x: number; y: number };
-  keyboard: number;
   width: number;
   height: number;
 };
 
 const DEVICE_PREFERENCES: Record<string, Preference> = {
   "e2e_emulator_28": {
-    keyboard: 980,
     emojiButtonCoordinates: undefined,
     width: 1080,
     height: 1920,
   },
   "e2e_emulator_31": {
-    keyboard: 830,
     emojiButtonCoordinates: { x: 324, y: 1704 },
     width: 1080,
     height: 1920,
   },
   "iPhone 16 Pro": {
-    keyboard: 291,
     emojiButtonCoordinates: { x: 40, y: 830 },
     width: 393,
     height: 852,
   },
   "iPhone 15 Pro": {
-    keyboard: 291,
     emojiButtonCoordinates: { x: 40, y: 830 },
     width: 393,
     height: 852,
   },
   "iPhone 14 Pro": {
-    keyboard: 291,
     emojiButtonCoordinates: { x: 40, y: 830 },
     width: 393,
     height: 852,
   },
   "iPhone 13 Pro": {
-    keyboard: 286,
     emojiButtonCoordinates: { x: 40, y: 830 },
     width: 390,
     height: 844,
