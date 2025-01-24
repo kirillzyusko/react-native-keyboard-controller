450be0fa12ff38ff0512b0ea361224c195e0ca10
kirillzyusko <zyusko.kirik@gmail.com>
2025-01-24 10:35:08 +0100

    fix: dynamic parent distance

--- ios/extensions/UIResponder.swift
+++ ios/extensions/UIResponder.swift
@@ -20,39 +20,42 @@ public extension UIResponder {
   }
 
   internal func findFirstResponder(sender _: AnyObject) {
-    let type = String(describing: type(of: self))
-    // handle `contextMenuHidden` prop - in this case the parent is considered as a first responder
-    // (but actually its children is an actual input), so we apply correction here and point out
-    // to the actual first responder (first children)
-    let isChildrenActuallyFirstResponder =
-      type == "RCTMultilineTextInputView" ||
-      type == "RCTSinglelineTextInputView" ||
-      type == "RCTTextInputComponentView"
-    if isChildrenActuallyFirstResponder {
-      UIResponder._currentFirstResponder = (self as? UIView)?.subviews[0]
-    } else {
-      UIResponder._currentFirstResponder = self
-    }
+    UIResponder._currentFirstResponder = self
   }
 }
 
 public extension Optional where Wrapped == UIResponder {
   var reactViewTag: NSNumber {
-    #if KEYBOARD_CONTROLLER_NEW_ARCH_ENABLED
-      return ((self as? UIView)?.superview?.tag ?? -1) as NSNumber
-    #else
-      return (self as? UIView)?.superview?.reactTag ?? -1
-    #endif
+    guard let view = self as? UIView else {
+      return -1
+    }
+
+    let tagExtractor: (UIView) -> NSNumber? = { v in
+      #if KEYBOARD_CONTROLLER_NEW_ARCH_ENABLED
+        return (v.tag != 0) ? NSNumber(value: v.tag) : nil
+      #else
+        return v.reactTag
+      #endif
+    }
+
+    return climbSuperviewChain(startingFrom: view, using: tagExtractor) ?? -1
   }
 
-  var nativeID: String? {
-    guard let superview = (self as? UIView)?.superview else { return nil }
+  private func climbSuperviewChain(startingFrom start: UIView,
+                                   using tagExtractor: (UIView) -> NSNumber?)
+    -> NSNumber?
+  {
+    var currentView: UIView? = start
 
-    #if KEYBOARD_CONTROLLER_NEW_ARCH_ENABLED
-      return (superview as NSObject).value(forKey: "nativeId") as? String
-    #else
-      return superview.nativeID
-    #endif
+    while let v = currentView {
+      if let extractedTag = tagExtractor(v) {
+        return extractedTag
+      }
+
+      currentView = v.superview
+    }
+
+    return nil
   }
 }
 
@@ -67,6 +70,7 @@ public extension Optional where Wrapped: UIResponder {
          scrollView.frame.width >= scrollView.contentSize.width,
          scrollView.isScrollEnabled
       {
+        print("\(scrollView.frame.width) \(scrollView.frame.size.width) \(scrollView.contentSize.width)")
         return scrollView.reactViewTag
       }
 
