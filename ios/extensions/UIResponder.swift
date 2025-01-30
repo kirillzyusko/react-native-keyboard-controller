6d0efb4853496194086ed8267d37d1215c7bece4
kirillzyusko <zyusko.kirik@gmail.com>
2025-01-30 15:24:58 +0100

    fix: clean up the code

--- ios/extensions/UIResponder.swift
+++ ios/extensions/UIResponder.swift
@@ -9,81 +9,21 @@
 import Foundation
 import UIKit
 
-extension UIView {
-  @objc func findFirstResponder() -> UIResponder? {
-    if isFirstResponder {
-      return self
-    }
-    for subview in subviews {
-      if let responder = subview.findFirstResponder() {
-        return responder
-      }
-    }
-    return nil
-  }
-}
-
-extension UIWindow {
-  @objc override func findFirstResponder() -> UIResponder? {
-    return rootViewController?.view.findFirstResponder() ?? super.findFirstResponder()
-  }
-}
-
-// If needed, handle navigation or tab controllers by extending UIViewController
-extension UIViewController {
-  @objc func findFirstResponder() -> UIResponder? {
-    if view.isFirstResponder {
-      return view
-    }
-    return view.findFirstResponder() ?? presentedViewController?.findFirstResponder()
-  }
-}
-
 @objc
 public extension UIResponder {
-  private weak static var _currentFirstResponder: UIResponder?
-
   static var current: UIResponder? {
-    for window in UIApplication.shared.windows {
-      if let responder = window.findFirstResponder() {
-        return responder
-      }
-    }
-    return nil
+    guard let window = UIApplication.shared.activeWindow else { return nil }
+    return window.findFirstResponder()
   }
-
-  /* internal func findFirstResponder(sender _: AnyObject) {
-     let type = String(describing: type(of: self))
-     // handle `contextMenuHidden` prop - in this case the parent is considered as a first responder
-     // (but actually its children is an actual input), so we apply correction here and point out
-     // to the actual first responder (first children)
-     let isChildrenActuallyFirstResponder =
-       type == "RCTMultilineTextInputView" ||
-       type == "RCTSinglelineTextInputView" ||
-       type == "RCTTextInputComponentView"
-     if isChildrenActuallyFirstResponder {
-       UIResponder._currentFirstResponder = (self as? UIView)?.subviews[0]
-     } else {
-       UIResponder._currentFirstResponder = self
-     }
-   } */
 }
 
 public extension Optional where Wrapped == UIResponder {
   var reactViewTag: NSNumber {
-    guard let this = self as? UIView else {
-      return -1
-    }
-
-    let tagExtractor: (UIView) -> NSNumber? = { view in
-      #if KEYBOARD_CONTROLLER_NEW_ARCH_ENABLED
-        return (view.tag != 0) ? NSNumber(value: view.tag) : nil
-      #else
-        return view.reactTag
-      #endif
-    }
-
-    return climbSuperviewChain(startingFrom: this.superview, using: tagExtractor) ?? -1
+    #if KEYBOARD_CONTROLLER_NEW_ARCH_ENABLED
+      return ((self as? UIView)?.superview?.tag ?? -1) as NSNumber
+    #else
+      return (self as? UIView)?.superview?.reactTag ?? -1
+    #endif
   }
 
   var nativeID: String? {
@@ -98,23 +38,6 @@ public extension Optional where Wrapped == UIResponder {
     #else
       return superview.nativeID
     #endif
-  }
-
-  private func climbSuperviewChain(
-    startingFrom start: UIView?,
-    using tagExtractor: (UIView) -> NSNumber?
-  ) -> NSNumber? {
-    var currentView: UIView? = start
-
-    while let view = currentView {
-      if let extractedTag = tagExtractor(view) {
-        return extractedTag
-      }
-
-      currentView = view.superview
-    }
-
-    return nil
   }
 }
 
