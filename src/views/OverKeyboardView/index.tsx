2a923a6962f4b2a4b65d69f311d5247eb2559ef4
Kirill Zyusko <zyusko.kirik@gmail.com>
2025-02-25 22:42:29 +0100

    refactor: use `nativeFabricUIManager` instead of `IS_FABRIC` to detect new architecture in JS (#832)
    ## 📜 Description
    
    Replace `global._IS_FABRIC` check with `global.nativeFabricUIManager`
    check.
    
    ## 💡 Motivation and Context
    
    `_IS_FABRIC` is injected by reanimated, and it seems like they may
    change the variable name and don't treat it as a breaking change.
    
    To avoid further incompatibility I decided to re-work that piece and
    rely on presence of fabric manager provided by RN.
    
    ## 📢 Changelog
    
    <!-- High level overview of important changes -->
    <!-- For example: fixed status bar manipulation; added new types
    declarations; -->
    <!-- If your changes don't affect one of platform/language below - then
    remove this platform/language -->
    
    ### JS
    
    - rely on `global.nativeFabricUIManager` instead of `global._IS_FABRIC`;
    
    ## 🤔 How Has This Been Tested?
    
    Tested on iOS in paper/fabric example projects.
    
    ## 📸 Screenshots (if appropriate):
    
    <img width="176" alt="image"
    src="https://github.com/user-attachments/assets/33622eee-020c-43eb-b0e2-89a91d216719"
    />
    
    ## 📝 Checklist
    
    - [x] CI successfully passed
    - [x] I added new mocks and corresponding unit-tests if library API was
    changed

--- src/views/OverKeyboardView/index.tsx
+++ src/views/OverKeyboardView/index.tsx
@@ -1,6 +1,7 @@
 import React, { useMemo } from "react";
 import { Platform, StyleSheet, View } from "react-native";
 
+import { IS_FABRIC } from "../../architecture";
 import { RCTOverKeyboardView } from "../../bindings";
 import { useWindowDimensions } from "../../hooks";
 
@@ -18,8 +19,7 @@ const OverKeyboardView = ({
       styles.absolute,
       // On iOS - stretch view to full window dimensions to make yoga work
       // On Android Fabric we temporarily use the same approach
-      // @ts-expect-error `_IS_FABRIC` is injected by REA
-      Platform.OS === "ios" || global._IS_FABRIC ? inner : undefined,
+      Platform.OS === "ios" || IS_FABRIC ? inner : undefined,
     ],
     [inner],
   );
