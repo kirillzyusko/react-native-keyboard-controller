c16d4af40ce8e114e86c427c6319351fd7644e1b
kirillzyusko <zyusko.kirik@gmail.com>
2025-03-26 16:50:33 +0100

    fix: make tests stable

--- FabricExample/src/screens/Examples/ReanimatedChatFlatList/index.tsx
+++ FabricExample/src/screens/Examples/ReanimatedChatFlatList/index.tsx
@@ -1,6 +1,12 @@
 import { useHeaderHeight } from "@react-navigation/elements";
-import React from "react";
-import { FlatList, TextInput } from "react-native";
+import React, { useRef } from "react";
+import {
+  FlatList,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import { KeyboardAvoidingView } from "react-native-keyboard-controller";
 
 import Message from "../../../components/Message";
@@ -19,24 +25,38 @@ const RenderItem: ListRenderItem<MessageProps> = ({ item, index }) => {
 
 function ReanimatedChatFlatList() {
   const headerHeight = useHeaderHeight();
+  const ref = useRef<FlatList>(null);
 
   return (
-    <KeyboardAvoidingView
-      behavior="translate-with-padding"
-      keyboardVerticalOffset={headerHeight}
-      style={styles.container}
-      testID="flat-list.container"
-    >
-      <FlatList
-        inverted
-        contentContainerStyle={styles.contentContainer}
-        data={reversedMessages}
-        initialNumToRender={15}
-        renderItem={RenderItem}
-        testID="flat-list.chat"
-      />
-      <TextInput style={styles.textInput} testID="flat-list.input" />
-    </KeyboardAvoidingView>
+    <>
+      <KeyboardAvoidingView
+        behavior="translate-with-padding"
+        keyboardVerticalOffset={headerHeight}
+        style={styles.container}
+        testID="flat-list.container"
+      >
+        <FlatList
+          ref={ref}
+          inverted
+          contentContainerStyle={styles.contentContainer}
+          data={reversedMessages}
+          initialNumToRender={15}
+          renderItem={RenderItem}
+          testID="flat-list.chat"
+        />
+        <TextInput style={styles.textInput} testID="flat-list.input" />
+        <TouchableOpacity
+          activeOpacity={0.8}
+          style={styles.fab}
+          testID="flat-list.scrollToTop"
+          onPress={() => ref.current?.scrollToEnd()}
+        >
+          <View style={styles.circle}>
+            <Text style={styles.icon}>↑</Text>
+          </View>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
+    </>
   );
 }
 
