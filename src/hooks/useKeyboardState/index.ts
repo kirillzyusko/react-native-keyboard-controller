be344ec23830eac9a850d4203d84b32783538841
kirillzyusko <zyusko.kirik@gmail.com>
2025-04-01 12:06:44 +0200

    feat: useKeyboardState

---
+++ src/hooks/useKeyboardState/index.ts
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+
+import { KeyboardEvents } from "../../bindings";
+import { KeyboardController } from "../../module";
+
+// TODO: expose isVisible, isTransitioning (isHiding/isShowing) props?
+export const useKeyboardState = () => {
+  const [state, setState] = useState(KeyboardController.state());
+
+  useEffect(() => {
+    const didShowSubscription = KeyboardEvents.addListener(
+      "keyboardDidShow",
+      (e) => {
+        setState(e);
+      },
+    );
+    const didHideSubscription = KeyboardEvents.addListener(
+      "keyboardDidHide",
+      (e) => setState(e),
+    );
+
+    // we might have missed an update between reading a value in render and
+    // `addListener` in this handler, so we set it here. If there was
+    // no change, React will filter out this update as a no-op.
+    setState(KeyboardController.state());
+
+    return () => {
+      didShowSubscription.remove();
+      didHideSubscription.remove();
+    };
+  }, []);
+
+  return state;
+};
