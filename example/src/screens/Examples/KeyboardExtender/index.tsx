1a8413233663494259b20d544699c9ec9213bd51
kirillzyusko <zyusko.kirik@gmail.com>
2025-04-14 19:07:41 +0200

    feat: continue experiments

--- example/src/screens/Examples/KeyboardExtender/index.tsx
+++ example/src/screens/Examples/KeyboardExtender/index.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import {
   Alert,
   Button,
+  Image,
   StyleSheet,
   Text,
   TextInput,
@@ -19,17 +20,20 @@ import Reanimated, {
   withTiming,
 } from "react-native-reanimated";
 
-// TODO: is not stretched to full width <- fixed with additional View inside KeyboardExtender
-// TODO: don't hardcode height as 44 in native code <- rely on internal view height
 // TODO: remove _touchHandler when view gets detached from keyboard?
 // TODO: test how GestureHandler works there
+// TODO: add ability to make custom keyboard extender for specific inputs (group/textInputNativeID)
+// TODO: don't overwrite existing inputAccessoryView?
+// TODO: remove view when enabled={false}
+// TODO: test iOS 15
+// TODO: Android implementation
 
 export default function KeyboardExtendExample() {
   const [showExtend, setShowExtend] = useState(true);
   const [isOKVMode, setOKVMode] = useState(false);
   const opacity = useSharedValue(1);
   // TODO: replace with animated value
-  const {height} = useKeyboardState();
+  const { height } = useKeyboardState();
 
   const animatedStyle = useAnimatedStyle(
     () => ({
@@ -39,61 +43,78 @@ export default function KeyboardExtendExample() {
   );
 
   return (
-    <View style={styles.container}>
-      <Button
-        title={isOKVMode ? "OKV" : "Default"}
-        onPress={() => setOKVMode(!isOKVMode)}
+    <>
+      <Image
+        source={require("./background.jpg")}
+        style={{ ...StyleSheet.absoluteFillObject, flex: 1, width: "100%" }}
       />
-      <Button
-        title="Change opacity"
-        onPress={() => {
-          opacity.set(
-            withTiming(opacity.value === 0 ? 1 : 0, {
-              duration: 1000,
-            }),
-          );
-        }}
-      />
-      <Button
-        title={showExtend ? "Detach" : "Attach"}
-        onPress={() => setShowExtend(!showExtend)}
-      />
-      <TextInput placeholder="Type something..." style={styles.input} />
+      <View style={styles.container}>
+        <Button
+          title={isOKVMode ? "OKV" : "Default"}
+          onPress={() => setOKVMode(!isOKVMode)}
+        />
+        <Button
+          title="Change opacity"
+          onPress={() => {
+            opacity.set(
+              withTiming(opacity.value === 0 ? 1 : 0, {
+                duration: 1000,
+              }),
+            );
+          }}
+        />
+        <Button
+          title={showExtend ? "Detach" : "Attach"}
+          onPress={() => setShowExtend(!showExtend)}
+        />
+        <TextInput
+          placeholder="Type something..."
+          placeholderTextColor="#1c1c1c"
+          style={styles.input}
+        />
 
-      <TextInput placeholder="Another input field..." style={styles.input} />
+        <TextInput
+          placeholder="Another input field..."
+          placeholderTextColor="#1c1c1c"
+          style={styles.input}
+        />
 
-      <KeyboardExtender enabled={showExtend}>
-        <Reanimated.View style={[styles.keyboardExtend, animatedStyle]}>
-          <TouchableOpacity onPress={() => Alert.alert("10$")}>
-            <Text style={styles.priceText}>10$</Text>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={() => Alert.alert("20$")}>
-            <Text style={styles.priceText}>20$</Text>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={() => Alert.alert("50$")}>
-            <Text style={styles.priceText}>50$</Text>
-          </TouchableOpacity>
-        </Reanimated.View>
-      </KeyboardExtender>
-      <OverKeyboardView visible={isOKVMode}>
-        <View style={{flex: 1, justifyContent: "flex-end"}}>
-          {/* TODO replace hardcoded value */}
-          <TextInput placeholder="Search..." style={[styles.input, {marginBottom: height - 61.73}]} />
-        </View>
-      </OverKeyboardView>
-    </View>
+        <KeyboardExtender enabled={showExtend}>
+          <Reanimated.View style={[styles.keyboardExtend, animatedStyle]}>
+            <TouchableOpacity onPress={() => Alert.alert("10$")}>
+              <Text style={styles.priceText}>10$</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => Alert.alert("20$")}>
+              <Text style={styles.priceText}>20$</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => Alert.alert("50$")}>
+              <Text style={styles.priceText}>50$</Text>
+            </TouchableOpacity>
+          </Reanimated.View>
+        </KeyboardExtender>
+        <OverKeyboardView visible={isOKVMode}>
+          <View style={{ flex: 1, justifyContent: "flex-end" }}>
+            {/* TODO replace hardcoded value */}
+            <TextInput
+              placeholder="Search..."
+              style={[styles.input, { marginBottom: height - 61.73 }]}
+            />
+          </View>
+        </OverKeyboardView>
+      </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 20,
+    paddingHorizontal: 20,
   },
   input: {
     height: 40,
     borderWidth: 1,
-    borderColor: "#ccc",
+    borderColor: "#1c1c1c",
     borderRadius: 4,
     padding: 10,
     marginBottom: 20,
