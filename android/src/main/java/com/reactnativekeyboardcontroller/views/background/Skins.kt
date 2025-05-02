81a69ccfae62b2f45e47bbc143114a95aef24999
kirillzyusko <zyusko.kirik@gmail.com>
2025-05-02 16:51:57 +0200

    feat: Samsung, TTS keyboards

--- android/src/main/java/com/reactnativekeyboardcontroller/views/background/Skins.kt
+++ android/src/main/java/com/reactnativekeyboardcontroller/views/background/Skins.kt
@@ -1,6 +1,7 @@
 package com.reactnativekeyboardcontroller.views.background
 
-import android.graphics.Color
+import android.os.Build
+import android.util.Log
 import androidx.annotation.ColorInt
 import androidx.core.content.ContextCompat
 import com.facebook.react.uimanager.ThemedReactContext
@@ -11,23 +12,28 @@ import com.reactnativekeyboardcontroller.extensions.isSystemDarkMode
 val imeColorMap: Map<String, Pair<Int, Int>> = mapOf(
   "com.google.android.inputmethod.latin" to (R.color.gboard_light to R.color.gboard_dark),
   "com.touchtype.swiftkey" to (R.color.swiftkey_light to R.color.swiftkey_dark),
-  "com.google.android.tts" to (R.color.gboard_light to R.color.gboard_dark),
-  "ru.yandex.androidkeyboard" to (R.color.yandex_light to R.color.yandex_dark)
+  "com.google.android.googlequicksearchbox" to (R.color.gboard_tts_light to R.color.gboard_tts_dark),
+  "com.google.android.tts" to (R.color.gboard_tts_light to R.color.gboard_tts_dark),
+  "ru.yandex.androidkeyboard" to (R.color.yandex_light to R.color.yandex_dark),
+  "com.samsung.android.honeyboard" to (R.color.samsung_light to R.color.samsung_dark)
 )
 
+// TODO: re-work as extension?
 object Skins {
   @ColorInt
   fun ThemedReactContext.getInputMethodColor(): Int {
     val imePackage = currentImePackage()
     val isDark = isSystemDarkMode()
 
+    println("Current IME: $imePackage")
+    println(imePackage)
+    Log.i("Skins", "Current IME: $imePackage")
+
     val (lightColorRes, darkColorRes) = imeColorMap[imePackage]
       ?: (R.color.gboard_dark to R.color.gboard_dark)
 
-    val resId = if (isDark) darkColorRes else lightColorRes
+    // on Android 10 dark theme doesn't affect keyboard color
+    val resId = if (isDark && Build.VERSION.SDK_INT > Build.VERSION_CODES.Q) darkColorRes else lightColorRes
     return ContextCompat.getColor(this, resId)
   }
-
-  @ColorInt
-  private fun getDefaultColor(): Int = Color.parseColor("#FFCCCCCC")
 }
