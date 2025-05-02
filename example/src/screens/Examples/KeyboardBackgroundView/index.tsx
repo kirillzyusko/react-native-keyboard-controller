b3feb1236c830ace9382dfca85567f8948a90159
kirillzyusko <zyusko.kirik@gmail.com>
2025-05-02 16:52:45 +0200

    feat: shared transition example

--- example/src/screens/Examples/KeyboardBackgroundView/index.tsx
+++ example/src/screens/Examples/KeyboardBackgroundView/index.tsx
@@ -17,19 +17,24 @@ import {
 import Reanimated, {
   Easing,
   interpolate,
+  interpolateColor,
   useAnimatedStyle,
   useSharedValue,
   withRepeat,
   withSpring,
   withTiming,
 } from "react-native-reanimated";
-import { SafeAreaView } from "react-native-safe-area-context";
+import {
+  SafeAreaView,
+  useSafeAreaInsets,
+} from "react-native-safe-area-context";
 
 const ReanimatedBackgroundView = Reanimated.createAnimatedComponent(
   KeyboardBackgroundView,
 );
+const ReanimatedTextInput = Reanimated.createAnimatedComponent(TextInput);
 
-const KeyboardBackdropViewExample = () => {
+const KeyboardBackdropViewExample2 = () => {
   const progress = useSharedValue(0);
 
   useEffect(() => {
@@ -62,7 +67,7 @@ const KeyboardBackdropViewExample = () => {
         [0, 0.8, 1],
         [0, 0, 25],
       ),
-      transform: [{ translateY: -progress.value * 70 + 34 }, { rotate: "45deg" }],
+      transform: [{ translateY: -progress.value * 70 }, { rotate: "45deg" }],
     }),
     [],
   );
@@ -87,7 +92,7 @@ const KeyboardBackdropViewExample = () => {
         style={styles.textInput}
       />
       <KeyboardStickyView>
-        <ReanimatedBackgroundView
+        <Reanimated.View
           style={[
             {
               width: 50,
@@ -104,12 +109,12 @@ const KeyboardBackdropViewExample = () => {
             mainCircle,
           ]}
         >
-                  <Image
+          <Image
             source={require("./ai.png")}
             style={{ transform: [{ rotate: "-45deg" }], width: 20, height: 20 }}
           />
-        </ReanimatedBackgroundView>
-        <ReanimatedBackgroundView
+        </Reanimated.View>
+        <Reanimated.View
           style={[
             {
               width: 100,
@@ -118,10 +123,74 @@ const KeyboardBackdropViewExample = () => {
               right: -5,
               bottom: -122,
               position: "absolute",
+              backgroundColor: "#1e1f26",
             },
             secondCircle,
           ]}
         />
+      </KeyboardStickyView>
+    </SafeAreaView>
+  );
+};
+
+// shared transitions
+const KeyboardBackdropViewExample = () => {
+  const { bottom } = useSafeAreaInsets();
+  const { progress } = useReanimatedKeyboardAnimation();
+
+  const opacity = useAnimatedStyle(
+    () => ({
+      height: 291 + 70,
+      opacity: interpolate(progress.value, [0, 0.5, 1], [0, 0.9, 1]),
+    }),
+    [],
+  );
+  const inputColor = useAnimatedStyle(
+    () => ({
+      backgroundColor: interpolateColor(
+        progress.value,
+        [0, 1],
+        ["#323232", "#474747"],
+      ),
+    }),
+    [],
+  );
+
+  return (
+    <SafeAreaView
+      style={{
+        backgroundColor: "#000000",
+        flex: 1,
+        justifyContent: "flex-end",
+      }}
+    >
+      <KeyboardStickyView offset={{ closed: 291, opened: 291 + bottom }}>
+        <ReanimatedBackgroundView style={opacity}></ReanimatedBackgroundView>
+        <View
+          style={{
+            marginHorizontal: 30,
+            marginVertical: 16,
+            position: "absolute",
+            left: 0,
+            right: 0,
+            bottom: 0,
+            top: 0,
+          }}
+        >
+          <ReanimatedTextInput
+            placeholder="127.0.0.1"
+            placeholderTextColor="#ecececec"
+            style={[
+              {
+                width: "100%",
+                padding: 10,
+                borderRadius: 8,
+                textAlign: "center",
+              },
+              inputColor,
+            ]}
+          />
+        </View>
       </KeyboardStickyView>
     </SafeAreaView>
   );
