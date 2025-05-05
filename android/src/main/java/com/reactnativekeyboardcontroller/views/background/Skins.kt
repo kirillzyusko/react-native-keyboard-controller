de122aac87ec598b533f8cebedb52bb63734ab3c
kirillzyusko <zyusko.kirik@gmail.com>
2025-05-05 23:39:23 +0200

    chore: try to respect Material theme

--- android/src/main/java/com/reactnativekeyboardcontroller/views/background/Skins.kt
+++ android/src/main/java/com/reactnativekeyboardcontroller/views/background/Skins.kt
@@ -3,19 +3,31 @@ package com.reactnativekeyboardcontroller.views.background
 import android.os.Build
 import android.util.Log
 import androidx.annotation.ColorInt
+import androidx.compose.material3.dynamicLightColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.ui.graphics.toArgb
 import androidx.core.content.ContextCompat
 import com.facebook.react.uimanager.ThemedReactContext
 import com.reactnativekeyboardcontroller.R
 import com.reactnativekeyboardcontroller.extensions.currentImePackage
 import com.reactnativekeyboardcontroller.extensions.isSystemDarkMode
 
+object ImePackages {
+  const val GBOARD = "com.google.android.inputmethod.latin"
+  const val SWIFT_KEY = "com.touchtype.swiftkey"
+  const val GBOARD_TTS = "com.google.android.googlequicksearchbox"
+  const val GOOGLE_TTS = "com.google.android.tts"
+  const val YANDEX = "ru.yandex.androidkeyboard"
+  const val SAMSUNG = "com.samsung.android.honeyboard"
+}
+
 val imeColorMap: Map<String, Pair<Int, Int>> = mapOf(
-  "com.google.android.inputmethod.latin" to (R.color.gboard_light to R.color.gboard_dark),
-  "com.touchtype.swiftkey" to (R.color.swiftkey_light to R.color.swiftkey_dark),
-  "com.google.android.googlequicksearchbox" to (R.color.gboard_tts_light to R.color.gboard_tts_dark),
-  "com.google.android.tts" to (R.color.gboard_tts_light to R.color.gboard_tts_dark),
-  "ru.yandex.androidkeyboard" to (R.color.yandex_light to R.color.yandex_dark),
-  "com.samsung.android.honeyboard" to (R.color.samsung_light to R.color.samsung_dark)
+  ImePackages.GBOARD to (R.color.gboard_light to R.color.gboard_dark),
+  ImePackages.SWIFT_KEY to (R.color.swiftkey_light to R.color.swiftkey_dark),
+  ImePackages.GBOARD_TTS to (R.color.gboard_tts_light to R.color.gboard_tts_dark),
+  ImePackages.GOOGLE_TTS to (R.color.gboard_tts_light to R.color.gboard_tts_dark),
+  ImePackages.YANDEX to (R.color.yandex_light to R.color.yandex_dark),
+  ImePackages.SAMSUNG to (R.color.samsung_light to R.color.samsung_dark)
 )
 
 // TODO: re-work as extension?
@@ -28,6 +40,67 @@ object Skins {
     println("Current IME: $imePackage")
     println(imePackage)
     Log.i("Skins", "Current IME: $imePackage")
+
+    val colorScheme = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+      dynamicLightColorScheme(this)
+    } else {
+      lightColorScheme() // fallback
+    }
+
+    val colors = listOf(
+      "primary" to colorScheme.primary,
+      "onPrimary" to colorScheme.onPrimary,
+      "primaryContainer" to colorScheme.primaryContainer,
+      "onPrimaryContainer" to colorScheme.onPrimaryContainer,
+      "inversePrimary" to colorScheme.inversePrimary,
+      "secondary" to colorScheme.secondary,
+      "onSecondary" to colorScheme.onSecondary,
+      "secondaryContainer" to colorScheme.secondaryContainer,
+      "onSecondaryContainer" to colorScheme.onSecondaryContainer,
+      "tertiary" to colorScheme.tertiary,
+      "onTertiary" to colorScheme.onTertiary,
+      "tertiaryContainer" to colorScheme.tertiaryContainer,
+      "onTertiaryContainer" to colorScheme.onTertiaryContainer,
+      "background" to colorScheme.background,
+      "onBackground" to colorScheme.onBackground,
+      "surface" to colorScheme.surface,
+      "onSurface" to colorScheme.onSurface,
+      "surfaceVariant" to colorScheme.surfaceVariant,
+      "onSurfaceVariant" to colorScheme.onSurfaceVariant,
+      "surfaceTint" to colorScheme.surfaceTint,
+      "inverseSurface" to colorScheme.inverseSurface,
+      "inverseOnSurface" to colorScheme.inverseOnSurface,
+      "error" to colorScheme.error,
+      "onError" to colorScheme.onError,
+      "errorContainer" to colorScheme.errorContainer,
+      "onErrorContainer" to colorScheme.onErrorContainer,
+      "outline" to colorScheme.outline,
+      "outlineVariant" to colorScheme.outlineVariant,
+      "scrim" to colorScheme.scrim,
+      "surfaceBright" to colorScheme.surfaceBright,
+      "surfaceDim" to colorScheme.surfaceDim,
+      "surfaceContainer" to colorScheme.surfaceContainer,
+      "surfaceContainerHigh" to colorScheme.surfaceContainerHigh,
+      "surfaceContainerHighest" to colorScheme.surfaceContainerHighest,
+      "surfaceContainerLow" to colorScheme.surfaceContainerLow,
+      "surfaceContainerLowest" to colorScheme.surfaceContainerLowest,
+    )
+
+    colors.forEach { (name, color) ->
+      val hex = "#%02X%02X%02X".format(
+        (color.red * 255).toInt(),
+        (color.green * 255).toInt(),
+        (color.blue * 255).toInt()
+      )
+      Log.d("ColorScheme", "$name = $hex")
+    }
+
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S && imePackage == ImePackages.GBOARD) {
+      if (isDark) {
+        return colorScheme.onBackground.toArgb()
+      }
+      return colorScheme.surfaceContainer.toArgb()
+    }
 
     val (lightColorRes, darkColorRes) = imeColorMap[imePackage]
       ?: (R.color.gboard_dark to R.color.gboard_dark)
