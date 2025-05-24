686d9f5497ea583f49daa6cb34260154cf9a3432
Macovei Gabriel <macovei.gabriel.razvan@gmail.com>
2025-05-24 19:34:49 +0300

    Add back the comments
    Add back the comments

--- src/views/OverKeyboardView/index.tsx
+++ src/views/OverKeyboardView/index.tsx
@@ -7,6 +7,19 @@ import { useWindowDimensions } from "../../hooks";
 import type { OverKeyboardViewProps } from "../../types/views";
 import type { PropsWithChildren } from "react";
 
+/**
+ * A view component that renders its children over the keyboard without closing the keyboard.
+ * Acts similar to modal, but doesn't close the keyboard when it's visible.
+ *
+ * @param props - Component props.
+ * @returns A view component that renders over the keyboard.
+ * @example
+ * ```tsx
+ * <OverKeyboardView visible={true}>
+ *   <Text>This will appear over the keyboard</Text>
+ * </OverKeyboardView>
+ * ```
+ */
 const OverKeyboardView = ({
   children,
   visible,
