9f781d4d2a71addbd415e253459f768c34914d24
Kirill Zyusko <zyusko.kirik@gmail.com>
2025-06-11 17:56:10 +0200

    docs: enable `llms.txt` for docs (#976)
    ## 📜 Description
    
    Added generation of `llms.txt`.
    
    ## 💡 Motivation and Context
    
    In reality people already use less and less classic search and more and
    more trust to LLMs. However current LLMs struggle to index classic
    websites and unofficial standard is usage of `llms.txt` - markdown files
    that contains documentation.
    
    To continuously generate llms files I decided to use a special plugin
    that does. Maybe in future I'll switch to official docusaurus plugin,
    but now let's not waste time and implement something to make docs LLMs
    friendly.
    
    ## 📢 Changelog
    
    <!-- High level overview of important changes -->
    <!-- For example: fixed status bar manipulation; added new types
    declarations; -->
    <!-- If your changes don't affect one of platform/language below - then
    remove this platform/language -->
    
    ### Docs
    
    - continuously generate `llms.txt`;
    - added patch-package;
    - fixed broken anchors;
    
    ## 🤔 How Has This Been Tested?
    
    Preview will not have LLMs integration (for now), so the only way to
    test is to merge it and verify deploy.
    
    ## 📸 Screenshots (if appropriate):
    
    <img width="266" alt="image"
    src="https://github.com/user-attachments/assets/4e26ca9f-1882-4440-9158-8534a05f8b64"
    />
    
    ## 📝 Checklist
    
    - [x] CI successfully passed
    - [x] I added new mocks and corresponding unit-tests if library API was
    changed

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -107,6 +107,24 @@ const config = {
       }),
     ],
   ],
+  plugins: [
+    [
+      "@signalwire/docusaurus-plugin-llms-txt",
+      {
+        // Plugin options
+        depth: 3,
+        siteTitle: "react-native0keyboard-controller",
+        siteDescription:
+          "Documentation for react-native-keyboard-controller. Package that allows to control each frame of keyboard movement in react native.",
+        content: {
+          includeBlog: true,
+          includePages: true,
+          excludeRoutes: ["**/docs/1.*/**"],
+          enableMarkdownFiles: true,
+        },
+      },
+    ],
+  ],
 
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
