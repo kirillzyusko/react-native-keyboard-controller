fdf95fce7dfad44800234d372995005c4d54d85b
kirillzyusko <zyusko.kirik@gmail.com>
2025-06-16 00:01:48 +0300

    fix: e2e tests

--- e2e/kit/015-keyboard-extender.e2e.ts
+++ e2e/kit/015-keyboard-extender.e2e.ts
@@ -1,7 +1,25 @@
-import { waitAndTap } from "./helpers";
+import { expectBitmapsToBeEqual } from "./asserts";
+import {
+  scrollDownUntilElementIsVisible,
+  waitAndTap,
+  waitForElementById,
+  waitForExpect,
+} from "./helpers";
 
 describe("`KeyboardExtender` specification", () => {
   it("should navigate to `Keyboard Extender` screen", async () => {
+    await scrollDownUntilElementIsVisible(
+      "main_scroll_view",
+      "keyboard_extender",
+    );
     await waitAndTap("keyboard_extender");
+    await waitForElementById("donation_amount");
+  });
+
+  it("should have `KeyboardExtender` above the keyboard", async () => {
+    await waitAndTap("donation_amount");
+    await waitForExpect(async () => {
+      await expectBitmapsToBeEqual("KeyboardExtenderIsAttached");
+    });
   });
 });
