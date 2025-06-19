aed22b7397fbc3947fd397b7e7f3081bbfc5201e
kirillzyusko <zyusko.kirik@gmail.com>
2025-06-20 00:07:54 +0300

    fix: ignorable border radius on Fabric

--- android/src/fabric/java/com/reactnativekeyboardcontroller/KeyboardBackgroundViewManager.kt
+++ android/src/fabric/java/com/reactnativekeyboardcontroller/KeyboardBackgroundViewManager.kt
@@ -1,8 +1,6 @@
 package com.reactnativekeyboardcontroller
 
 import com.facebook.react.uimanager.ThemedReactContext
-import com.facebook.react.uimanager.ViewManagerDelegate
-import com.facebook.react.viewmanagers.KeyboardBackgroundViewManagerDelegate
 import com.facebook.react.viewmanagers.KeyboardBackgroundViewManagerInterface
 import com.facebook.react.views.view.ReactViewGroup
 import com.facebook.react.views.view.ReactViewManager
@@ -13,9 +11,6 @@ class KeyboardBackgroundViewManager :
   ReactViewManager(),
   KeyboardBackgroundViewManagerInterface<ReactViewGroup> {
   private val manager = KeyboardBackgroundViewManagerImpl()
-  private val mDelegate = KeyboardBackgroundViewManagerDelegate(this)
-
-  override fun getDelegate(): ViewManagerDelegate<ReactViewGroup> = mDelegate
 
   override fun getName(): String = KeyboardBackgroundViewManagerImpl.NAME
 
