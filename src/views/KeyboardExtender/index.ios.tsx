ec4156b0172b273f6d44c3618ceddca686985b30
kirillzyusko <zyusko.kirik@gmail.com>
2025-06-23 13:44:51 +0200

    feat: implementation for platforms with missing native API (aka polyfill)

--- src/views/KeyboardExtender/index.ios.tsx
+++ src/views/KeyboardExtender/index.ios.tsx
@@ -1,16 +1,11 @@
 import React from "react";
-import { Animated } from "react-native";
+import { View } from "react-native";
 
-import { KeyboardBackgroundView } from "../../bindings";
-import { KeyboardStickyView } from "../../components";
-import { useKeyboardAnimation } from "../../hooks";
+import { RCTKeyboardExtender } from "../../bindings";
+import { useWindowDimensions } from "../../hooks";
 
 import type { KeyboardExtenderProps } from "../../types";
 import type { PropsWithChildren } from "react";
-
-const AnimatedKeyboardBackgroundView = Animated.createAnimatedComponent(
-  KeyboardBackgroundView,
-);
 
 /**
  * A component that embeds its children into the keyboard thus enhancing keyboard functionality.
@@ -28,14 +23,12 @@ const AnimatedKeyboardBackgroundView = Animated.createAnimatedComponent(
  */
 const KeyboardExtender = (props: PropsWithChildren<KeyboardExtenderProps>) => {
   const { children, enabled = true } = props;
-  const { progress } = useKeyboardAnimation();
+  const { width } = useWindowDimensions();
 
   return (
-    <KeyboardStickyView enabled={enabled}>
-      <AnimatedKeyboardBackgroundView style={{ opacity: progress }}>
-        {children}
-      </AnimatedKeyboardBackgroundView>
-    </KeyboardStickyView>
+    <RCTKeyboardExtender enabled={enabled}>
+      <View style={{ width }}>{children}</View>
+    </RCTKeyboardExtender>
   );
 };
 
