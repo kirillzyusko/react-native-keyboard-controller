5dd404306f1568c0b3e63305b15cc09f1cd0a346
kirillzyusko <zyusko.kirik@gmail.com>
2025-06-24 13:21:13 +0200

    feat: keyboard state selector

--- src/hooks/useKeyboardState/index.ts
+++ src/hooks/useKeyboardState/index.ts
@@ -12,11 +12,14 @@ const getLatestState = () => ({
   isVisible: KeyboardController.isVisible(),
 });
 
+type KeyboardStateSelector<T> = (state: KeyboardState) => T;
+
 /**
  * React Hook that represents the current keyboard state on iOS and Android.
  * It tracks keyboard visibility, height, appearance, type and other properties.
  * This hook subscribes to keyboard events and updates the state reactively.
  *
+ * @param selector - a function that receives the current keyboard state and picks only necessary properties to avoid frequent re-renders.
  * @returns Object {@link KeyboardState|containing} keyboard state information.
  * @see {@link https://kirillzyusko.github.io/react-native-keyboard-controller/docs/api/hooks/keyboard/use-keyboard-state|Documentation} page for more details.
  * @example
@@ -32,22 +35,24 @@ const getLatestState = () => ({
  * }
  * ```
  */
-export const useKeyboardState = (): KeyboardState => {
-  const [state, setState] = useState(getLatestState);
+function useKeyboardState<T>(
+  selector: KeyboardStateSelector<T> = (state) => state as T,
+): T {
+  const [state, setState] = useState<T>(() => selector(getLatestState()));
 
   useEffect(() => {
     const subscriptions = EVENTS.map((event) =>
       KeyboardEvents.addListener(event, () =>
         // state will be updated by global listener first,
         // so we simply read it and don't derive data from the event
-        setState(getLatestState),
+        setState(selector(getLatestState())),
       ),
     );
 
     // we might have missed an update between reading a value in render and
     // `addListener` in this handler, so we set it here. If there was
     // no change, React will filter out this update as a no-op.
-    setState(getLatestState);
+    setState(selector(getLatestState()));
 
     return () => {
       subscriptions.forEach((subscription) => subscription.remove());
@@ -55,4 +60,6 @@ export const useKeyboardState = (): KeyboardState => {
   }, []);
 
   return state;
-};
+}
+
+export { useKeyboardState };
