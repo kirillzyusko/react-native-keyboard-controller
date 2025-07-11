c45be7a40e1e1c1c90973952adcaba68f68a240d
kirillzyusko <zyusko.kirik@gmail.com>
2025-07-11 17:24:29 +0200

    fix: old XCode compilation errors

--- ios/views/KeyboardExtenderContainerView.swift
+++ ios/views/KeyboardExtenderContainerView.swift
@@ -10,29 +10,30 @@ import UIKit
 @objc
 public class KeyboardExtenderContainerView: UIView {
   private var visualEffectView: UIVisualEffectView?
-  
+
   @objc public static func create(frame: CGRect, contentView: UIView) -> UIView {
-    if #available(iOS 26.0, *) {
-      let padding = 20.0
-      let visualEffectView = UIVisualEffectView()
-      let glassEffect = UIGlassEffect()
-      glassEffect.tintColor = UIColor.black.withAlphaComponent(0.3)
-      glassEffect.isInteractive = true
-      visualEffectView.effect = glassEffect
-      visualEffectView.bounds = CGRect(x: 0, y: padding, width: frame.width - (2 * padding), height: frame.height)
-      
-      // visualEffectView.bounds = CGRect(x: padding, y: padding, width: frame.width - 2 * padding, height: frame.height)
-      visualEffectView.overrideUserInterfaceStyle = FocusedInputHolder.shared.get()?.keyboardAppearanceValue == "dark" ? .dark : .light
-      
-      visualEffectView.contentView.addSubview(contentView)
-      // contentView.bounds = visualEffectView.contentView.bounds
-      
-      return visualEffectView
-    } else {
-      let inputView = UIInputView(frame: frame, inputViewStyle: .keyboard)
-      inputView.addSubview(contentView)
-      
-      return inputView
-    }
+    #if compiler(>=6.0)
+      if #available(iOS 26.0, *) {
+        let padding = 20.0
+        let visualEffectView = UIVisualEffectView()
+        let glassEffect = UIGlassEffect()
+        glassEffect.tintColor = UIColor.black.withAlphaComponent(0.3)
+        glassEffect.isInteractive = true
+        visualEffectView.effect = glassEffect
+        visualEffectView.bounds = CGRect(x: 0, y: padding, width: frame.width - (2 * padding), height: frame.height)
+
+        // visualEffectView.bounds = CGRect(x: padding, y: padding, width: frame.width - 2 * padding, height: frame.height)
+        visualEffectView.overrideUserInterfaceStyle = FocusedInputHolder.shared.get()?.keyboardAppearanceValue == "dark" ? .dark : .light
+
+        visualEffectView.contentView.addSubview(contentView)
+        // contentView.bounds = visualEffectView.contentView.bounds
+
+        return visualEffectView
+      }
+    #endif
+    let inputView = UIInputView(frame: frame, inputViewStyle: .keyboard)
+    inputView.addSubview(contentView)
+
+    return inputView
   }
 }
