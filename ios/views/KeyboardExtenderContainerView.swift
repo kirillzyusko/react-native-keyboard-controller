e88408a5ed644565576cda4626f6fe669fb7ce3c
kirillzyusko <zyusko.kirik@gmail.com>
2025-07-12 11:29:46 +0200

    fix: change bottom padding

--- ios/views/KeyboardExtenderContainerView.swift
+++ ios/views/KeyboardExtenderContainerView.swift
@@ -12,7 +12,8 @@ public class KeyboardExtenderContainerView: UIView {
   @objc public static func create(frame: CGRect, contentView: UIView) -> UIView {
     if #available(iOS 26.0, *) {
       if let glassEffectClass = NSClassFromString("UIGlassEffect") as? UIVisualEffect.Type {
-        let padding = 20.0
+        let paddingHorizontal = 20.0
+        let paddingBottom = 10.0
         let isDark = FocusedInputHolder.shared.get()?.keyboardAppearanceValue == "dark"
 
         let glassEffect = glassEffectClass.init()
@@ -24,20 +25,20 @@ public class KeyboardExtenderContainerView: UIView {
         let wrapperView = UIView(frame: frame)
         wrapperView.backgroundColor = .clear
 
-        let innerWidth = frame.width - padding * 2
+        let innerWidth = frame.width - paddingHorizontal * 2
         let innerHeight = frame.height
 
         let visualEffectView = UIVisualEffectView(effect: glassEffect)
         visualEffectView.overrideUserInterfaceStyle = isDark ? .dark : .light
 
         visualEffectView.frame = CGRect(
-          x: padding,
-          y: -padding,
+          x: paddingHorizontal,
+          y: -paddingBottom,
           width: innerWidth,
           height: innerHeight
         )
         contentView.frame = CGRect(
-          x: -padding,
+          x: -paddingHorizontal,
           y: 0,
           width: innerWidth,
           height: innerHeight
