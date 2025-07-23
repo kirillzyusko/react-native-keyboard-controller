95d0293ca36a6ce53e7fb9c4b5aa117e9460c5e7
kirillzyusko <zyusko.kirik@gmail.com>
2025-07-23 22:03:47 +0200

    fix: do not use `BackgroundStyleApplicator` directly

--- android/src/main/java/com/reactnativekeyboardcontroller/views/background/KeyboardBackgroundViewGroup.kt
+++ android/src/main/java/com/reactnativekeyboardcontroller/views/background/KeyboardBackgroundViewGroup.kt
@@ -3,7 +3,6 @@ package com.reactnativekeyboardcontroller.views.background
 import android.annotation.SuppressLint
 import android.content.res.Configuration
 import android.view.WindowInsets
-import com.facebook.react.uimanager.BackgroundStyleApplicator
 import com.facebook.react.uimanager.ThemedReactContext
 import com.facebook.react.views.view.ReactViewGroup
 
@@ -14,18 +13,18 @@ class KeyboardBackgroundViewGroup(
   // view mounted
   override fun onAttachedToWindow() {
     super.onAttachedToWindow()
-    BackgroundStyleApplicator.setBackgroundColor(this, reactContext.getInputMethodColor())
+    super.setBackgroundColor(reactContext.getInputMethodColor())
   }
 
   // theme changed
   override fun onConfigurationChanged(newConfig: Configuration?) {
     super.onConfigurationChanged(newConfig)
-    BackgroundStyleApplicator.setBackgroundColor(this, reactContext.getInputMethodColor())
+    super.setBackgroundColor(reactContext.getInputMethodColor())
   }
 
   // keyboard changed
   override fun onApplyWindowInsets(insets: WindowInsets?): WindowInsets {
-    BackgroundStyleApplicator.setBackgroundColor(this, reactContext.getInputMethodColor())
+    super.setBackgroundColor(reactContext.getInputMethodColor())
     return super.onApplyWindowInsets(insets)
   }
 
