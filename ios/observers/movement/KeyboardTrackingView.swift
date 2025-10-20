kirillzyusko/react-native-keyboard-controller

commit 6503e23efd2dd149b432cc65d721e2fc5974a854

fix: iOS 26 + Modal (#1158)
## 📜 Description

Fixed a problem when events stop working on iOS 26 inside `Modal`.

## 💡 Motivation and Context

The current problem is that we attach a `keyboardLayoutGuide` view to
the `rootView`. However when non-transparent `Modal` gets shown, then
`rootView` will be detached and modal root view will be attached:

|App content|Modal shown|
|------------|-------------|
|<img width="910" height="1152" alt="image"
src="https://github.com/user-attachments/assets/491bfb7c-9f6e-40a9-8b02-cdbb33b68fae"
/>|<img width="838" height="1098" alt="image"
src="https://github.com/user-attachments/assets/379c2ba0-775d-429e-996e-6cf5656f9cc9"
/>|

The first attempt that I tried to do is to attach `keyboardLayoutGuide`
directly to the window. However in this case:
- I can not see `keyboardLayoutGuide` view
- it looks like it's not getting animated.

So I'm not sure if it's actually working method (maybe that
`keyboardLayoutGuide` gets overwritten later).

Anyway I found the other way. When keyboard appear we'll receive a
notification `UIWindow.didBecomeVisibleNotification` - from the handler
of this notification we can attach a `keyboardLayoutGuide` and it will
be animated when keyboard appears (even after initial attachment - if we
would use `keyboardWillShow` then this trick wouldn't work, because it's
too late to attach something there and rely on animated transitions - I
think `keyboardLayoutGuide` internally also relies on keyboard
notifications and if we attach it from keyboard notification then newly
mounted handler will not receive an event).

To assure better performance I decided to keep a reference to the view
to which handler has been attached. It allows us to skip re-creation in
case if view hasn't been changed.

Initial changes for
https://github.com/kirillzyusko/react-native-keyboard-controller/issues/1141

## 📢 Changelog

<!-- High level overview of important changes -->
<!-- For example: fixed status bar manipulation; added new types

ios/observers/movement/KeyboardTrackingView.swift
@@ -14,6 +14,7 @@ import UIKit
 final class KeyboardTrackingView: UIView {
   private var keyboardView: UIView? { KeyboardViewLocator.shared.resolve() }
   private var keyboardHeight = 0.0
+  private weak var currentAttachedView: UIView?
 
   static let invalidPosition: CGFloat = -.greatestFiniteMagnitude
 
@@ -54,27 +55,35 @@ final class KeyboardTrackingView: UIView {
       name: UIResponder.keyboardDidShowNotification,
       object: nil
     )
+    NotificationCenter.default.addObserver(
+      self,
+      selector: #selector(attachToTopmostView),
+      name: UIWindow.didBecomeVisibleNotification,
+      object: nil
+    )
+  }
 
-    guard
-      let window = UIApplication.shared.activeWindow,
-      let rootView = window.rootViewController?.view
-    else {
-      return
-    }
+  @objc private func attachToTopmostView() {
+    guard let topView = UIApplication.topViewController()?.view else { return }
 
-    rootView.addSubview(self)
+    if currentAttachedView === topView { return }
+
+    removeFromSuperview()
+
+    topView.addSubview(self)
+    currentAttachedView = topView
 
     translatesAutoresizingMaskIntoConstraints = false
 
     if #available(iOS 15.0, *) {
       if #available(iOS 17.0, *) {
-        rootView.keyboardLayoutGuide.usesBottomSafeArea = false
+        topView.keyboardLayoutGuide.usesBottomSafeArea = false
       }
 
       NSLayoutConstraint.activate([
-        leadingAnchor.constraint(equalTo: rootView.leadingAnchor, constant: 0),
-        trailingAnchor.constraint(equalTo: rootView.trailingAnchor, constant: 0),
-        bottomAnchor.constraint(equalTo: rootView.keyboardLayoutGuide.topAnchor, constant: 0),
+        leadingAnchor.constraint(equalTo: topView.leadingAnchor, constant: 0),
+        trailingAnchor.constraint(equalTo: topView.trailingAnchor, constant: 0),
+        bottomAnchor.constraint(equalTo: topView.keyboardLayoutGuide.topAnchor, constant: 0),
         heightAnchor.constraint(equalToConstant: 0),
       ])
     }

commit 3ab39a0eddb9a4b37a6f18a394e8e48c11a6832f

fix: attach keyboard tracking view in advance and only when window has been changed (#1170)
## 📜 Description

Fixed strange animation introduced by
`react-native-keyboard-controller`.

## 💡 Motivation and Context

This regression was introduced in
https://github.com/kirillzyusko/react-native-keyboard-controller/pull/1158
Basiucally I discovered 2 issues:

- the tracking view could be attached later to the window so we could
see "unreal" values, where keyboard height would be equal to whole
screen size and `progress` could be 2+
https://github.com/kirillzyusko/react-native-keyboard-controller/issues/1166#issuecomment-3419679794
- since we start to attach tracking view to each new window we can
attach it to system alerts etc.
https://github.com/kirillzyusko/react-native-keyboard-controller/issues/1169

So the manner that we choose was slightly agressive. The original issue
was caused by fact that tracking view gets detached when modal appears.
But when `Alert`/transparent `Modal` are shown, then we don't need to
re-attach a veiw, because old view is not detached. Additionally I
didin't like the fact that we subscribe to
`didBecomeVisibleNotification` event, which gets triggered every time
before the keyboard appears (potentially it may slow down the app).

So in this PR I decided to re-work the approach. Now instead of "try to
attach to top window root view" we follow approach "re-attach tracking
view only if current tracking view was detached". And this approach
works better because:
- we don't attach listener for each new window (attach it only when
necessary)
- we attach it **before** keyboard starts its movement.

So new solution seems to work better and fix all the issues caused by
its previous implementation.

Also I think it's worth to mention several solutions that didn't work:
- I tried to use KVO on `view.window` property and it didn't give a
desired behavior (listener was never called);
- I tried to avoid using `keyboardLayoutGuide`, but when keyboard

ios/observers/movement/KeyboardTrackingView.swift
@@ -15,6 +15,7 @@ final class KeyboardTrackingView: UIView {
   private var keyboardView: UIView? { KeyboardViewLocator.shared.resolve() }
   private var keyboardHeight = 0.0
   private weak var currentAttachedView: UIView?
+  private var isAttaching = false
 
   static let invalidPosition: CGFloat = -.greatestFiniteMagnitude
 
@@ -55,12 +56,14 @@ final class KeyboardTrackingView: UIView {
       name: UIResponder.keyboardDidShowNotification,
       object: nil
     )
-    NotificationCenter.default.addObserver(
-      self,
-      selector: #selector(attachToTopmostView),
-      name: UIWindow.didBecomeVisibleNotification,
-      object: nil
-    )
+    attachToTopmostView()
+  }
+
+  override func willMove(toWindow newWindow: UIWindow?) {
+    // When the view is being removed from the window, we need to re-attach it
+    if newWindow == nil, !isAttaching {
+      attachToTopmostView()
+    }
   }
 
   @objc private func attachToTopmostView() {
@@ -68,6 +71,7 @@ final class KeyboardTrackingView: UIView {
 
     if currentAttachedView === topView { return }
 
+    isAttaching = true
     removeFromSuperview()
 
     topView.addSubview(self)
@@ -87,6 +91,7 @@ final class KeyboardTrackingView: UIView {
         heightAnchor.constraint(equalToConstant: 0),
       ])
     }
+    isAttaching = false
   }
 
   @objc private func keyboardWillAppear(_ notification: Notification) {