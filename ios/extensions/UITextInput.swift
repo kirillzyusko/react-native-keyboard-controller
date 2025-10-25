85dd190efa3967811db7473df33053e6bb0be57d
kirillzyusko <zyusko.kirik@gmail.com>
2025-10-25 23:59:55 +0200

    fix: avoid `ensureLayout` calls

--- ios/extensions/UITextInput.swift
+++ ios/extensions/UITextInput.swift
@@ -11,11 +11,13 @@ import UIKit
 
 public extension UITextInput {
   var canSelectionFitIntoLayout: Bool {
-    guard let textView = self as? UITextView else { return true }
-
-    // Force layout to ensure accurate rect calculation
-    textView.layoutManager.ensureLayout(for: textView.textContainer)
-
+    if let selfObj = self as? NSObject,
+       let delegate = selfObj.value(forKey: "textInputDelegate") as? NSObject,
+       let comingFromJS = delegate.value(forKey: "_comingFromJS") as? Bool,
+       comingFromJS {
+      return false
+    }
+    
     guard let selectedRange = selectedTextRange else { return false }
 
     guard let range = textRange(from: selectedRange.start, to: selectedRange.end) else { return false }
