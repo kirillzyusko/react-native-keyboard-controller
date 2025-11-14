852fa4a22329f793a79b88b522892d773f405f28
Kirill Zyusko <zyusko.kirik@gmail.com>
2025-11-14 14:22:55 +0100

    fix: dynamic `bottomOffset` over-scrolling (#1204)
    ## 📜 Description
    
    Fixed an issue when change of `bottomSheet` with opened keyboard leads
    to unexpectedly big scroll.
    
    ## 💡 Motivation and Context
    
    The issue is that our `point`/`relativeScrollTo` remains the same. But
    when we focus our input we get `scrollPosition.value === 0`. When
    scrolling has completed we update that value to an actual scroll
    position (let's say to 209). When `bottomOffset` gets changed we scroll
    from current position (remember `point` hasn't been changed) but we add
    already scrolled distance and we get an incorrect offset.
    
    To overcome this issue we need to reset our scroll position to
    "unscrolled" state. In fact we already use that pattern with scroll
    restoration (change scroll position, call `maybeScroll`, restore
    position). So I decided to create a utility function called
    `performScrollWithRestoration` that will substitute scroll to a new
    position, do a scrolling and restore original position.
    
    The fix is working well, it fixes a problem in example app and e2e tests
    successfully pass 🤞
    
    Closes
    https://github.com/kirillzyusko/react-native-keyboard-controller/issues/1203
    
    ## 📢 Changelog
    
    <!-- High level overview of important changes -->
    <!-- For example: fixed status bar manipulation; added new types
    declarations; -->
    <!-- If your changes don't affect one of platform/language below - then
    remove this platform/language -->
    
    ### JS
    
    - added `performScrollWithRestoration` utility function;
    - call `performScrollWithRestoration` when `bottomOffset` gets changed
    (passing `scrollBeforeKeyboardMovement` value).
    
    ## 🤔 How Has This Been Tested?
    
    Tested manually in Fabric example, iPhone 16 Pro (iOS 26.0).
    
    ## 📸 Screenshots (if appropriate):
    
    |Before|After|
    |------|------|
    |<video
    src="https://github.com/user-attachments/assets/f01235d5-e94a-47fd-9221-fb74d4b495eb">|<video
    src="https://github.com/user-attachments/assets/865f0a90-1766-43c7-ab9a-4ff961ba76fe">|
    
    ## 📝 Checklist
    
    - [x] CI successfully passed
    - [x] I added new mocks and corresponding unit-tests if library API was
    changed

---
+++ e2e/kit/016-aware-scroll-view-with-sticky-view.e2e.ts
@@ -0,0 +1,35 @@
+import { expectBitmapsToBeEqual } from "./asserts";
+import {
+  scrollDownUntilElementIsVisible,
+  waitAndTap,
+  waitForExpect,
+} from "./helpers";
+
+const BLINKING_CURSOR = 0.35;
+
+describe("AwareScrollView with StickyView test cases", () => {
+  it("should push input above keyboard on focus", async () => {
+    await waitAndTap("aware_scroll_view_sticky_footer");
+    await scrollDownUntilElementIsVisible(
+      "aware_scroll_sticky_view_scroll_container",
+      "TextInput#4",
+    );
+    await waitAndTap("TextInput#4");
+    await waitForExpect(async () => {
+      await expectBitmapsToBeEqual(
+        "AwareScrollViewWithStickyViewFirstInputFocused",
+        BLINKING_CURSOR,
+      );
+    });
+  });
+
+  it("should react on `bottomOffset` change", async () => {
+    await waitAndTap("toggle_height");
+    await waitForExpect(async () => {
+      await expectBitmapsToBeEqual(
+        "AwareScrollViewWithStickyViewBottomOffsetChanged",
+        BLINKING_CURSOR,
+      );
+    });
+  });
+});
