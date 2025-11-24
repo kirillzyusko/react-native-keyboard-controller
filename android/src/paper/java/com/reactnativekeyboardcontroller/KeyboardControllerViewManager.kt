fdcffe525731eeba37a562a520ae538ee4b42282
kirillzyusko <zyusko.kirik@gmail.com>
2025-11-24 11:51:22 +0300

    feat: implement Android (paper)

--- android/src/paper/java/com/reactnativekeyboardcontroller/KeyboardControllerViewManager.kt
+++ android/src/paper/java/com/reactnativekeyboardcontroller/KeyboardControllerViewManager.kt
@@ -1,5 +1,6 @@
 package com.reactnativekeyboardcontroller
 
+import com.facebook.react.bridge.ReadableArray
 import com.facebook.react.uimanager.ThemedReactContext
 import com.facebook.react.uimanager.annotations.ReactProp
 import com.facebook.react.views.view.ReactViewGroup
@@ -57,6 +58,18 @@ class KeyboardControllerViewManager : ReactViewManager() {
     manager.setPreserveEdgeToEdge(view, isPreservingEdgeToEdge)
   }
   // endregion
+
+  // region Commands
+  override fun receiveCommand(root: ReactViewGroup, commandId: String, args: ReadableArray?) {
+    super.receiveCommand(root, commandId, args)
+
+    when (commandId) {
+      "synchronizeFocusedInputLayout" -> {
+        manager.synchronizeFocusedInputLayout(root as EdgeToEdgeReactViewGroup)
+      }
+    }
+  }
+  //endregion
 
   // region Getters
   override fun getExportedCustomDirectEventTypeConstants(): MutableMap<String, Any> =
