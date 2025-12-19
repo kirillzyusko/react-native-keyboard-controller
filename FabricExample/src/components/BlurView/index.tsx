922d724ef77f6141d00a8a5ea6c047d107ad9922
kirillzyusko <zyusko.kirik@gmail.com>
2025-12-19 12:10:37 +0100

    fix: blur view

--- FabricExample/src/components/BlurView/index.tsx
+++ FabricExample/src/components/BlurView/index.tsx
@@ -1,5 +1,24 @@
-function BlurView({children}) {
-  return children;
+import type { ViewStyle } from "react-native";
+
+type BlurViewProps = {
+  blurAmount: number;
+  blurType: string;
+  reducedTransparencyFallbackColor: string;
+  style: ViewStyle;
+  children?: React.ReactNode;
+};
+
+function BlurView({ blurAmount, children, style }: BlurViewProps) {
+  return (
+    <div
+      style={{
+        ...(style as React.CSSProperties),
+        filter: `blur(${blurAmount}px)`,
+      }}
+    >
+      {children}
+    </div>
+  );
 }
 
 export default BlurView;
