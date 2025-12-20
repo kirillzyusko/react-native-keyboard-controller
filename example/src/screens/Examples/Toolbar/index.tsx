9ddaeef7cfb328f11996badc407aa404d684c4df
Kirill Zyusko <zyusko.kirik@gmail.com>
2025-12-20 19:20:28 +0100

    feat: web example (#1254)
    ## 📜 Description
    
    Added web example for FabricExample.
    
    ## 💡 Motivation and Context
    
    This PR adds initial changes for
    https://github.com/kirillzyusko/react-native-keyboard-controller/discussions/43
    implementation. For now I just add:
    - example project;
    - job for CI to assure that build can be done successfully;
    - e2e test that assures that app can be running without runtime crashes.
    
    Later I'm planning to see how `VirtualKeyboard` API can be integrated in
    this package and whether it's possible to use any kind of polyfill for
    unsupported platforms. Also I'll explore how this package can be
    integrated in `WebView`.
    
    ## 📢 Changelog
    
    <!-- High level overview of important changes -->
    <!-- For example: fixed status bar manipulation; added new types
    declarations; -->
    <!-- If your changes don't affect one of platform/language below - then
    remove this platform/language -->
    
    ### JS
    
    - update react-navigation (couldn't make it working with navigation
    6.x);
    - polyfilled `BlurView`+`haptic` in example app;
    - added web example;
    - updated snapshots (testing-library);
    
    ### E2E
    
    - updated all assets for all platforms;
    - starts to use Maestro for web testing (maybe in future we'll migrate
    to Maestro as universal testing framework);
    
    ### CI
    
    - added build stage for web example;
    - added e2e test (maestro) that verifies that app is running
    successfully;
    
    ## 🤔 How Has This Been Tested?
    
    Tested via CI + manually on localhost:8080.
    
    ## 📸 Screenshots (if appropriate):
    
    |Main menu|Example|
    |-----------|---------|
    
    |![telegram-cloud-photo-size-2-5348379378095689628-y](https://github.com/user-attachments/assets/c4526831-b45d-432c-8bbd-771f0d8dd344)|<video
    src="https://github.com/user-attachments/assets/e55ee1c9-8f76-4f09-8460-95aa01a87d3e">|
    
    ## 📝 Checklist
    
    - [x] CI successfully passed
    - [x] I added new mocks and corresponding unit-tests if library API was
    changed

--- example/src/screens/Examples/Toolbar/index.tsx
+++ example/src/screens/Examples/Toolbar/index.tsx
@@ -1,14 +1,14 @@
-import { BlurView } from "@react-native-community/blur";
 import React, { useCallback, useEffect, useState } from "react";
 import { Modal, Platform, StyleSheet, Text, View } from "react-native";
-import { trigger } from "react-native-haptic-feedback";
 import {
   KeyboardAwareScrollView,
   KeyboardToolbar,
 } from "react-native-keyboard-controller";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 
+import BlurView from "../../../components/BlurView";
 import TextInput from "../../../components/TextInput";
+import trigger from "../../../modules/haptic";
 
 import AutoFillContacts from "./Contacts";
 
