ea1ca63854de7fcc70a2c081e22349582334972a
kirillzyusko <zyusko.kirik@gmail.com>
2026-01-28 09:49:23 +0100

    feat: complete playground test

--- src/components/KeyboardAwareScrollView/index.tsx
+++ src/components/KeyboardAwareScrollView/index.tsx
@@ -33,6 +33,7 @@ const OS = Platform.OS;
 const ReanimatedClippingScrollView =
   Reanimated.createAnimatedComponent(ClippingScrollView);
 
+import styles from "./styles";
 import { useSmoothKeyboardHandler } from "./useSmoothKeyboardHandler";
 import { debounce, scrollDistanceWithRespectToSnapPoints } from "./utils";
 
@@ -47,7 +48,6 @@ import type {
   FocusedInputSelectionChangedEvent,
   NativeEvent,
 } from "react-native-keyboard-controller";
-import styles from "./styles";
 
 export type KeyboardAwareScrollViewProps = {
   /** The distance between the keyboard and the caret inside a focused `TextInput` when the keyboard is shown. Default is `0`. */
