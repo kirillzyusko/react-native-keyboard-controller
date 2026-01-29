c7a4d4b794d66dc891037964b52e27cb8b34bf34
kirillzyusko <zyusko.kirik@gmail.com>
2026-01-29 12:08:32 +0100

    chore: revert old changes

--- example/src/screens/Examples/InteractiveKeyboardIOS/index.tsx
+++ example/src/screens/Examples/InteractiveKeyboardIOS/index.tsx
@@ -1,20 +1,96 @@
 import React, { useCallback, useRef, useState } from "react";
-import { TextInput, View } from "react-native";
+import { TextInput } from "react-native";
 import {
-  ChatKit,
   KeyboardGestureArea,
-  KeyboardStickyView,
+  useKeyboardHandler,
 } from "react-native-keyboard-controller";
+import Reanimated, {
+  useAnimatedProps,
+  useAnimatedScrollHandler,
+  useAnimatedStyle,
+  useSharedValue,
+} from "react-native-reanimated";
 
 import Message from "../../../components/Message";
 import { history } from "../../../components/Message/data";
 
-import styles, { TEXT_INPUT_HEIGHT, contentContainerStyle } from "./styles";
+import styles from "./styles";
 
-import type { LayoutChangeEvent, ScrollView } from "react-native";
+import type { LayoutChangeEvent } from "react-native";
+
+const AnimatedTextInput = Reanimated.createAnimatedComponent(TextInput);
+
+const useKeyboardAnimation = () => {
+  const progress = useSharedValue(0);
+  const height = useSharedValue(0);
+  const inset = useSharedValue(0);
+  const offset = useSharedValue(0);
+  const scroll = useSharedValue(0);
+  const shouldUseOnMoveHandler = useSharedValue(false);
+
+  useKeyboardHandler({
+    onStart: (e) => {
+      "worklet";
+
+      // i. e. the keyboard was under interactive gesture, and will be showed
+      // again. Since iOS will not schedule layout animation for that we can't
+      // simply update `height` to destination and we need to listen to `onMove`
+      // handler to have a smooth animation
+      if (progress.value !== 1 && progress.value !== 0 && e.height !== 0) {
+        // eslint-disable-next-line react-compiler/react-compiler
+        shouldUseOnMoveHandler.value = true;
+
+        return;
+      }
+
+      progress.value = e.progress;
+      height.value = e.height;
+
+      inset.value = e.height;
+      // Math.max is needed to prevent overscroll when keyboard hides (and user scrolled to the top, for example)
+      offset.value = Math.max(e.height + scroll.value, 0);
+    },
+    onInteractive: (e) => {
+      "worklet";
+
+      progress.value = e.progress;
+      height.value = e.height;
+    },
+    onMove: (e) => {
+      "worklet";
+
+      if (shouldUseOnMoveHandler.value) {
+        progress.value = e.progress;
+        height.value = e.height;
+      }
+    },
+    onEnd: (e) => {
+      "worklet";
+
+      height.value = e.height;
+      progress.value = e.progress;
+      shouldUseOnMoveHandler.value = false;
+    },
+  });
+
+  const onScroll = useAnimatedScrollHandler({
+    onScroll: (e) => {
+      scroll.value = e.contentOffset.y - inset.value;
+    },
+  });
+
+  return { height, progress, onScroll, inset, offset };
+};
+
+const TEXT_INPUT_HEIGHT = 50;
+
+const contentContainerStyle = {
+  paddingBottom: TEXT_INPUT_HEIGHT,
+};
 
 function InteractiveKeyboard() {
-  const ref = useRef<ScrollView>(null);
+  const ref = useRef<Reanimated.ScrollView>(null);
+  const { height, onScroll, inset, offset } = useKeyboardAnimation();
   const [inputHeight, setInputHeight] = useState(TEXT_INPUT_HEIGHT);
   const [text, setText] = useState("");
 
@@ -26,37 +102,59 @@ function InteractiveKeyboard() {
     ref.current?.scrollToEnd({ animated: false });
   }, []);
 
+  const textInputStyle = useAnimatedStyle(
+    () => ({
+      position: "absolute",
+      minHeight: TEXT_INPUT_HEIGHT,
+      width: "100%",
+      backgroundColor: "#BCBCBC",
+      transform: [{ translateY: -height.value }],
+    }),
+    [],
+  );
+
+  const props = useAnimatedProps(() => ({
+    contentInset: {
+      bottom: inset.value,
+    },
+    contentOffset: {
+      x: 0,
+      y: offset.value,
+    },
+  }));
+
   return (
-    <View
+    <KeyboardGestureArea
       offset={inputHeight}
       style={styles.container}
-      
       textInputNativeID="chat-input"
     >
-      <ChatKit.ScrollView
+      <Reanimated.ScrollView
+        ref={ref}
+        // simulation of `automaticallyAdjustKeyboardInsets` behavior on RN < 0.73
+        animatedProps={props}
         automaticallyAdjustContentInsets={false}
         contentContainerStyle={contentContainerStyle}
         contentInsetAdjustmentBehavior="never"
         keyboardDismissMode="interactive"
         testID="chat.scroll"
-        // onContentSizeChange={scrollToBottom}
+        onContentSizeChange={scrollToBottom}
+        onScroll={onScroll}
       >
         {history.map((message, index) => (
           <Message key={index} {...message} />
         ))}
-      </ChatKit.ScrollView>
-      <KeyboardStickyView style={styles.composer}>
-        <TextInput
-          multiline
-          nativeID="chat-input"
-          style={styles.input}
-          testID="chat.input"
-          value={text}
-          onChangeText={setText}
-          onLayout={onInputLayoutChanged}
-        />
-      </KeyboardStickyView>
-    </View>
+      </Reanimated.ScrollView>
+      <AnimatedTextInput
+        multiline
+        nativeID="chat-input"
+        style={textInputStyle}
+        testID="chat.input"
+        value={text}
+        onChangeText={setText}
+        onLayout={onInputLayoutChanged}
+      />
+    </KeyboardGestureArea>
   );
 }
 
