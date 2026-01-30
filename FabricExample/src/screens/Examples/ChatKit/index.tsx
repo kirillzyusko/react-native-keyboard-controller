17968d388028a74fb5e9aba0f8618d93f4840f73
kirillzyusko <zyusko.kirik@gmail.com>
2026-01-30 19:54:00 +0100

    fix: use global zustand store for app playground, split code into smaller parts, fixes for inverted FlatList

--- FabricExample/src/screens/Examples/ChatKit/index.tsx
+++ FabricExample/src/screens/Examples/ChatKit/index.tsx
@@ -1,12 +1,9 @@
-import BottomSheet, { BottomSheetView } from "@gorhom/bottom-sheet";
 import { LegendList } from "@legendapp/list";
 import { FlashList } from "@shopify/flash-list";
 import React, { useCallback, useEffect, useRef, useState } from "react";
 import {
-  Button,
   FlatList,
   Image,
-  Text,
   TextInput,
   TouchableOpacity,
   View,
@@ -18,32 +15,24 @@ import {
 } from "react-native-keyboard-controller";
 
 import Message from "../../../components/Message";
-import Switch from "../../../components/Switch";
 
-import { messages as initial } from "./data";
-import styles, { TEXT_INPUT_HEIGHT, contentContainerStyle } from "./styles";
+import ConfigSheet from "./config";
+import { useChatConfigStore } from "./store";
+import styles, {
+  TEXT_INPUT_HEIGHT,
+  contentContainerStyle,
+  invertedContentContainerStyle,
+} from "./styles";
 
-import type { MessageProps } from "../../../components/Message/types";
-import type { ExamplesStackParamList } from "../../../navigation/ExamplesStack";
-import type { StackScreenProps } from "@react-navigation/stack";
 import type { LayoutChangeEvent, ScrollView } from "react-native";
 
-type Props = StackScreenProps<ExamplesStackParamList>;
-
-function ChatKitPlayground({ navigation }: Props) {
+function ChatKitPlayground() {
   const ref = useRef<ScrollView>(null);
   const textInputRef = useRef<TextInput>(null);
   const textRef = useRef("");
   const [inputHeight, setInputHeight] = useState(TEXT_INPUT_HEIGHT);
-  const [inverted, setInverted] = useState(false);
-  const [beginning, setBeginning] = useState(false);
-  const [messages, setMessages] = useState<MessageProps[]>(initial);
-
-  const bottomSheetModalRef = useRef<BottomSheet>(null);
-
-  const handlePresentModalPress = useCallback(() => {
-    bottomSheetModalRef.current?.expand();
-  }, []);
+  const { inverted, messages, reversedMessages, addMessage, mode } =
+    useChatConfigStore();
 
   const onInputLayoutChanged = useCallback((e: LayoutChangeEvent) => {
     setInputHeight(e.nativeEvent.layout.height);
@@ -52,36 +41,14 @@ function ChatKitPlayground({ navigation }: Props) {
     textRef.current = text;
   }, []);
   const onSend = useCallback(() => {
-    setMessages((m) => [...m, { text: textRef.current, sender: true }]);
+    addMessage({ text: textRef.current, sender: true });
     textInputRef.current?.clear();
     textRef.current = "";
-  }, []);
+  }, [addMessage]);
 
   useEffect(() => {
     ref.current?.scrollToEnd({ animated: true });
   }, [messages]);
-
-  useEffect(() => {
-    if (beginning) {
-      setMessages([initial[0]]);
-    } else {
-      setMessages(initial);
-    }
-  }, [beginning]);
-
-  useEffect(() => {
-    navigation.setOptions({
-      headerRight: () => (
-        <Text
-          style={styles.header}
-          testID="open_bottom_sheet_modal"
-          onPress={handlePresentModalPress}
-        >
-          Open config
-        </Text>
-      ),
-    });
-  }, [navigation]);
 
   const scrollToBottom = useCallback(() => {
     ref.current?.scrollToEnd({ animated: false });
@@ -94,38 +61,44 @@ function ChatKitPlayground({ navigation }: Props) {
         style={styles.container}
         textInputNativeID="chat-input"
       >
-        <LegendList
-          maintainScrollAtEnd
-          maintainVisibleContentPosition
-          data={messages}
-          inverted={inverted}
-          keyExtractor={(item) => item.text}
-          renderItem={({ item }) => <Message {...item} />}
-          renderScrollComponent={(props) => (
-            <ChatKit.ScrollView
-              ref={ref}
-              automaticallyAdjustContentInsets={false}
-              contentContainerStyle={contentContainerStyle}
-              contentInsetAdjustmentBehavior="never"
-              inverted={inverted}
-              keyboardDismissMode="interactive"
-              testID="chat.scroll"
-              {...props}
-            />
-          )}
-        />
-        {/*<ChatKit.ScrollView
-          ref={ref}
-          automaticallyAdjustContentInsets={false}
-          contentContainerStyle={contentContainerStyle}
-          contentInsetAdjustmentBehavior="never"
-          keyboardDismissMode="interactive"
-          testID="chat.scroll"
-        >
-          {messages.map((message, index) => (
-            <Message key={index} {...message} />
-          ))}
-        </ChatKit.ScrollView>*/}
+        {mode === "flat" && (
+          <FlatList
+            data={inverted ? messages : reversedMessages}
+            inverted={inverted}
+            keyExtractor={(item) => item.text}
+            renderItem={({ item }) => <Message {...item} />}
+            renderScrollComponent={(props) => (
+              <ChatKit.ScrollView
+                ref={ref}
+                automaticallyAdjustContentInsets={false}
+                contentContainerStyle={
+                  inverted
+                    ? invertedContentContainerStyle
+                    : contentContainerStyle
+                }
+                contentInsetAdjustmentBehavior="never"
+                inverted={inverted}
+                keyboardDismissMode="interactive"
+                testID="chat.scroll"
+                {...props}
+              />
+            )}
+          />
+        )}
+        {mode === "scroll" && (
+          <ChatKit.ScrollView
+            ref={ref}
+            automaticallyAdjustContentInsets={false}
+            contentContainerStyle={contentContainerStyle}
+            contentInsetAdjustmentBehavior="never"
+            keyboardDismissMode="interactive"
+            testID="chat.scroll"
+          >
+            {messages.map((message, index) => (
+              <Message key={index} {...message} />
+            ))}
+          </ChatKit.ScrollView>
+        )}
         <KeyboardStickyView style={styles.composer}>
           <TextInput
             ref={textInputRef}
@@ -141,35 +114,7 @@ function ChatKitPlayground({ navigation }: Props) {
           </TouchableOpacity>
         </KeyboardStickyView>
       </View>
-      <BottomSheet ref={bottomSheetModalRef} index={-1} snapPoints={["40%"]}>
-        <BottomSheetView style={styles.bottomSheetContent}>
-          <Button
-            testID="bottom_sheet_close_modal"
-            title="Close modal"
-            onPress={() => bottomSheetModalRef.current?.close()}
-          />
-          <View style={styles.switchContainer}>
-            <Text>Toggle inverted</Text>
-            <Switch
-              testID="bottom_sheet_toggle_inverted_state"
-              value={inverted}
-              onChange={() => {
-                setInverted((i) => !i);
-              }}
-            />
-          </View>
-          <View style={styles.switchContainer}>
-            <Text>Toggle beginning</Text>
-            <Switch
-              testID="bottom_sheet_toggle_beginning_state"
-              value={beginning}
-              onChange={() => {
-                setBeginning((b) => !b);
-              }}
-            />
-          </View>
-        </BottomSheetView>
-      </BottomSheet>
+      <ConfigSheet />
     </>
   );
 }
