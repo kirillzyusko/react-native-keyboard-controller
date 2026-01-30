4b60b30867d7c6530661f4984b5c4d8ae7600805
kirillzyusko <zyusko.kirik@gmail.com>
2026-01-30 19:54:00 +0100

    fix: use global zustand store for app playground, split code into smaller parts, fixes for inverted FlatList

---
+++ FabricExample/src/screens/Examples/ChatKit/config.tsx
@@ -0,0 +1,127 @@
+import BottomSheet, { BottomSheetView } from "@gorhom/bottom-sheet";
+import { useNavigation } from "@react-navigation/native";
+import React, { useCallback, useEffect, useRef } from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
+
+import Switch from "../../../components/Switch";
+
+import { useChatConfigStore } from "./store";
+
+function ConfigSheet() {
+  const navigation = useNavigation();
+  const bottomSheetModalRef = useRef<BottomSheet>(null);
+  const inverted = useChatConfigStore((state) => state.inverted);
+  const setInverted = useChatConfigStore((state) => state.setInverted);
+  const beginning = useChatConfigStore((state) => state.beginning);
+  const setBeginning = useChatConfigStore((state) => state.setBeginning);
+  const mode = useChatConfigStore((state) => state.mode);
+  const setMode = useChatConfigStore((state) => state.setMode);
+
+  const handlePresentModalPress = useCallback(() => {
+    bottomSheetModalRef.current?.expand();
+  }, []);
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <Text
+          style={styles.header}
+          testID="open_bottom_sheet_modal"
+          onPress={handlePresentModalPress}
+        >
+          Open config
+        </Text>
+      ),
+    });
+  }, [navigation]);
+
+  return (
+    <BottomSheet ref={bottomSheetModalRef} index={-1} snapPoints={["50%"]}>
+      <BottomSheetView style={styles.bottomSheetContent}>
+        <Button
+          testID="bottom_sheet_close_modal"
+          title="Close modal"
+          onPress={() => bottomSheetModalRef.current?.close()}
+        />
+        <View style={styles.switchContainer}>
+          <Text>Toggle inverted</Text>
+          <Switch
+            testID="bottom_sheet_toggle_inverted_state"
+            value={inverted}
+            onChange={() => {
+              setInverted(!inverted);
+            }}
+          />
+        </View>
+        <View style={styles.switchContainer}>
+          <Text>Toggle beginning</Text>
+          <Switch
+            testID="bottom_sheet_toggle_beginning_state"
+            value={beginning}
+            onChange={() => {
+              setBeginning(!beginning);
+            }}
+          />
+        </View>
+        <View style={styles.switchContainer}>
+          <Text>Use Scroll View</Text>
+          <Switch
+            testID="bottom_sheet_toggle_scroll_view_list_state"
+            value={mode === "scroll"}
+            onChange={() => {
+              setMode("scroll");
+            }}
+          />
+        </View>
+        <View style={styles.switchContainer}>
+          <Text>Use Flat List</Text>
+          <Switch
+            testID="bottom_sheet_toggle_flat_list_state"
+            value={mode === "flat"}
+            onChange={() => {
+              setMode("flat");
+            }}
+          />
+        </View>
+        <View style={styles.switchContainer}>
+          <Text>Use Flash List</Text>
+          <Switch
+            testID="bottom_sheet_toggle_flash_list_state"
+            value={mode === "flash"}
+            onChange={() => {
+              setMode("flash");
+            }}
+          />
+        </View>
+        <View style={styles.switchContainer}>
+          <Text>Use Legend List</Text>
+          <Switch
+            testID="bottom_sheet_toggle_legend_list_state"
+            value={mode === "legend"}
+            onChange={() => {
+              setMode("legend");
+            }}
+          />
+        </View>
+      </BottomSheetView>
+    </BottomSheet>
+  );
+}
+
+const styles = StyleSheet.create({
+  header: {
+    color: "black",
+    paddingRight: 12,
+  },
+  bottomSheetContent: {
+    flex: 1,
+  },
+  switchContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    margin: 16,
+  },
+});
+
+export default ConfigSheet;
---
+++ FabricExample/src/screens/Examples/ChatKit/store.ts
@@ -0,0 +1,46 @@
+import { create } from "zustand";
+
+import { messages as initialMessages } from "./data";
+
+import type { MessageProps } from "../../../components/Message/types";
+
+type ListMode = "scroll" | "flat" | "flash" | "legend";
+
+interface ChatConfigStore {
+  inverted: boolean;
+  setInverted: (inverted: boolean) => void;
+  beginning: boolean;
+  setBeginning: (beginning: boolean) => void;
+  messages: MessageProps[];
+  reversedMessages: MessageProps[];
+  setMessages: (messages: MessageProps[]) => void;
+  addMessage: (message: MessageProps) => void;
+  mode: ListMode;
+  setMode: (mode: ListMode) => void;
+  resetMessages: () => void;
+}
+
+// TODO: why do we need to use reversedMessages always?
+
+export const useChatConfigStore = create<ChatConfigStore>((set, get) => ({
+  inverted: false,
+  setInverted: (inverted) => set({ inverted }),
+  beginning: false,
+  setBeginning: (beginning) => {
+    set(() => {
+      const newMessages = beginning ? [initialMessages[0]] : initialMessages;
+
+      return { beginning, messages: newMessages };
+    });
+  },
+  messages: initialMessages,
+  get reversedMessages() {
+    return get().messages.slice().reverse();
+  },
+  setMessages: (messages) => set({ messages }),
+  addMessage: (message) =>
+    set((state) => ({ messages: [...state.messages, message] })),
+  mode: "scroll",
+  setMode: (mode) => set({ mode }),
+  resetMessages: () => set({ messages: initialMessages }),
+}));
