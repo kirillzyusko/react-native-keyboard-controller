e7168f2daa83c317e7286da364e523d01b8095b9
kirillzyusko <zyusko.kirik@gmail.com>
2026-03-10 12:49:23 +0100

    fix: fabric builds

--- android/src/fabric/java/com/reactnativekeyboardcontroller/KeyboardToolbarGroupViewManager.kt
+++ android/src/fabric/java/com/reactnativekeyboardcontroller/KeyboardToolbarGroupViewManager.kt
@@ -7,11 +7,10 @@ import com.facebook.react.viewmanagers.KeyboardToolbarGroupViewManagerDelegate
 import com.facebook.react.viewmanagers.KeyboardToolbarGroupViewManagerInterface
 import com.reactnativekeyboardcontroller.managers.KeyboardToolbarGroupViewManagerImpl
 import com.reactnativekeyboardcontroller.views.KeyboardToolbarGroupReactViewGroup
-import com.reactnativekeyboardcontroller.views.overlay.OverKeyboardHostView
 
 class KeyboardToolbarGroupViewManager :
   ViewGroupManager<KeyboardToolbarGroupReactViewGroup>(),
-  KeyboardToolbarGroupViewManagerInterface<OverKeyboardHostView> {
+  KeyboardToolbarGroupViewManagerInterface<KeyboardToolbarGroupReactViewGroup> {
   private val manager = KeyboardToolbarGroupViewManagerImpl()
   private val mDelegate = KeyboardToolbarGroupViewManagerDelegate(this)
 
