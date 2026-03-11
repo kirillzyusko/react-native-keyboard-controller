f5dca3cdf96142bb160c9edfea349375053fca65
Kirill Zyusko <zyusko.kirik@gmail.com>
2026-03-11 14:31:56 +0100

    fix: `KeyboardAvoidingView` types union (#1354)
    ## 📜 Description
    
    Removed union type for `KeyboardAvodiingView`.
    
    ## 💡 Motivation and Context
    
    Initially I thought that changes in
    https://github.com/kirillzyusko/react-native-keyboard-controller/pull/711
    are cool, but in reality it causes more headache to devs because TS not
    always can understand which behavior passed etc. So after all attempts
    the easiest option is to remove conditional types.
    
    Closes
    https://github.com/kirillzyusko/react-native-keyboard-controller/issues/1340
    
    ## 📢 Changelog
    
    <!-- High level overview of important changes -->
    <!-- For example: fixed status bar manipulation; added new types
    declarations; -->
    <!-- If your changes don't affect one of platform/language below - then
    remove this platform/language -->
    
    ### JS
    
    - don't use union types for `KeyboardAvoidingView` as it causes many TS
    errors where code is valid;
    
    ### Example
    
    - ported automatic mode to FabricExample project;
    - moved options to separate file;
    
    ## 🤔 How Has This Been Tested?
    
    Tested manually on iPhone 17 Pro (iOS 26.2).
    
    ## 📝 Checklist
    
    - [x] CI successfully passed
    - [x] I added new mocks and corresponding unit-tests if library API was
    changed

---
+++ example/src/navigation/ExamplesStack/options.ts
@@ -0,0 +1,97 @@
+import { ScreenNames } from "../../constants/screenNames";
+
+export const options = {
+  [ScreenNames.ANIMATED_EXAMPLE]: {
+    title: "Keyboard animation ⌨️",
+  },
+  [ScreenNames.REANIMATED_CHAT]: {
+    title: "Chat",
+  },
+  [ScreenNames.REANIMATED_CHAT_FLAT_LIST]: {
+    title: "Chat FlatList",
+  },
+  [ScreenNames.EVENTS]: {
+    title: "Events",
+  },
+  [ScreenNames.AWARE_SCROLL_VIEW]: {
+    title: "Aware scroll view",
+  },
+  [ScreenNames.AWARE_SCROLL_VIEW_STICKY_FOOTER]: {
+    title: "Aware scroll view sticky footer",
+  },
+  [ScreenNames.STATUS_BAR]: {
+    headerShown: false,
+    title: "Status bar manipulation",
+  },
+  [ScreenNames.LOTTIE]: {
+    title: "Lottie animation",
+  },
+  [ScreenNames.NON_UI_PROPS]: {
+    title: "Non UI Props",
+  },
+  [ScreenNames.INTERACTIVE_KEYBOARD]: {
+    title: "Interactive keyboard",
+  },
+  [ScreenNames.INTERACTIVE_KEYBOARD_IOS]: {
+    title: "Interactive keyboard 🍏",
+  },
+  [ScreenNames.NATIVE_STACK]: {
+    title: "Native stack",
+  },
+  [ScreenNames.KEYBOARD_AVOIDING_VIEW]: {
+    title: "KAV",
+  },
+  [ScreenNames.KEYBOARD_AVOIDING_VIEW_AUTOMATIC]: {
+    title: "KAV Automatic",
+  },
+  [ScreenNames.ENABLED_DISABLED]: {
+    title: "Enabled/disabled",
+  },
+  [ScreenNames.CLOSE]: {
+    title: "Close keyboard",
+  },
+  [ScreenNames.FOCUSED_INPUT_HANDLERS]: {
+    title: "Focused input handlers",
+  },
+  [ScreenNames.TOOLBAR]: {
+    title: "Toolbar",
+  },
+  [ScreenNames.MODAL]: {
+    title: "Modal",
+  },
+  [ScreenNames.BOTTOM_TAB_BAR]: {
+    headerShown: false,
+  },
+  [ScreenNames.OVER_KEYBOARD_VIEW]: {
+    title: "View over keyboard",
+  },
+  [ScreenNames.IMAGE_GALLERY]: {
+    title: "Image gallery",
+  },
+  [ScreenNames.USE_KEYBOARD_STATE]: {
+    title: "useKeyboardState",
+  },
+  [ScreenNames.LIQUID_KEYBOARD]: {
+    title: "Liquid keyboard",
+    headerShown: false,
+  },
+  [ScreenNames.KEYBOARD_SHARED_TRANSITIONS]: {
+    title: "Keyboard shared transitions",
+    headerShown: false,
+  },
+  [ScreenNames.KEYBOARD_EXTENDER]: {
+    title: "Keyboard Extender",
+    headerShown: false,
+  },
+  [ScreenNames.CHAT_KIT]: {
+    title: "Chat Kit",
+    headerTintColor: "#fff",
+    headerStyle: {
+      backgroundColor: "#3A3A3C",
+    },
+    headerBackTitle: "Back",
+  },
+  [ScreenNames.AI_LEGEND_LIST_CHAT]: {
+    title: "AI LegendList Chat",
+  },
+};
