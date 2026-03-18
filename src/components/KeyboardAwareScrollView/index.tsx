kirillzyusko/react-native-keyboard-controller

commit 6872736017cfdc32f491b150efec3bf5b0e11db8

fix: automatically detect top border of `KeyboardAwareScrollView` (#1352)
## 📜 Description

Automatically detect top border of `KeyboardAwareScrollView`.

## 💡 Motivation and Context

Continue the epic with better discovery of component location on the
screen and logical continuation of
https://github.com/kirillzyusko/react-native-keyboard-controller/pull/1346

In this PR I started to detect relative position of `ScrollView`, so
that I better understand if caret is not visible because it obscured by
other elements (header etc.) It's still not perfectly implemented and
there is still a "blind"/"dead" zone where text is already hidden but
scroll doesn't happen. I'll fix it in following PRs but for now I just
want to bring these changes to upcoming `1.21.0` release 🤞

We also can't use `measureInWindow` because it produces incorrect
measurements, so we need to use our custom implementation that has been
added in
https://github.com/kirillzyusko/react-native-keyboard-controller/pull/1355

Significantly improves UI for behavior described in
https://github.com/kirillzyusko/react-native-keyboard-controller/issues/1341

## 📢 Changelog

<!-- High level overview of important changes -->
<!-- For example: fixed status bar manipulation; added new types
declarations; -->
<!-- If your changes don't affect one of platform/language below - then
remove this platform/language -->

### JS

- auto detect position of `KeyboardAwareScrollView` on the screen to
better understand top border of the component relative to screen;

## 🤔 How Has This Been Tested?

Tested manually on iPhone 17 Pro (iOS 26.2, simulator).

## 📸 Screenshots (if appropriate):


https://github.com/user-attachments/assets/43fe233f-9c83-40c9-9642-a68e2d8e8e7c

## 📝 Checklist

- [x] CI successfully passed
- [x] I added new mocks and corresponding unit-tests if library API was
changed

src/components/KeyboardAwareScrollView/index.tsx
@@ -16,6 +16,7 @@ import Reanimated, {
   useSharedValue,
 } from "react-native-reanimated";
 
+import { KeyboardControllerNative } from "../../bindings";
 import {
   useFocusedInputHandler,
   useReanimatedFocusedInput,
@@ -150,14 +151,25 @@ const KeyboardAwareScrollView = forwardRef<
       useSharedValue<FocusedInputSelectionChangedEvent | null>(null);
     const ghostViewSpace = useSharedValue(-1);
     const pendingSelectionForFocus = useSharedValue(false);
+    const scrollViewPageY = useSharedValue(0);
 
     const { height } = useWindowDimensions();
 
     const onScrollViewLayout = useCallback(
-      (e: LayoutChangeEvent) => {
-        scrollViewTarget.value = findNodeHandle(scrollViewAnimatedRef.current);
+      async (e: LayoutChangeEvent) => {
+        const handle = findNodeHandle(scrollViewAnimatedRef.current);
+
+        scrollViewTarget.value = handle;
 
         onLayout?.(e);
+
+        if (handle !== null) {
+          const { y } = await KeyboardControllerNative.viewPositionInWindow(
+            handle,
+          );
+
+          scrollViewPageY.value = y;
+        }
       },
       [onLayout],
     );
@@ -205,7 +217,7 @@ const KeyboardAwareScrollView = forwardRef<
           return interpolatedScrollTo;
         }
 
-        if (point < 0) {
+        if (point < scrollViewPageY.value) {
           const positionOnScreen = visibleRect - bottomOffset;
           const topOfScreen = scrollPosition.value + point;
 

commit ecd3bbd8e337cfde910fb4cedd0319e0503448f2

fix: could not fing view for tag warning (#1379)
## 📜 Description

Silent `could not fing view for tag` warning for
`KeyboardAwareScrollView` (similarly how it works for
`KeyboardAvoidingView` now).

## 💡 Motivation and Context

This issue got introduced after
https://github.com/kirillzyusko/react-native-keyboard-controller/pull/1352
(which was based on
https://github.com/kirillzyusko/react-native-keyboard-controller/pull/1346)

In certain cases `onLayout` may be triggered but view may be not laid
out yet (if you use it with `react-native-pager` lazy). Since changes in
https://github.com/kirillzyusko/react-native-keyboard-controller/pull/1352
were purely additional (we try to understand the position of element on
the screen, but before these changes we always assumed we were in the
top of the screen) I decided to wrap it for now in `try/catch` block.

Silenting error is not a correct fix, but this is the best what we can
do now - we will not spam logs when devs update the package (in
https://github.com/kirillzyusko/react-native-keyboard-controller/pull/1346
we also used `catch` block), but yes, in certain cases detection of top
border will not work (and it didn't work before, so it's fair enough to
silent the error).

Closes
https://github.com/kirillzyusko/react-native-keyboard-controller/issues/1375

## 📢 Changelog

<!-- High level overview of important changes -->
<!-- For example: fixed status bar manipulation; added new types
declarations; -->
<!-- If your changes don't affect one of platform/language below - then
remove this platform/language -->

### JS

- handle rejected promises within `try/catch` block;

## 🤔 How Has This Been Tested?

Tested via e2e tests, I didn't test changes manually because I don't
have a repro.

## 📝 Checklist

- [x] CI successfully passed
- [x] I added new mocks and corresponding unit-tests if library API was
changed

src/components/KeyboardAwareScrollView/index.tsx
@@ -164,11 +164,15 @@ const KeyboardAwareScrollView = forwardRef<
         onLayout?.(e);
 
         if (handle !== null) {
-          const { y } = await KeyboardControllerNative.viewPositionInWindow(
-            handle,
-          );
+          try {
+            const { y } = await KeyboardControllerNative.viewPositionInWindow(
+              handle,
+            );
 
-          scrollViewPageY.value = y;
+            scrollViewPageY.value = y;
+          } catch {
+            // ignore
+          }
         }
       },
       [onLayout],