kirillzyusko/react-native-keyboard-controller

commit bba6afc9eb3183cb295ebd3a14eabb87bad257b9

perf: don't change `currentKeyboardFrame` each frame (#1381)
## 📜 Description

Don't change keyboard padding in `KeyboardAwareScrollView` each frame as
keyboard moves and do it only in the beginning of the animation (when
keyboard appears) or in the end of the animation (when keyboard closed).

## 💡 Motivation and Context

After
https://github.com/kirillzyusko/react-native-keyboard-controller/pull/797
introduced removal of `ghost padding` we don't need to change `inset`
every frame. Instead we can use an optimized approach and:
- add full keyboard frame padding only in the beginning of the animation
(when keyboard appears)
- remove  full keyboard frame padding when keyboard fully closed.

Roughly it gives us `2x` less load on UI thread because now we only need
to adjust scroll position inside `onMove` handler. A similar approach is
already used in `KeyboardChatScrollView` (we also change padding only
one time there). So these changes just makes components consistent.

## 📢 Changelog

<!-- High level overview of important changes -->
<!-- For example: fixed status bar manipulation; added new types
declarations; -->
<!-- If your changes don't affect one of platform/language below - then
remove this platform/language -->

### JS

- don't change `currentKeyboardFrame` each frame;

## 🤔 How Has This Been Tested?

Tested manually on iPhone 17 Pro (iOS 26.2, simulator), Pixel 7 Pro (API
36, real device).

## 📸 Screenshots (if appropriate):

|iOS|Android|
|---|--------|
|<video
src="https://github.com/user-attachments/assets/9e5f72cc-b6c7-4ea5-999f-d5c56c046809">|<video
src="https://github.com/user-attachments/assets/31f5a675-2de8-4136-91fe-7390364bbb97">|

## 📝 Checklist

- [x] CI successfully passed
- [x] I added new mocks and corresponding unit-tests if library API was
changed

src/components/KeyboardAwareScrollView/index.tsx
@@ -416,6 +416,7 @@ const KeyboardAwareScrollView = forwardRef<
             keyboardWillChangeSize ||
             focusWasChanged
           ) {
+            syncKeyboardFrame(e);
             // persist scroll value
             scrollPosition.value = position.value;
             // just persist height - later will be used in interpolation
@@ -463,8 +464,6 @@ const KeyboardAwareScrollView = forwardRef<
         },
         onMove: (e) => {
           "worklet";
-
-          syncKeyboardFrame(e);
 
           if (removeGhostPadding(e.height)) {
             return;

commit 90dd77289e9709275241e8e63252a208809c2dfe

fix: `KeyboardAwareScrollView` regression after optimization (#1387)
## 📜 Description

Call `syncKeyboardFrame` after `keyboardHeight` has been set.

## 💡 Motivation and Context

If we set call `syncKeyboardFrame` and `keyboardHeight = 0`, then
interpolation simply doesn't work we interpolate between `[0, 0]` to
`[0, 0]`, so we get `0`. This regression has been introduced after
https://github.com/kirillzyusko/react-native-keyboard-controller/pull/1381
changes and in this follow-up PR I'm fixing it 🤞

The fix is quite straightforward - we just need to call
`syncKeyboardFrame` after updating `keyboardHeight` variable.

Closes
https://github.com/kirillzyusko/react-native-keyboard-controller/issues/1385

## 📢 Changelog

<!-- High level overview of important changes -->
<!-- For example: fixed status bar manipulation; added new types
declarations; -->
<!-- If your changes don't affect one of platform/language below - then
remove this platform/language -->

### JS

- call `syncKeyboardFrame` after `keyboardHeight` has been set.

## 🤔 How Has This Been Tested?

Tested manually on iPhone 17 Pro (iOS 26.2).

## 📸 Screenshots (if appropriate):

|Before|After|
|-------|-----|
|<video
src="https://github.com/user-attachments/assets/70e2fef8-81b0-455d-b877-98067dfd3965">|<video
src="https://github.com/user-attachments/assets/08113d77-89b3-4b42-8d0e-f6334c05c602">|

## 📝 Checklist

- [x] CI successfully passed
- [x] I added new mocks and corresponding unit-tests if library API was
changed

src/components/KeyboardAwareScrollView/index.tsx
@@ -416,11 +416,12 @@ const KeyboardAwareScrollView = forwardRef<
             keyboardWillChangeSize ||
             focusWasChanged
           ) {
-            syncKeyboardFrame(e);
             // persist scroll value
             scrollPosition.value = position.value;
             // just persist height - later will be used in interpolation
             keyboardHeight.value = e.height;
+            // and update keyboard spacer size
+            syncKeyboardFrame(e);
           }
 
           // focus was changed