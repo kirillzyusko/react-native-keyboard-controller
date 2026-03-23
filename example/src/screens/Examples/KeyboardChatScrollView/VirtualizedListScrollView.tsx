dd9ee20049cd4b9d39df15ae6f33ba789b19e6f0
kirillzyusko <zyusko.kirik@gmail.com>
2026-03-23 15:01:58 +0100

    docs: add workaround for non-working `scrollToEnd`

--- example/src/screens/Examples/KeyboardChatScrollView/VirtualizedListScrollView.tsx
+++ example/src/screens/Examples/KeyboardChatScrollView/VirtualizedListScrollView.tsx
@@ -1,4 +1,4 @@
-import React, { forwardRef, useCallback, useState } from "react";
+import React, { forwardRef, useCallback, useRef, useState } from "react";
 import {
   type LayoutChangeEvent,
   type ScrollViewProps,
@@ -15,10 +15,12 @@ import {
   invertedContentContainerStyle,
 } from "./styles";
 
+import type { Ref } from "react";
 import type { SharedValue } from "react-native-reanimated";
 
 type VirtualizedListScrollViewProps = ScrollViewProps & {
   extraContentPadding?: SharedValue<number>;
+  scrollViewRef?: Ref<VirtualizedListScrollViewRef>;
 };
 
 export type VirtualizedListScrollViewRef = React.ElementRef<
@@ -28,49 +30,77 @@ export type VirtualizedListScrollViewRef = React.ElementRef<
 const VirtualizedListScrollView = forwardRef<
   VirtualizedListScrollViewRef,
   VirtualizedListScrollViewProps
->(({ onLayout: onLayoutProp, extraContentPadding, ...props }, ref) => {
-  const [layoutPass, setLayoutPass] = useState(0);
-  const { bottom } = useSafeAreaInsets();
-  const chatKitOffset = bottom - MARGIN;
+>(
+  (
+    { onLayout: onLayoutProp, extraContentPadding, scrollViewRef, ...props },
+    ref,
+  ) => {
+    // combine FlatList's internal ref with the user-provided scrollViewRef
+    const scrollViewRefStable = useRef<Ref<VirtualizedListScrollViewRef>>(null);
 
-  const { inverted, freeze, mode, keyboardLiftBehavior } = useChatConfigStore();
-
-  // on old arch only FlatList and FlashList supports `inverted` prop
-  const isInvertedSupported =
-    inverted && (mode === "flat" || mode === "flash") ? inverted : false;
-  const onLayout = useCallback(
-    (e: LayoutChangeEvent) => {
-      setLayoutPass((l) => l + 1);
-      onLayoutProp?.(e);
-    },
-    [onLayoutProp],
-  );
-
-  return (
-    <>
-      <KeyboardChatScrollView
-        ref={ref}
-        automaticallyAdjustContentInsets={false}
-        contentContainerStyle={
-          inverted ? invertedContentContainerStyle : contentContainerStyle
+    scrollViewRefStable.current = scrollViewRef ?? null;
+    const combinedRef = useCallback(
+      (instance: VirtualizedListScrollViewRef | null) => {
+        if (typeof ref === "function") {
+          ref(instance);
+        } else if (ref) {
+          ref.current = instance;
         }
-        contentInsetAdjustmentBehavior="never"
-        extraContentPadding={extraContentPadding}
-        freeze={freeze}
-        inverted={isInvertedSupported}
-        keyboardDismissMode="interactive"
-        keyboardLiftBehavior={keyboardLiftBehavior}
-        offset={chatKitOffset}
-        testID="chat.scroll"
-        onLayout={onLayout}
-        {...props}
-      />
-      <Text style={styles.counter} testID="layout_passes">
-        Layout pass: {layoutPass}
-      </Text>
-    </>
-  );
-});
+
+        const svRef = scrollViewRefStable.current;
+
+        if (typeof svRef === "function") {
+          svRef(instance);
+        } else if (svRef) {
+          svRef.current = instance;
+        }
+      },
+      [ref, scrollViewRefStable],
+    );
+    const [layoutPass, setLayoutPass] = useState(0);
+    const { bottom } = useSafeAreaInsets();
+    const chatKitOffset = bottom - MARGIN;
+
+    const { inverted, freeze, mode, keyboardLiftBehavior } =
+      useChatConfigStore();
+
+    // on old arch only FlatList and FlashList supports `inverted` prop
+    const isInvertedSupported =
+      inverted && (mode === "flat" || mode === "flash") ? inverted : false;
+    const onLayout = useCallback(
+      (e: LayoutChangeEvent) => {
+        setLayoutPass((l) => l + 1);
+        onLayoutProp?.(e);
+      },
+      [onLayoutProp],
+    );
+
+    return (
+      <>
+        <KeyboardChatScrollView
+          ref={combinedRef}
+          automaticallyAdjustContentInsets={false}
+          contentContainerStyle={
+            inverted ? invertedContentContainerStyle : contentContainerStyle
+          }
+          contentInsetAdjustmentBehavior="never"
+          extraContentPadding={extraContentPadding}
+          freeze={freeze}
+          inverted={isInvertedSupported}
+          keyboardDismissMode="interactive"
+          keyboardLiftBehavior={keyboardLiftBehavior}
+          offset={chatKitOffset}
+          testID="chat.scroll"
+          onLayout={onLayout}
+          {...props}
+        />
+        <Text style={styles.counter} testID="layout_passes">
+          Layout pass: {layoutPass}
+        </Text>
+      </>
+    );
+  },
+);
 
 const styles = StyleSheet.create({
   counter: {
