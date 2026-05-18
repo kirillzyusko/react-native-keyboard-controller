c9bbd3a4a5407936fe55bb23c09daa29609014dc
kirillzyusko <zyusko.kirik@gmail.com>
2026-05-18 13:17:37 +0200

    feat: RNKC + KAV

--- example/src/screens/Examples/KeyboardAvoidingView/index.tsx
+++ example/src/screens/Examples/KeyboardAvoidingView/index.tsx
@@ -6,7 +6,10 @@ import {
   TouchableOpacity,
   View,
 } from "react-native";
-import { KeyboardAvoidingView } from "react-native-keyboard-controller";
+import {
+  KeyboardAvoidingView,
+  KeyboardStickyView,
+} from "react-native-keyboard-controller";
 
 import styles from "./styles";
 
@@ -21,73 +24,45 @@ const behaviors: Behavior[] = ["padding", "height", "position"];
 
 export default function KeyboardAvoidingViewExample({ navigation }: Props) {
   const [behavior, setBehavior] = useState<Behavior>(behaviors[0]);
-  const [isPackageImplementation, setPackageImplementation] = useState(true);
+  const [isPackageImplementation, setPackageImplementation] = useState(false);
 
-  useEffect(() => {
-    navigation.setOptions({
-      headerRight: () => (
-        <View style={styles.row}>
-          <Text
-            style={styles.header}
-            testID="keyboard_avoiding_view.implementation"
-            onPress={() => setPackageImplementation((value) => !value)}
-          >
-            {isPackageImplementation ? "Package" : "RN"}
-          </Text>
-          <Text
-            style={styles.header}
-            testID="keyboard_avoiding_view.behavior"
-            onPress={() => {
-              const index = behaviors.indexOf(behavior);
-
-              setBehavior(
-                behaviors[index === behaviors.length - 1 ? 0 : index + 1],
-              );
-            }}
-          >
-            {behavior}
-          </Text>
-        </View>
-      ),
-    });
-  }, [isPackageImplementation, behavior]);
-
-  const Container = isPackageImplementation
-    ? KeyboardAvoidingView
-    : RNKeyboardAvoidingView;
+  const Container = RNKeyboardAvoidingView;
 
   return (
-    <Container
+    <KeyboardAvoidingView
       behavior={behavior}
       contentContainerStyle={styles.container}
-      keyboardVerticalOffset={100}
-      style={styles.content}
+      style={{flex: 1}}
       testID="keyboard_avoiding_view.container"
     >
-      <View style={styles.inner}>
-        <Text style={styles.heading}>Header</Text>
-        <View style={styles.inputs}>
-          <TextInput
-            placeholder="Username"
-            placeholderTextColor="#7C7C7C"
-            style={styles.textInput}
-            testID="keyboard_avoiding_view.username"
-          />
-          <TextInput
-            secureTextEntry
-            placeholder="Password"
-            placeholderTextColor="#7C7C7C"
-            style={styles.textInput}
-            testID="keyboard_avoiding_view.password"
-          />
+      <View style={styles.content}>
+        <Text style={styles.heading}>Welcome back!</Text>
+        <View style={styles.inner}>
+          <View style={styles.inputs}>
+            <TextInput
+              placeholder="Username"
+              placeholderTextColor="#7C7C7C"
+              style={styles.textInput}
+              testID="keyboard_avoiding_view.username"
+            />
+            <TextInput
+              secureTextEntry
+              placeholder="Password"
+              placeholderTextColor="#7C7C7C"
+              style={styles.textInput}
+              testID="keyboard_avoiding_view.password"
+            />
+          </View>
         </View>
-        <TouchableOpacity
-          style={styles.button}
-          testID="keyboard_avoiding_view.submit"
-        >
-          <Text style={styles.text}>Submit</Text>
-        </TouchableOpacity>
+        <>
+          <TouchableOpacity
+            style={styles.button}
+            testID="keyboard_avoiding_view.submit"
+          >
+            <Text style={styles.text}>Login</Text>
+          </TouchableOpacity>
+        </>
       </View>
-    </Container>
+    </KeyboardAvoidingView>
   );
 }
