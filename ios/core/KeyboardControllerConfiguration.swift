85e9bcea9642dd63fed821800aff838b90053ff0
kirillzyusko <zyusko.kirik@gmail.com>
2026-07-01 22:48:29 +0200

    fix: swiftlint

--- ios/core/KeyboardControllerConfiguration.swift
+++ ios/core/KeyboardControllerConfiguration.swift
@@ -8,7 +8,8 @@
 enum KeyboardControllerConfiguration {
   static var usesKeyboardLayoutGuideTracking: Bool {
     // `KeyboardLayoutGuide` is not stable yet, especially on iOS 26. Several bugs:
-    // - KVO on interactive dismissal produces incorrect values on iOS 26.0 (and the fix on iOS 26.0 breaks the behavior on iOS 26.2)
+    // - KVO on interactive dismissal produces incorrect values on iOS 26.0
+    // (and the fix on iOS 26.0 breaks the behavior on iOS 26.2)
     // - `TimingAnimation` is never picked up properly (after interactive dismissal when keyboard goes up)
     return false
   }
