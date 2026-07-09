d577c57e70ff747901cae4b1f1c59d900dc1d721
kirillzyusko <zyusko.kirik@gmail.com>
2026-07-09 11:49:40 +0200

    fix: use new lookup method on iOS 26+

--- ios/traversal/KeyboardView.swift
+++ ios/traversal/KeyboardView.swift
@@ -10,25 +10,33 @@ import Foundation
 import UIKit
 
 enum KeyboardView {
-  private static let windowPrefix = "<UITextEffectsWindow"
+  private static let windowPrefix = ["<UITextEffectsWindow", "<UIRemoteKeyboardWindow"]
   private static let containerPrefixes = ["<UIInputSetContainerView", "<UITrackingWindowView"]
   private static let hostPrefixes = ["<UIInputSetHostView", "<UIKeyboardItemContainerView"]
   /// inspired by https://stackoverflow.com/questions/32598490/show-uiview-with-buttons-over-keyboard-like-in-skype-viber-messengers-swift-i
   static func find() -> UIView? {
-    let keyboardWindow = UIWindow.keyboardWindow
+    var windows: [UIWindow?] = []
 
-    guard let window = keyboardWindow else {
-      return nil
+    if #available(iOS 26.0, *) {
+      windows = [UIWindow.keyboardWindow]
+    } else {
+      windows = UIApplication.shared.windows
     }
 
-    for subview in window.subviews {
-      if subview.description.hasAnyPrefix(containerPrefixes) {
-        for hostView in subview.subviews {
-          if hostView.description.hasAnyPrefix(hostPrefixes), hostView.frame.height != 0 {
-            return hostView
+    for window in windows {
+      guard let currentWindow = window else { continue }
+
+      if currentWindow.description.hasAnyPrefix(windowPrefix) {
+        for subview in currentWindow.subviews {
+          if subview.description.hasAnyPrefix(containerPrefixes) {
+            for hostView in subview.subviews {
+              if hostView.description.hasAnyPrefix(hostPrefixes), hostView.frame.height != 0 {
+                return hostView
+              }
+            }
+            break
           }
         }
-        break
       }
     }
 
