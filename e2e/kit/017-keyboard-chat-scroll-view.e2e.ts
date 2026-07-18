78f7a9b14665b1928528998484bbb1c601fa1b93
kirillzyusko <zyusko.kirik@gmail.com>
2026-07-18 13:10:51 +0200

    fix: interactive dismissal test

--- e2e/kit/017-keyboard-chat-scroll-view.e2e.ts
+++ e2e/kit/017-keyboard-chat-scroll-view.e2e.ts
@@ -62,6 +62,7 @@ describe("`KeyboardChatScrollView` specs", () => {
   });
 
   it("should close keyboard interactively", async () => {
+    await waitAndTap("chat.input");
     await element(by.id("chat.scroll")).swipe("up", "fast", 0.2, 0.5, 0.2);
     await delay(5000);
     await element(by.id("chat.scroll")).swipe("down", "fast", 0.4, 0.5, 0.2);
