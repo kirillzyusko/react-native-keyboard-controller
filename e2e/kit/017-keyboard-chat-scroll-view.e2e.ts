7689ffdd0c2be01854422f32225f1b5704e21c93
Kirill Zyusko <zyusko.kirik@gmail.com>
2026-07-20 10:46:29 +0200

    e2e: add tests for `KeyboardChatScrollView` (#1395)
    ## 📜 Description
    
    Cover basic functionality of `KeyboardChatScrollView` with e2e tests.
    
    ## 💡 Motivation and Context
    
    This is something that I wanted to do for a very long time. But when I
    built first e2e test kit I realized that
    https://github.com/kirillzyusko/react-native-keyboard-controller/pull/1552
    was a blocker (it failed Android tests time to time).
    
    Recently I fixed that and it unblocked this PR. However now I realized
    that `useExtraContentPadding` seems to be flaky as well 🤷‍♂️ (produces
    different scroll time-to-time). But I'll leave it for future - for now I
    just disabled test with `.skip`
    
    ## 📢 Changelog
    
    <!-- High level overview of important changes -->
    <!-- For example: fixed status bar manipulation; added new types
    declarations; -->
    <!-- If your changes don't affect one of platform/language below - then
    remove this platform/language -->
    
    ### E2E
    
    - added `KeyboardChatScrollView` tests;
    
    ## 🤔 How Has This Been Tested?
    
    Tested via this PR.
    
    ## 📸 Screenshots (if appropriate):
    
    <img width="502" height="309" alt="image"
    src="https://github.com/user-attachments/assets/8d078f89-d5b8-4b91-8c52-7abdb93e079e"
    />
    
    ## 📝 Checklist
    
    - [x] CI successfully passed
    - [x] I added new mocks and corresponding unit-tests if library API was
    changed

---
+++ e2e/kit/017-keyboard-chat-scroll-view.e2e.ts
@@ -0,0 +1,85 @@
+import { expect } from "detox";
+
+import { expectBitmapsToBeEqual } from "./asserts";
+import {
+  scrollDownUntilElementIsVisible,
+  waitAndReplace,
+  waitAndTap,
+  waitAndType,
+  waitForElementById,
+  waitForExpect,
+} from "./helpers";
+
+const closeKeyboard = async () => {
+  await waitAndTap("layout_passes");
+};
+
+describe("`KeyboardChatScrollView` specs", () => {
+  it("should navigate to `KeyboardChatScrollView` example", async () => {
+    await scrollDownUntilElementIsVisible("main_scroll_view", "chat_kit");
+    await waitAndTap("chat_kit");
+    await waitForElementById("chat.input");
+  });
+
+  it("should use non-inverted `FlatList`", async () => {
+    await waitAndTap("open_bottom_sheet_modal");
+    await waitAndTap("bottom_sheet_toggle_flat_list_state");
+    await waitAndTap("bottom_sheet_close_modal");
+    await element(by.id("chat.scroll")).swipe("up", "fast", 1, 0.5, 0.5);
+    await element(by.id("chat.scroll")).swipe("up", "fast", 1, 0.5, 0.5);
+    await expect(element(by.id("layout_passes"))).toHaveText("Layout pass: 1");
+  });
+
+  it("should push content up with `always` mode", async () => {
+    await waitAndTap("chat.input");
+    await waitForExpect(async () => {
+      await expectBitmapsToBeEqual("KeyboardChatScrollViewAlways");
+    });
+    await expect(element(by.id("layout_passes"))).toHaveText("Layout pass: 1");
+  });
+
+  it("should push content down even if you scrolled a little bit", async () => {
+    await element(by.id("chat.scroll")).swipe("down", "slow", 0.1, 0.5, 0.3);
+    await closeKeyboard();
+    await waitForExpect(async () => {
+      await expectBitmapsToBeEqual("KeyboardChatScrollViewAlwaysClosed");
+    });
+    await expect(element(by.id("layout_passes"))).toHaveText("Layout pass: 1");
+  });
+
+  it("should not change position if we reached the top", async () => {
+    await element(by.id("chat.scroll")).swipe("down", "fast", 1, 0.5, 0.5);
+    await element(by.id("chat.scroll")).swipe("down", "fast", 1, 0.5, 0.5);
+    await waitAndTap("chat.input");
+    await element(by.id("chat.scroll")).swipe("down", "fast", 0.2, 0.5, 0.2);
+    await element(by.id("chat.scroll")).swipe("down", "fast", 0.2, 0.5, 0.2);
+    await expect(element(by.id("layout_passes"))).toHaveText("Layout pass: 1");
+    await closeKeyboard();
+    await waitForExpect(async () => {
+      await expectBitmapsToBeEqual("KeyboardChatScrollViewAlwaysClosedAtTop");
+    });
+  });
+
+  it("should close keyboard interactively", async () => {
+    await waitAndTap("chat.input");
+    await element(by.id("chat.scroll")).swipe("up", "fast", 0.2, 0.5, 0.2);
+    await element(by.id("chat.scroll")).swipe("down", "fast", 0.4, 0.5, 0.2);
+    await waitForExpect(async () => {
+      await expectBitmapsToBeEqual(
+        "KeyboardChatScrollViewAlwaysClosedInteractively",
+      );
+    });
+  });
+
+  // TODO: this test is flaky on iOS, so temporarily skip it
+  it.skip("should change content position when input grows", async () => {
+    await waitAndTap("chat.input");
+    await waitAndType("chat.input", "\n\n\n\n");
+    await waitForExpect(async () => {
+      await expectBitmapsToBeEqual("KeyboardChatScrollViewAlwaysInputGrown");
+    });
+    await waitAndReplace("chat.input", "");
+  });
+
+  it.todo("should work with inverted list");
+});
